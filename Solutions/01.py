d84cc5a344163e3a7b000d0d83956984eb0ea5a5
Ruppy <ruppysuppy@gmail.com>
2019-11-22 21:22:59 +0530

    Updated prob 1

--- Solutions/01.py
+++ Solutions/01.py
@@ -18,6 +18,33 @@ def target_sum(arr, len_arr, target):
     # If no element sums up to the target element, False is returned
     return [False]
 
+# FUNCTION TO PERFORM THE OPERATION (NO ADDITIONAL SPACE)
+def target_sum_2ptr(arr, len_arr, target):
+    arr.sort() # Sorting the list
+
+    # Setting default values
+    ptr1 = 0
+    ptr2 = len_arr - 1
+
+    # Main loop iterating and checking if the target sum is achieved
+    while (ptr1 < ptr2):
+        # If the target is achieved, True is returned
+        if (arr[ptr1] + arr[ptr2] == target):
+            return True
+        
+        # If the sum of the elements is less than the target, the pointer starting from the begining is incremented.
+        # This is because the array is sorted and so arr[i] <= arr[i + 1]
+        elif (arr[ptr1] + arr[ptr2] < target):
+            ptr1 += 1
+
+        # If the sum of the elements is greater than the target, the pointer starting from the end is decremented.
+        # This is because the array is sorted and so arr[i] >= arr[i - 1]
+        else:
+            ptr2 -= 1
+    
+    # If the pointers meet, then there are no elements which add up to the target, so False is returned
+    return False
+
 # DRIVER CODE
 Arr = [int(i) for i in input("Enter the array elements (separated by a space): ").split()]
 target = int(input("Enter the target sum: "))
@@ -34,4 +61,7 @@ SPECS:
 
 TIME COMPLEXITY: O(n)
 SPACE COMPLEXITY: O(n)
+
+NOTE: If additional space is not allowed to be used, then the list can be sorted and 2 pointer method applied.
+As a result, time complexity becomes O(nlog(n)) and space complexity becomes O(1)
 '''
