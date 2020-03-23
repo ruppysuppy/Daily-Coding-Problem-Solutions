29f08cd513a7afd126fb8e7bfe95856e5dff9fe8
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-03-23 09:16:07 +0530

    Uploaded prob 128

---
+++ Solutions/128.py
@@ -0,0 +1,57 @@
+'''
+Problem:
+
+The Tower of Hanoi is a puzzle game with three rods and n disks, each a different size.
+All the disks start off on the first rod in a stack. 
+They are ordered by size, with the largest disk on the bottom and the smallest one at the top.
+The goal of this puzzle is to move all the disks from the first rod to the last rod while following these rules:
+* You can only move one disk at a time.
+* A move consists of taking the uppermost disk from one of the stacks and placing it on top of another stack.
+* You cannot place a larger disk on top of a smaller disk.
+Write a function that prints out all the steps necessary to complete the Tower of Hanoi. 
+You should assume that the rods are numbered, with the first rod being 1, the second (auxiliary) rod being 2, and the last (goal) rod being 3.
+
+
+EXAMPLE:
+
+Input = 3
+Output = Move 1 to 3
+         Move 1 to 2
+         Move 3 to 2
+         Move 1 to 3
+         Move 2 to 1
+         Move 2 to 3
+         Move 1 to 3
+'''
+
+# FUNCTION TO PERFORM THE OPERATION
+def towers_of_hanoi(n, start_rod=None, aux_rod=None, end_rod=None):
+    # on the first call of the function, the rod names are initialized and a small intro printed
+    if (not start_rod):
+        # initialized name for the rod, not using the convention mentioned in the question
+        start_rod = 'start_rod'
+        print(f"\nTower of Hanoi for {n} Disks ========================================")
+    if (not aux_rod):
+        # initialized name for the rod, not using the convention mentioned in the question
+        aux_rod = 'aux_rod'
+    if (not end_rod):
+        # initialized name for the rod, not using the convention mentioned in the question
+        end_rod = 'end_rod'
+
+    # if the number of disks left to move is 1, its just shifted [base case for recursion]
+    if (n == 1):
+        print(f'Move disk 1 from {start_rod} to {end_rod}')
+        return
+    
+    # moving the top disk of the start rod to the proper position in the auxilary rod using the end rod as buffer
+    towers_of_hanoi(n-1, start_rod, end_rod, aux_rod)
+    # moving the top disk from the start rod to the end rod
+    print(f'Move disk {n} from {start_rod} to {end_rod}')
+    # moving the top disk of the auxilary rod to the proper position in the end rod using the start rod as buffer
+    towers_of_hanoi(n-1, aux_rod, start_rod, end_rod)
+
+# DRIVER CODE
+towers_of_hanoi(3)
+towers_of_hanoi(4)
+towers_of_hanoi(5)
+towers_of_hanoi(6)
