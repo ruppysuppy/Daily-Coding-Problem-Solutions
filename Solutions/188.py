014275deff0144fce14ad537c954d5f81ea62bbb
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-05-22 09:08:35 +0530

    Uploaded prob 188

---
+++ Solutions/188.py
@@ -0,0 +1,40 @@
+'''
+Problem:
+
+What will this code print out?
+
+def make_functions():
+    flist = []
+
+    for i in [1, 2, 3]:
+        def print_i():
+            print(i)
+        flist.append(print_i)
+
+    return flist
+
+functions = make_functions()
+for f in functions:
+    f()
+
+How can we make it print out what we apparently want?
+'''
+
+# The code will print 3 thrice (in 3 lines) as i is passed by reference
+
+def make_functions():
+    flist = []
+
+    for i in [1, 2, 3]:
+        # the function can (and should) be shifted outside the loop
+        # this program shows the minimum necessary changes required to print 1 to 3
+        def print_i(i):
+            print(i)
+        flist.append((print_i, i))
+
+    return flist
+
+
+functions = make_functions()
+for f, i in functions:
+    f(i)
