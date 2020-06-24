dbfeae5ce774e70ab88aa80cf90827728b6d5d15
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-06-24 09:07:24 +0530

    Uploaded prob 221

---
+++ Solutions/221.py
@@ -0,0 +1,35 @@
+'''
+Problem:
+
+Let's define a "sevenish" number to be one which is either a power of 7, or the sum of unique powers of 7. 
+The first few sevenish numbers are 1, 7, 8, 49, and so on. Create an algorithm to find the nth sevenish number.
+'''
+
+# FUNCTION TO PERFORM THE OPERATION
+def get_sevenish_num(number):
+    curr = 1
+    curr_iteration = 1
+
+    while (curr < number):
+        curr_iteration += 1
+        curr += curr_iteration
+    
+    curr -= curr_iteration
+    
+    result = 7 ** (curr_iteration - 1)
+    curr_to_add = 1
+
+    for _ in range(number - curr - 1):
+        result += curr_to_add
+        curr_to_add *= 7
+
+    return result
+
+
+# DRIVER CODE
+print(get_sevenish_num(1)) # 1 = 7 ^ 0
+print(get_sevenish_num(2)) # 7 = 7 ^ 1
+print(get_sevenish_num(3)) # 8 = 7 ^ 0 + 7 ^ 1
+print(get_sevenish_num(4)) # 49 = 7 ^ 2
+print(get_sevenish_num(5)) # 50 = 7 ^ 0 + 7 ^ 2
+print(get_sevenish_num(6)) # 57 = 7 ^ 0 + 7 ^ 1 + 7 ^ 2
