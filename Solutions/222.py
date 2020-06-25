a7dce0ad2f8ba9dc0df6b1878344f0e2df094f54
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-06-25 08:43:43 +0530

    Uploaded prob 222

---
+++ Solutions/222.py
@@ -0,0 +1,29 @@
+'''
+Problem:
+
+Given an absolute pathname that may have . or .. as part of it, return the shortest standardized path.
+
+Example:
+
+Input = "/usr/bin/../bin/./scripts/../"
+Output = "/usr/bin/"
+'''
+
+
+def get_shortest_standardized_path(path):
+    path_list = path.split('/')
+    stack = []
+
+    for curr in path_list:
+        if (curr == '..'):
+            stack.pop()
+        elif (curr == '.'):
+            continue
+        else:
+            stack.append(curr)
+    
+    return "/".join(stack)
+
+
+if __name__ == "__main__":
+    print(get_shortest_standardized_path("/usr/bin/../bin/./scripts/../"))
