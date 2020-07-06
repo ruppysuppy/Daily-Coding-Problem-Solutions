4b439b093b35d9e6f2d1963d86bebb303fd2ad0f
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-07-06 09:02:55 +0530

    Uploaded prob 233

---
+++ Solutions/233.py
@@ -0,0 +1,15 @@
+'''
+Problem:
+
+Implement the function fib(n), which returns the nth number in the Fibonacci sequence, using only O(1) space.
+'''
+
+
+def fib(n):
+    curr, last = 1, 0
+    for _ in range(n-1): curr, last = last + curr, curr
+    return curr
+
+
+# DRIVER CODE
+for i in range(1, 11): print(f"Fib {i}:\t{fib(i)}")
