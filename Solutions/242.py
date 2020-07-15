eccd2825264ee3c6397b7d5eda6d1105d32103e4
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-07-15 08:59:13 +0530

    Uploaded prob 242

---
+++ Solutions/242.py
@@ -0,0 +1,32 @@
+'''
+Problem:
+
+You are given an array of length 24, where each element represents the number of new subscribers during the corresponding hour.
+Implement a data structure that efficiently supports the following:
+* update(hour: int, value: int): Increment the element at index hour by value.
+* query(start: int, end: int): Retrieve the number of subscribers that have signed up between start and end (inclusive).
+You can assume that all values get cleared at the end of the day, and that you will not be asked for start and end values that wrap around midnight.
+'''
+
+class Hourly_Subscribers:
+    def __init__(self):
+        self.sub_count = [0 for _ in range(24)]
+    
+    def update(self, hour, value):
+        self.sub_count[hour - 1] += value
+    
+    def query(self, start, end):
+        return sum(self.sub_count[start : end + 1])
+
+
+# DRIVER CODE
+hs = Hourly_Subscribers()
+
+hs.update(2, 50)
+hs.update(5, 100)
+
+print(hs.query(1, 7))
+
+hs.update(2, 10)
+
+print(hs.query(1, 7))
