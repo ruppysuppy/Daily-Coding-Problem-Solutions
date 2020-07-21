479565e02e90c0c1fc023b768c94df0f82245d7b
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-07-21 09:04:48 +0530

    Uploaded prob 248

---
+++ Solutions/248.py
@@ -0,0 +1,18 @@
+'''
+Problem:
+
+Find the maximum of two numbers without using any if-else statements, branching, or direct comparisons.
+'''
+
+# max using bit-wise operations
+def get_max(num1, num2):
+    return num1 ^ ((num1 ^ num2) & -(num1 < num2))  
+
+
+# DRIVER CODE
+print(get_max(1, 5))
+print(get_max(4, 3))
+print(get_max(-3, 6))
+print(get_max(5, -4))
+print(get_max(-4, -2))
+print(get_max(-3, -6))
