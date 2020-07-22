f1a4352d859494695034e224c83aaf65ccf3d5fc
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-07-22 08:52:18 +0530

    Uploaded prob 249

---
+++ Solutions/249.py
@@ -0,0 +1,19 @@
+'''
+Problem:
+
+Given an array of integers, find the maximum XOR of any two elements.
+'''
+
+from sys import maxsize
+
+
+def get_max_xor(arr):
+    max_xor = -maxsize
+    for index, elem1 in enumerate(arr):
+        for elem2 in arr[index + 1:]:
+            max_xor = max(max_xor, elem1 ^ elem2)
+    return max_xor
+
+
+# DRIVER CODE
+print(get_max_xor([1, 2, 3, 4]))
