a50e582c21e75ae781270a987f09823ee3148632
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-08-04 09:04:28 +0530

    Refactored solution 2 + Updated solution 1 question

--- Solutions/01.py
+++ Solutions/01.py
@@ -1,8 +1,10 @@
 '''
 Problem:
 
-Given n integers and a target number, find whether 2 of the given numbers sum up to the
-target number.
+Given a list of numbers, return whether any two sums to k. For example, given
+[10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass?
 '''
 
 
