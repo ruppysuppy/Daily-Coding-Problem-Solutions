6b7ac77fa7f8afe0ffba39de3279f5e0d132991d
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-08-04 09:42:22 +0530

    Refactored solution 4

--- Solutions/04.py
+++ Solutions/04.py
@@ -1,36 +1,48 @@
 '''
 Problem:
 
-Find the 1st missing positive number in the given set of numbers in O(n).
-Eg: 1, 2, 3, 4 => 5
+Given an array of integers, find the first missing positive integer in linear time and
+constant space. In other words, find the lowest positive integer that does not exist in
+the array. The array can contain duplicates and negative numbers as well.
+
+For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+
+You can modify the input array in-place.
 '''
 
-# FUNCTION TO PERFORM THE OPERATION
-def first_positive_integer(Arr, length):
-    # Cache to store the numbers (later used to get access in O(1) time)
-    num_cache = set()
+from typing import List
 
-    # Populating Cache
-    for i in Arr:
-        num_cache.add(i)
 
-    # Checking if the number is in cache, if its absent, the value is returned
-    # The range goes from 1 to length + 2 (as the range function yields values in [start, stop))
-    # In an array of 'length' elements, at most 'length' unique values can be present
-    # So we are bound to find a value which is not present ((length+2) - 1 = (length+1))
-    for i in range(1, length+2):
-        if (i not in num_cache):
-            return i
+def first_missing_positive_integer(arr: List[int]) -> int:
+    # placing the positive elements (< length) in their proper position
+    # proper position index = element - 1
+    # if after the palcement is complete, index of the 1st element not in its proper
+    # position is the answer
+    length = len(arr)
+    # even though there is a nested loop it is a O(n) algorithm as the cap on the
+    # maximum iterations is 2 * n [Amortised analysis]
+    for i in range(length):
+        correctPos = arr[i] - 1
+        while 1 <= arr[i] <= length and arr[i] != arr[correctPos]:
+            arr[i], arr[correctPos] = arr[correctPos], arr[i]
+            correctPos = arr[i] - 1
+
+    for i in range(length):
+        if i + 1 != arr[i]:
+            return i + 1
+    return length + 1
+
 
 # DRIVER CODE
-Arr = [1, 2, 3, 4]#[int(i) for i in input("Enter the elements of the array (separated by space): ").split()]
-ans = first_positive_integer(Arr, len(Arr))
+print(first_missing_positive_integer([3, 4, 2, 1]))
+print(first_missing_positive_integer([3, 4, -1, 1]))
+print(first_missing_positive_integer([1, 2, 5]))
+print(first_missing_positive_integer([-1, -2]))
 
-print(ans)
 
 '''
 SPECS:
 
 TIME COMPLEXITY: O(n)
-SPACE COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(1)
 '''
