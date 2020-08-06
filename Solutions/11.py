a7afd7425456c75bfb46f4516c45c6a0a37d5e7c
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-08-06 09:47:36 +0530

    Refactored solution 11, 12

--- Solutions/11.py
+++ Solutions/11.py
@@ -1,89 +1,40 @@
 '''
 Problem:
 
-Implement an autocomplete system. 
-That is, given a query string s and a set of all possible query strings, return all strings in the set that have s as a prefix.
+Implement an autocomplete system. That is, given a query string s and a set of all
+possible query strings, return all strings in the set that have s as a prefix.
 
-Example:
-['dog', 'deer', 'deal'], 'de' => [deer, deal]
+For example, given the query string de and the set of strings [dog, deer, deal], return
+[deer, deal].
+
+Hint: Try preprocessing the dictionary into a more efficient data structure to speed up
+queries.
 '''
 
-# Node class for the Trie Structure
-class Node():
-    def __init__(self, ):
-        self.children = {}
-        self.end = False
+from typing import List
 
-# Trie Structure
-class Trie():
-    # Initialize
-    def __init__(self):
-        self.root = Node()
-    
-    # Adding a string to the Trie
-    def add(self, string):
-        # Starting from the root
-        node = self.root
+# Local Import from the DataStructure module
+from DataStructures.Trie import Trie
 
-        # Traversing and Creating new nodes as required
-        for i in string:
-            # If the the node exists moving the pointer to the location
-            if (i in node.children):
-                node = node.children[i]
 
-            # Creating a new node in case it doesn't exist
-            else:
-                node.children[i] = Node()
-                node = node.children[i]
-        
-        # Placing flag to indicate end of a word
-        node.end = True
-    
-    # Function to generate the trie from the given words
-    def form_trie(self, keys):
-        for key in keys:
-            self.add(key)
-    
-    # Suggestion function (Uses suggestion helper)
-    def suggestion(self, prefix):
-        pos = self.root
-        word = prefix
-        word_list = []
-
-        # Traversing to the desired position (end of prefix)
-        for i in prefix:
-            # If the prefix doesn't exist, empty list is returned
-            if (i not in pos.children):
-                return word_list
-            pos = pos.children[i]
-        
-        # Calling Suggestion helper to traverse the Trie and return suggestions
-        return self.suggestion_helper(pos, word, word_list)
-
-    # Recursive helper function    
-    def suggestion_helper(self, node, word, word_list):
-        # If it is the end of a word (determined using the flag), the word is added to the list
-        if (node.end == True):
-            word_list.append(word)
-        
-        # For all characters in the node, suggestion helper is called recursively to generate the list of words
-        for i in node.children:
-            word_list = self.suggestion_helper(node.children[i], word+i, word_list)
-
-        return word_list
-
-# FUNCTION TO PERFORM THE OPERATION
-def get_suggestion(word_list, prefix):
-    # Creating the Trie Object
+def get_suggestion(word_list: List[str], prefix: str) -> List[str]:
+    # using tree data structure to get the suggestions (for deatils, check 
+    # ./DataStructres/Trie)
     trie = Trie()
+    trie.add_words(word_list)
+    prefix_match = trie.get_suggestions(prefix)
+    # type casting the result to list as the return type is a set
+    return list(prefix_match)
 
-    # Generating the Trie
-    trie.form_trie(word_list)
-
-    # Getting the suggestions
-    List = trie.suggestion(prefix)
-
-    return List
 
 # DRIVER CODE
 print(get_suggestion(['deer', 'dog', 'deal'], 'de'))
+
+
+'''
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(n)
+[n = total number of characters (all words)]
+'''
--- Solutions/12.py
+++ Solutions/12.py
@@ -1,38 +1,40 @@
 '''
 Problem:
 
-There exists a staircase with N steps, and you can climb up either 1 or 2 steps at a time. 
-Given N, write a function that returns the number of unique ways you can climb the staircase. The order of the steps matters.
+There exists a staircase with N steps, and you can climb up either 1 or 2 steps at a
+time. Given N, write a function that returns the number of unique ways you can climb
+the staircase. The order of the steps matters.
 
-What if, instead of being able to climb 1 or 2 steps at a time, you could climb any number from a set of positive integers X? 
-For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time.
+For example, if N is 4, then there are 5 unique ways:
 
-EXAMPLE:
+1, 1, 1, 1
+2, 1, 1
+1, 2, 1
+1, 1, 2
+2, 2
 
-input = 4
-output = 5
+What if, instead of being able to climb 1 or 2 steps at a time, you could climb any
+number from a set of positive integers X? For example, if X = {1, 3, 5}, you could
+climb 1, 3, or 5 steps at a time.
 '''
 
-# FUNCTION TO PERFORM THE OPERATION
-def count_ways(steps, permissable_setps=[1,2]):
-    # Dynamic Programming to reduce time complexity to O(n)
-    # Array to store the number of ways a step can be reached
-    dynamic_arr = [0 for i in range(steps+1)]
+from typing import List
 
-    # Setting the number of ways 0 can be reached (1 = Not using any permissible steps)
-    dynamic_arr[0] = 1
 
-    # calculating using the formula steps_i = sum(steps_(i-j)) [i = [0, steps], j = [values in permissable_setps]]
-    # the sequence genearted is SIMILAR to fibonacci series
-    for pos in range(steps+1):
-        for step in permissable_setps:
-            temp = pos - step
+def count_ways(steps: int, permissable_steps: List[int] = [1, 2]) -> int:
+    # dynamic array to store the number of ways a step can be reached
+    num_ways = [0 for i in range(steps+1)]
+    # base case
+    num_ways[0] = 1
+    # calculating using the formula steps_i = sum(steps_i(i - j))
+    # i ε [0, steps); j ε permissable_steps
+    for pos in range(steps + 1):
+        for step in permissable_steps:
+            temp_pos = pos - step
+            if temp_pos >= 0:
+                num_ways[pos] += num_ways[temp_pos]
+    return num_ways[steps]
 
-            if (temp >= 0):
-                dynamic_arr[pos] += dynamic_arr[temp]
-    
-    # After filling up the values, the value at i'th position indicates the number of ways one can traverse i steps using the permissable_setps
-    return dynamic_arr[steps]
 
 # DRIVER CODE
 print(count_ways(4))
@@ -42,3 +44,12 @@ print(count_ways(3, [1, 3, 5]))
 print(count_ways(4, [1, 3, 5]))
 print(count_ways(5, [1, 3, 5]))
 print(count_ways(6, [1, 3, 5]))
+
+
+'''
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(n)
+[n = number of steps]
+'''
