a54490d27594000b73254a9984560179a08dd165
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-08-15 08:09:32 +0530

    Added solution 269

---
+++ Solutions/269.py
@@ -0,0 +1,66 @@
+"""
+Problem:
+
+You are given an string representing the initial conditions of some dominoes. Each
+element can take one of three values:
+
+L, meaning the domino has just been pushed to the left,
+R, meaning the domino has just been pushed to the right, or
+., meaning the domino is standing still.
+Determine the orientation of each tile when the dominoes stop falling. Note that if a
+domino receives a force from the left and right side simultaneously, it will remain
+upright.
+
+For example, given the string .L.R....L, you should return LL.RRRLLL.
+
+Given the string ..R...L.L, you should return ..RR.LLLL.
+"""
+
+
+def get_config_helper(dominos, length):
+    # creating and updating the new copy of dominos
+    has_been_updated = False
+    updated_dominos = dominos.copy()
+    for i in range(length):
+        if (
+            dominos[i] == "L"
+            and i > 0
+            and dominos[i - 1] == "."
+            and ((i > 1 and dominos[i - 2] != "R") or i <= 1)
+        ):
+            updated_dominos[i - 1] = "L"
+            has_been_updated = True
+        elif (
+            dominos[i] == "R"
+            and i < length - 1
+            and dominos[i + 1] == "."
+            and ((i < length - 2 and dominos[i + 2] != "L") or i >= length - 2)
+        ):
+            updated_dominos[i + 1] = "R"
+            has_been_updated = True
+    # if updation has been performed in the current iteration, the function is called
+    # recursively
+    if has_been_updated:
+        return get_config_helper(updated_dominos, length)
+    else:
+        return dominos
+
+
+def get_config(initial_state):
+    # breaking dominos into list as string is immutable
+    dominoes = list(initial_state)
+    return "".join(get_config_helper(dominoes, len(dominoes)))
+
+
+if __name__ == "__main__":
+    print(get_config(".L.R....L"))
+    print(get_config("..R...L.L"))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n ^ 2)
+SPACE COMPLEXITY: O(n ^ 2)
+[each iteration takes O(n) in both time & space and there can be n such iterations]
+"""
