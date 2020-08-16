ba22ee1d7a7a43217b6a0a734c6dd4cf345d8e2c
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-08-16 08:52:40 +0530

    Added solution 270 + DataStructures (Directed Weighted Graph + Min Priority Queue)

--- Solutions/DataStructures/Graph.py
+++ Solutions/DataStructures/Graph.py
@@ -94,3 +94,35 @@ class GraphUndirectedWeighted:
         self.add_node(node2)
         self.connections[node1][node2] = weight
         self.connections[node2][node1] = weight
+
+
+class GraphDirectedWeighted:
+    """
+    Graph Directed Weighted Class
+
+    Functions:
+    add_node: function to add a node in the graph
+    add_edge: function to add an edge between 2 nodes in the graph
+    """
+
+    def __init__(self) -> None:
+        self.connections = {}
+        self.nodes = 0
+
+    def __repr__(self) -> str:
+        return str(self.connections)
+
+    def __len__(self) -> int:
+        return self.nodes
+
+    def add_node(self, node: int) -> None:
+        # Add a node in the graph if it is not in the graph
+        if node not in self.connections:
+            self.connections[node] = {}
+            self.nodes += 1
+
+    def add_edge(self, node1: int, node2: int, weight: int) -> None:
+        # Add an edge between 2 nodes in the graph
+        self.add_node(node1)
+        self.add_node(node2)
+        self.connections[node1][node2] = weight
