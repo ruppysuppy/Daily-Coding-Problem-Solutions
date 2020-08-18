2c078ecf3f2bf76fa4134abd52d21814f3f571e2
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-08-18 08:56:29 +0530

    Added solution 272

---
+++ Solutions/272.py
@@ -0,0 +1,31 @@
+"""
+Problem:
+
+Write a function, throw_dice(N, faces, total), that determines how many ways it is
+possible to throw N dice with some number of faces each to get a specific total.
+
+For example, throw_dice(3, 6, 7) should equal 15.
+"""
+
+def throw_dice(N, faces, total, accumulator=0):
+    # base case for recursion
+    if N == 0 and total == 0:
+        return accumulator + 1
+    elif total < 0:
+        return accumulator
+    # dfs to calculate the answer
+    for i in range(1, faces + 1):
+        accumulator = throw_dice(N - 1, faces, total - i, accumulator)
+    return accumulator
+
+
+if __name__ == "__main__":
+    print(throw_dice(3, 6, 7))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(faces ^ log(total))
+SPACE COMPLEXITY: O(total) [call stack included]
+"""
