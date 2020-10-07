e96844b71e9653fe01124161ee6afba0db4f908d
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-10-07 08:41:16 +0530

    Added solution 322

---
+++ Solutions/322.py
@@ -0,0 +1,46 @@
+"""
+Problem:
+
+Starting from 0 on a number line, you would like to make a series of jumps that lead
+to the integer N.
+
+On the ith jump, you may move exactly i places to the left or right.
+
+Find a path with the fewest number of jumps required to get from 0 to N.
+"""
+
+
+def get_sum(x):
+    # Utitlity function to calculate sum of numbers from 1 to x
+    return (x * (x + 1)) // 2
+
+
+def count_jumps(n):
+    # getting the absolute value, answer will be same either it is positive or negative
+    n = abs(n)
+    ans = 0
+    # Continue till number is lesser or not in same parity
+    while get_sum(ans) < n or (get_sum(ans) - n) & 1:
+        ans += 1
+    return ans
+
+
+if __name__ == "__main__":
+    print(count_jumps(-3))
+    print(count_jumps(0))
+    print(count_jumps(1))
+    print(count_jumps(2))
+    print(count_jumps(3))
+    print(count_jumps(4))
+    print(count_jumps(5))
+    print(count_jumps(9))
+    print(count_jumps(10))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(1)
+"""
+
