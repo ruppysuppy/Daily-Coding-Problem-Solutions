398a8519cdb43281b0f9823c5e4cd573f816a2ef
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-10-23 08:55:51 +0530

    Added solution 338

---
+++ Solutions/338.py
@@ -0,0 +1,34 @@
+"""
+Problem:
+
+Given an integer n, find the next biggest integer with the same number of 1-bits on.
+For example, given the number 6 (0110 in binary), return 9 (1001).
+"""
+
+
+def get_set_bits(num: int) -> int:
+    # get the number of bits set in a number [runs in O(log(n))]
+    bin_num = bin(num)[2:]
+    return sum([int(digit) for digit in bin_num])
+
+
+def get_next_int(num: int) -> int:
+    # function to get the next number with same number of set bits
+    num_of_set_bits = get_set_bits(num)
+    curr = num + 1
+    while True:
+        if num_of_set_bits == get_set_bits(curr):
+            return curr
+        curr += 1
+
+
+if __name__ == "__main__":
+    print(get_next_int(6))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n) [as the result always lies between n and 2n]
+SPACE COMPLEXITY: O(log(n))
+"""
