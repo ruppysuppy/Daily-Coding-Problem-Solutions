4ce2680d9603a606213d7f70f92fde4d19540e10
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-10-25 09:05:08 +0530

    Added solution 340

---
+++ Solutions/340.py
@@ -0,0 +1,47 @@
+"""
+Problem:
+
+Given a set of points (x, y) on a 2D cartesian plane, find the two closest points. For
+example, given the points [(1, 1), (-1, -1), (3, 4), (6, 1), (-1, -6), (-4, -3)],
+return [(-1, -1), (1, 1)].
+"""
+
+from math import sqrt
+from sys import maxsize
+from typing import List, Tuple
+
+
+def get_distance(pt1: Tuple[int, int], pt2: Tuple[int, int]) -> float:
+    # helper function to get the distance between 2 points
+    x1, y1 = pt1
+    x2, y2 = pt2
+    dist = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2))
+    return dist
+
+
+def get_nearest_points(pts_arr: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
+    # function to get the nearest 2 points
+    length = len(pts_arr)
+    dist = maxsize
+    pt1, pt2 = None, None
+    # checking for all combinations
+    for index_1 in range(length):
+        for index_2 in range(index_1 + 1, length):
+            pt1_temp, pt2_temp = pts_arr[index_1], pts_arr[index_2]
+            dist_temp = get_distance(pt1_temp, pt2_temp)
+            if dist_temp < dist:
+                dist = dist_temp
+                pt1, pt2 = pt1_temp, pt2_temp
+    return [pt1, pt2]
+
+
+if __name__ == "__main__":
+    print(get_nearest_points([(1, 1), (-1, -1), (3, 4), (6, 1), (-1, -6), (-4, -3)]))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n ^ 2)
+SPACE COMPLEXITY: O(1)
+"""
