405f4f708863f7768298419838e81f05560ded0f
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-11-01 09:49:40 +0530

    Refactored solution 28

--- Solutions/028.py
+++ Solutions/028.py
@@ -1,120 +1,104 @@
 """
 Problem:
 
-Write an algorithm to justify text. 
-Given a sequence of words and an integer line length k, return a list of strings which represents each line, fully justified.
-More specifically, you should have as many words as possible in each line. There should be at least one space between each word. 
-Pad extra spaces when necessary so that each line has exactly length k. 
-Spaces should be distributed as equally as possible, with the extra spaces, if any, distributed starting from the left.
-If you can only fit one word on a line, then you should pad the right-hand side with spaces.
-Each word is guaranteed not to be longer than k
+Write an algorithm to justify text. Given a sequence of words and an integer line
+length k, return a list of strings which represents each line, fully justified.
 
-Example:
-Input = ["the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"],16
-Output =    ["the  quick brown", # 1 extra space on the left
-            "fox  jumps  over", # 2 extra spaces distributed evenly
-            "the   lazy   dog"] # 4 extra spaces distributed evenly
+More specifically, you should have as many words as possible in each line. There should
+be at least one space between each word. Pad extra spaces when necessary so that each
+line has exactly length k. Spaces should be distributed as equally as possible, with
+the extra spaces, if any, distributed starting from the left.
+
+If you can only fit one word on a line, then you should pad the right-hand side with
+spaces.
+
+Each word is guaranteed not to be longer than k.
+
+For example, given the list of words
+["the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"] and k = 16, you
+should return the following:
+
+["the quick brown", # 1 extra space on the left "fox jumps over", # 2 extra spaces
+distributed evenly "the lazy dog" # 4 extra spaces distributed evenly]
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def justify(word_list, k):
-    # Declaring necessary variables:
-    # res: stores the result
-    # temp: stores the words per line
-    # length: total length for the line (1 space included)
-    res = []
+from typing import List
+
+
+def add_word_handler(
+    result: List[str], temp: List[str], curr_string_length: int, k: int
+) -> None:
+    # helper function to handle adding a new word to the result
+    extra_spaces = k - curr_string_length
+    words_in_current_line = len(temp)
+    if words_in_current_line == 1:
+        # only 1 word is present, extra padding is added to the right
+        word = temp.pop()
+        result.append(word + " " * extra_spaces)
+    elif extra_spaces % (words_in_current_line - 1) == 0:
+        # space can be equally distributed
+        full_string = (" " * (extra_spaces // (words_in_current_line - 1) + 1)).join(
+            temp
+        )
+        result.append(full_string)
+    else:
+        # the space cannot be equally distributed
+        # extra spaces are added betweens the words, starting from the left
+        extra_uneven_spaces = extra_spaces % (words_in_current_line - 1)
+        regular = extra_spaces // (words_in_current_line - 1) + 1
+        temp_str = ""
+        for i in temp:
+            temp_str += i + " " * regular
+            if extra_uneven_spaces:
+                temp_str += " "
+                extra_uneven_spaces -= 1
+        result.append(temp_str.rstrip())
+
+
+def justify_text(word_list: List[str], k: int) -> List[str]:
+    # function to justify text
+    result = []
     temp = []
-    length = 0
-
-    # Looping over the list of words
-    for i in word_list:
-        # if no words have been added to temp
+    curr_string_length = 0
+    # iterating through the given words
+    for word in word_list:
+        curr_word_length = len(word)
         if temp == []:
-            # if the word is longer than k, no justification is possible
-            if len(i) > k:
-                return [None]
-            # else the word is added to temp and length updated
-            else:
-                temp.append(i)
-                length = len(i)
-
-        # if after the adding the current word, the length is within the limit, its added to temp and length updated
-        elif len(i) + length + 1 <= k:
-            temp.append(i)
-            length += len(i) + 1
-
-        # if after the adding the current word, the length oversteps the limit
-        elif len(i) + length + 1 > k:
-            # if the word is longer than k, no justification is possible
-            if len(i) + 1 > k:
-                return [None]
-            # Adding the final result to res
-            else:
-                # Extra spaces left
-                extra = k - length
-
-                # if only 1 word is present, extra padding is added to the right
-                if len(temp) == 1:
-                    res.append(temp[0] + " " * extra)
-
-                # if the space can be equally distributed, it is done so
-                elif extra % (len(temp) - 1) == 0:
-                    res.append((" " * (extra // (len(temp) - 1) + 1)).join(temp))
-
-                # if the space cannot be equally distributed, extra spaces are added betweens the words, starting from the left
-                else:
-                    extra_extra = extra % (len(temp) - 1)
-                    regular = extra // (len(temp) - 1) + 1
-
-                    temp_str = ""
-
-                    for i in temp:
-                        temp_str += i + " " * regular
-
-                        if extra_extra:
-                            temp_str += " "
-                            extra_extra -= 1
-
-                    res.append(temp_str.rstrip())
-
-                # updating temp and length
-                temp = [i]
-                length = len(i)
-
-    # operation after the iteration is complete (to add the last set of word(s))
-    if temp != []:
-        # Extra spaces left
-        extra = k - length
-
-        if len(temp) == 1:
-            res.append(temp[0] + " " * extra)
-
-        # if the space can be equally distributed, it is done so
-        elif extra % (len(temp) - 1) == 0:
-            res.append((" " * (extra // (len(temp) - 1) + 1)).join(temp))
-
-        # if the space cannot be equally distributed, extra spaces are added betweens the words, starting from the left
+            # no word added to the current string
+            temp.append(word)
+            curr_string_length = curr_word_length
+        elif curr_word_length + curr_string_length + 1 <= k:
+            # adding the current word doesn't cause overflow
+            temp.append(word)
+            curr_string_length += curr_word_length + 1
         else:
-            extra_extra = extra % (len(temp) - 1)
-            regular = extra // (len(temp) - 1) + 1
-
-            temp_str = ""
-
-            for i in temp:
-                temp_str += i + " " * regular
-
-                if extra_extra:
-                    temp_str += " "
-                    extra_extra -= 1
-
-            res.append(temp_str.rstrip())
-
-    return res
+            # adding the current word does cause overflow
+            add_word_handler(result, temp, curr_string_length, k)
+            # updating temp and length (only in case of overflow)
+            temp = [word]
+            curr_string_length = len(word)
+    if temp != []:
+        # if the last line caused an overflow
+        add_word_handler(result, temp, curr_string_length, k)
+    return result
 
 
-# DRIVER CODE
-for i in justify(
-    ["the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog", "Done"], 16
-):
-    # NOTE: Using the "'"s is not important, used it to denote thestart and end of the string (helpful in case of 1 word in 1 line)
-    print("'" + i + "'")
+if __name__ == "__main__":
+    for string in justify_text(
+        ["the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"], 16
+    ):
+        print("'" + string + "'")
+
+    for string in justify_text(["done"], 16):
+        print("'" + string + "'")
+
+    # NOTE: Using the "'"s is not important, used it to denote the start and end of the
+    # string (helpful in case of 1 word in 1 line)
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(n)
+"""
