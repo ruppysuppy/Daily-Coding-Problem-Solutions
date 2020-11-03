3a9e736e413c15969ad480947da790d507ce36c2
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-11-03 09:06:37 +0530

    Refactored solution 29

--- Solutions/029.py
+++ Solutions/029.py
@@ -1,73 +1,51 @@
 """
 Problem:
 
-Run-length encoding is a fast and simple method of encoding strings. 
-The basic idea is to represent repeated successive characters as a single count and character. 
-Implement run-length encoding and decoding. 
-You can assume the string to be encoded have no digits and consists solely of alphabetic characters. 
-You can assume the string to be decoded is valid.
+Run-length encoding is a fast and simple method of encoding strings. The basic idea is
+to represent repeated successive characters as a single count and character. For
+example, the string "AAAABBBCCDAA" would be encoded as "4A3B2C1D2A".
 
-Example:
-
-ENCODE:
-Input = "AAAABBBCCDAA"
-Output = "4A3B2C1D2A"
-
-DECODE:
-Input = "4A3B2C1D2A"
-Output = "AAAABBBCCDAA"
+Implement run-length encoding and decoding. You can assume the string to be encoded
+have no digits and consists solely of alphabetic characters. You can assume the string
+to be decoded is valid.
 """
 
-# FUNCTION TO PERFORM THE OPERATION (Encode)
-def encode(string):
-    # Declaring necessary variables
-    # ans: stores the encoded string
-    # temp: stores the current charcter
-    # count: stores the count of the current charcter
-    ans = ""
-    temp = string[0]
-    count = 0
 
-    # Looping over the string
-    for i in string:
-        # if the current charcter differs from the character in temp, count and temp is added to the answer
-        if i != temp:
-            ans += str(count) + temp
-            temp = i
+def encode(string: str) -> str:
+    # function to encode a string using run-length encoding
+    # Time Complexity: O(n), Space Complexity: O(1)
+    if not string:
+        return ""
+    encoded_string = ""
+    prev_char = string[0]
+    count = 0
+    # generating the encoded string
+    for char in string:
+        if char != prev_char:
+            encoded_string += str(count) + prev_char
+            prev_char = char
             count = 1
-        # if the current charcter is same as temp, count is incremented
         else:
             count += 1
-
-    # Adding the last character and count
-    ans += str(count) + temp
-    temp = i
-    count = 1
-
-    return ans
+    encoded_string += str(count) + prev_char
+    return encoded_string
 
 
-# FUNCTION TO PERFORM THE OPERATION (Decode)
-def decode(string):
-    # Declaring necessary variables
-    # ans: stores the encoded string
-    # temp: stores the count of the current charcter
-    ans = ""
-    temp = 0
-
-    # Looping over the string
-    for i in string:
-        # if the charcter is a digit, its added to temp at the units place
-        if i.isdigit():
-            temp = temp * 10 + int(i)
-        # else the result is addedc to the decoded string
+def decode(string: str) -> str:
+    # function to decode a string encoded with run-length encoding
+    # Time Complexity: O(n), Space Complexity: O(1)
+    decoded_string = ""
+    char_frequency = 0
+    # generating the decoded string
+    for char in string:
+        if char.isdigit():
+            char_frequency = char_frequency * 10 + int(char)
         else:
-            ans += i * temp
-            temp = 0
+            decoded_string += char * char_frequency
+            char_frequency = 0
+    return decoded_string
 
-    return ans
 
-
-# DRIVER CODE
-print(encode("AAAABBBCCDAA"))
-print(decode("4A3B2C1D2A"))
+if __name__ == "__main__":
+    print(encode("AAAABBBCCDAA"))
+    print(decode("4A3B2C1D2A"))
