6eff2e8bb4711a607e53c536cf195dc5a88ecb9a
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-11-09 09:19:44 +0530

    Refactored solution 31

--- Solutions/031.py
+++ Solutions/031.py
@@ -1,38 +1,51 @@
 """
 Problem:
 
-The edit distance between two strings refers to the minimum number of character insertions, deletions, and substitutions required to change one string to the other.
+The edit distance between two strings refers to the minimum number of character
+insertions, deletions, and substitutions required to change one string to the other.
+For example, the edit distance between "kitten" and "sitting" is three: substitute the
+"k" for "s", substitute the "e" for "i", and append a "g".
+
 Given two strings, compute the edit distance between them.
-
-Example:
-
-Input = “kitten”, “sitting”
-Output = 3
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def string_distance(str1, str2):
-    # if the strings are same, the distance between the strings is 0 [BASE CASE FOR RECURSION]
-    if str1 == str2:
-        return 0
-    # if str1 is an empty string, distance is length of str2 (len(str2) characters have to be inserted) [BASE CASE FOR RECURSION]
-    elif not str1:
-        return len(str2)
-    # if str2 is an empty string, distance is length of str1 (len(str1) characters have to be inserted) [BASE CASE FOR RECURSION]
-    elif not str2:
-        return len(str1)
 
-    # if the 1st character are the same for both strings
-    if str1[0] == str2[0]:
-        return string_distance(str1[1:], str2[1:])
+def string_distance(str1: str, str2: str) -> int:
+    # function to get the distance between 2 strings
+    n = len(str1)
+    m = len(str2)
+    dp = [[0 for x in range(m + 1)] for x in range(n + 1)]
+    # generating the look-up table in bottom up manner
+    for i in range(n + 1):
+        for j in range(m + 1):
+            # str1 empty (insert all characters of second string)
+            if i == 0:
+                dp[i][j] = j
+            # str2 empty (remove all characters of second string)
+            elif j == 0:
+                dp[i][j] = i
+            # last characters are same (ignore last char and recurse for remaining
+            # string)
+            elif str1[i - 1] == str2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            # last character are different (consider all possibilities and select the
+            # minimum)
+            else:
+                dp[i][j] = 1 + min(
+                    dp[i][j - 1],       # insertion
+                    dp[i - 1][j],       # deletion
+                    dp[i - 1][j - 1],   # replacement
+                )
+    return dp[n][m]
 
-    # if the 1st character are different, we choose the minimum distance for 1st character deletion, addition and modifying the charcter + 1 (due to the change in the character)
-    return 1 + min(
-        string_distance(str1[1:], str2),  # deletion from str1
-        string_distance(str1, str2[1:]),  # addition to str1
-        string_distance(str1[1:], str2[1:]),
-    )  # modification to str1
+
+if __name__ == "__main__":
+    print(string_distance("kitten", "sitting"))
 
 
-# DRIVER CODE
-print(string_distance("kitten", "sitting"))
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n x m)
+SPACE COMPLEXITY: O(n x m)
+"""
