0e849c24a65bcfa2914048b571f5113201c5fdee
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-11-11 08:35:24 +0530

    Added solution 357

---
+++ Solutions/357.py
@@ -0,0 +1,46 @@
+"""
+Problem:
+
+You are given a binary tree in a peculiar string representation. Each node is written
+in the form (lr), where l corresponds to the left child and r corresponds to the right
+child.
+
+If either l or r is null, it will be represented as a zero. Otherwise, it will be
+represented by a new (lr) pair.
+
+Here are a few examples:
+
+A root node with no children: (00)
+A root node with two children: ((00)(00))
+An unbalanced tree with three consecutive left children: ((((00)0)0)0)
+Given this representation, determine the depth of the tree.
+"""
+
+
+def get_depth(tree_repr: str) -> int:
+    # function to get the depth of a tree from its string representation
+    depth = 0
+    max_depth = 0
+    for char in tree_repr:
+        if char == "(":
+            # entering a node (depth addition)
+            depth += 1
+        elif char == ")":
+            # exiting a node (depth subtraction)
+            depth -= 1
+        max_depth = max(max_depth, depth)
+    return max_depth
+
+
+if __name__ == "__main__":
+    print(get_depth("(00)"))
+    print(get_depth("((00)(00))"))
+    print(get_depth("((((00)0)0)0)"))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(1)
+"""
