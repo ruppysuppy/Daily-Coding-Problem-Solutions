924f0a06ec35a94b0242c9941904e7ec311178f0
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-11-17 09:30:21 +0530

    Refactored solution 36

--- Solutions/036.py
+++ Solutions/036.py
@@ -4,56 +4,48 @@ Problem:
 Given the root to a binary search tree, find the second largest node in the tree.
 """
 
-# Local Import from the datastructure module
-from DataStructures.Tree import Node, Binary_Search_Tree
+from typing import Optional, Tuple
 
-# Helper function to get the largest node and parent
-def get_largest(node):
-    # if there is no node, None is returned
-    if node == None:
-        return None
+from DataStructures.Tree import Node, BinarySearchTree
 
-    # parent is set to None (if the root is the largest node, parent is returned as None)
+
+def get_largest_pair_from_current_node(node: Node) -> Tuple[Optional[Node], Node]:
     parent = None
-
-    # getting the right-most node (largest node as its a Binary Search Tree)
     while node.right:
         parent = node
         node = node.right
-
     # both the parent and the node is returned
-    return (parent, node)
+    return parent, node
 
 
-# FUNCTION TO PERFORM THE OPERATION
-def second_largest(tree):
-    # if the tree is empty, None is returned
-    if tree.root == None:
+def get_second_largest(tree: BinarySearchTree) -> Optional[int]:
+    if tree.root is None:
         return None
 
-    # finding the largest node
-    largest_parent, largest = get_largest(tree.root)
-
-    # if the largest node has a left node, the largest child of the left node is the 2nd largest node (BST property)
+    parent_of_largest, largest = get_largest_pair_from_current_node(tree.root)
+    # if the largest node has a left node, the largest child of the left node is the
+    # 2nd largest node (BST property)
     if largest.left:
-        return (get_largest(largest.left)[1]).val
-
-    # if the left node is absent and so is the parent (the tree contains only 1 node), None is returned
-    elif largest_parent == None:
+        _, second_largest_node = get_largest_pair_from_current_node(largest.left)
+        return second_largest_node.val
+    # if the left node and the parent is absent (the tree contains only 1 node),
+    # None is returned
+    elif parent_of_largest is None:
         return None
+    # if the largest parent is present its the 2nd largest node if no left node is
+    # present (BST property)
+    return parent_of_largest.val
 
-    # if the largest parent is present its the 2nd largest node if no left node is present (BST property)
-    return (largest_parent).val
 
+if __name__ == "__main__":
+    tree = BinarySearchTree()
 
-# DRIVER CODE
-tree = Binary_Search_Tree()
-tree.add(5)
-tree.add(3)
-tree.add(8)
-tree.add(2)
-tree.add(4)
-tree.add(7)
-tree.add(9)
+    tree.add(5)
+    tree.add(3)
+    tree.add(8)
+    tree.add(2)
+    tree.add(4)
+    tree.add(7)
+    tree.add(9)
 
-print(second_largest(tree))
+    print(get_second_largest(tree))
