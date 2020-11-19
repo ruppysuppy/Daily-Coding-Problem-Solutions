58bb37714c96293d45911a1ba9a49d86658d17bd
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-11-19 09:00:55 +0530

    Refactored solution 38

--- Solutions/038.py
+++ Solutions/038.py
@@ -1,48 +1,50 @@
-# NOTE: This solution is given by the Daily Coding Problem
-# This is a Classic Backtracking Problem, it has several solution methodologies but all use backtracking
-
 """
 Problem:
 
-You have an N by N board. 
-Write a function that, given N, returns the number of possible arrangements of the board where N queens can be placed on the board without threatening each other.
-That is, no two queens share the same row, column, or diagonal.
-
-Example:
-
-Input = 4
-Output = 2
+You have an N by N board. Write a function that, given N, returns the number of
+possible arrangements of the board where N queens can be placed on the board without
+threatening each other, i.e. no two queens share the same row, column, or diagonal.
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def n_queens(n, board=[]):
-    if n == len(board):
+from typing import List
+
+
+def n_queens(n: int, queen_positions: List[int] = []) -> int:
+    # N Queen solution using backtracking
+    if n == len(queen_positions):
         return 1
 
     count = 0
-
     for col in range(n):
-        board.append(col)
-
-        if is_valid(board):
-            count += n_queens(n, board)
-        board.pop()
-
+        queen_positions.append(col)
+        if is_valid(queen_positions):
+            count += n_queens(n, queen_positions)
+        queen_positions.pop()
     return count
 
 
-# Helper function to check any queens can attack the last queen.
-def is_valid(board):
-    current_queen_row, current_queen_col = len(board) - 1, board[-1]
-
-    for row, col in enumerate(board[:-1]):
+def is_valid(queen_positions: List[int]) -> bool:
+    # check to see if any queen is threatening the current queen
+    current_queen_row, current_queen_col = len(queen_positions) - 1, queen_positions[-1]
+    for row, col in enumerate(queen_positions[:-1]):
         diff = abs(current_queen_col - col)
-
-        if diff == 0 or diff == current_queen_row - row:
+        if (
+            diff == 0                           # same row
+            or diff == current_queen_row - row  # same diagonal
+        ):
             return False
-
     return True
 
 
-# DRIVER CODE
-print(n_queens(4))
+if __name__ == "__main__":
+    print(n_queens(1))
+    print(n_queens(4))
+    print(n_queens(5))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(2 ^ n)
+SPACE COMPLEXITY: O(n)
+"""
