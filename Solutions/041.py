ef21795bb217a793b0e172868d49ccf4c8bfdab6
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-11-20 09:28:24 +0530

    Refactored solution 41 & 40

--- Solutions/041.py
+++ Solutions/041.py
@@ -1,52 +1,65 @@
 """
 Problem:
 
-Given an unordered list of flights taken by someone, each represented as (origin, destination) pairs, and a starting airport, compute the person's itinerary. 
-If no such itinerary exists, return null. If there are multiple possible itineraries, return the lexicographically smallest one. 
-All flights must be used in the itinerary.
+Given an unordered list of flights taken by someone, each represented as
+(origin, destination) pairs, and a starting airport, compute the person's itinerary. If
+no such itinerary exists, return null. If there are multiple possible itineraries,
+return the lexicographically smallest one. All flights must be used in the itinerary.
 
-Example:
+For example, given the list of flights
+[('SFO', 'HKO'), ('YYZ', 'SFO'), ('YUL', 'YYZ'), ('HKO', 'ORD')] and starting airport
+'YUL', you should return the list ['YUL', 'YYZ', 'SFO', 'HKO', 'ORD'].
 
-[('SFO', 'HKO'), ('YYZ', 'SFO'), ('YUL', 'YYZ'), ('HKO', 'ORD')], 'YUL' => ['YUL', 'YYZ', 'SFO', 'HKO', 'ORD']
-[('SFO', 'COM'), ('COM', 'YYZ')], 'COM' => None
-[('A', 'B'), ('A', 'C'), ('B', 'C'), ('C', 'A')], 'A' => ['A', 'B', 'C', 'A', 'C'] 
+Given the list of flights [('SFO', 'COM'), ('COM', 'YYZ')] and starting airport 'COM',
+you should return null.
+
+Given the list of flights [('A', 'B'), ('A', 'C'), ('B', 'C'), ('C', 'A')] and starting
+airport 'A', you should return the list ['A', 'B', 'C', 'A', 'C'] even though
+['A', 'C', 'A', 'B', 'C'] is also a valid itinerary. However, the first one is
+lexicographically smaller.
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def get_itinerary(flights, starting_point, current_itinerary=[]):
-    # Base case of recursion (all flights except the final destination has been added)
+from typing import List, Optional, Tuple
+
+
+def get_itinerary(
+    flights: List[Tuple[str, str]],
+    current_position: str,
+    current_itinerary: List[str] = [],
+) -> Optional[List[str]]:
     if not flights and current_itinerary:
-        return current_itinerary + [starting_point]
-    # if no flights are provided, None is returned
+        return current_itinerary + [current_position]
     elif not flights:
         return None
 
-    # resulatant_itinerary stores the final result
     resulatant_itinerary = None
-
-    # iterating over the flight list
+    # generating the itinerary
     for index, (src, dest) in enumerate(flights):
-        # if the starting point is found the iternary using that destination is calculated (DFS)
-        if starting_point == src:
+        # the is constructed using the current position (using DFS)
+        if current_position == src:
             child_itinerary = get_itinerary(
                 flights[:index] + flights[index + 1 :], dest, current_itinerary + [src]
             )
-
-            # if the child_itinerary is a vaild itinerary and lexicographically smaller, the resulatant_itinerary is updated
             if child_itinerary and (
-                not resulatant_itinerary
-                or ("".join(child_itinerary) < "".join(resulatant_itinerary))
+                not resulatant_itinerary or child_itinerary < resulatant_itinerary
             ):
                 resulatant_itinerary = child_itinerary
-
     return resulatant_itinerary
 
 
-# DRIVER CODE
-print(
-    get_itinerary(
-        [("SFO", "HKO"), ("YYZ", "SFO"), ("YUL", "YYZ"), ("HKO", "ORD")], "YUL"
+if __name__ == "__main__":
+    print(
+        get_itinerary(
+            [("SFO", "HKO"), ("YYZ", "SFO"), ("YUL", "YYZ"), ("HKO", "ORD")], "YUL"
+        )
     )
-)
-print(get_itinerary([("SFO", "COM"), ("COM", "YYZ")], "COM"))
-print(get_itinerary([("A", "B"), ("A", "C"), ("B", "C"), ("C", "A")], "A"))
+    print(get_itinerary([("SFO", "COM"), ("COM", "YYZ")], "COM"))
+    print(get_itinerary([("A", "B"), ("A", "C"), ("B", "C"), ("C", "A")], "A"))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n ^ 3)
+SPACE COMPLEXITY: O(n ^ 2)
+"""
