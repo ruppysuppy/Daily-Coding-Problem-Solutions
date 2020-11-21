0741cf3a352b3d20baca3159073d922197766458
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-11-21 10:11:19 +0530

    Refactored solution 48

--- Solutions/048.py
+++ Solutions/048.py
@@ -3,8 +3,14 @@ Problem:
 
 Given pre-order and in-order traversals of a binary tree, write a function to reconstruct the tree.
 
-Example:
-[a, b, d, e, c, f, g] (preorder), [d, b, e, a, f, c, g] (inorder) =>
+For example, given the following preorder traversal:
+
+[a, b, d, e, c, f, g]
+And the following inorder traversal:
+
+[d, b, e, a, f, c, g]
+You should return the following tree:
+
     a
    / \
   b   c
@@ -12,52 +18,51 @@ Example:
 d  e f  g
 """
 
-# Local Import from the datastructure module
-from DataStructures.Tree import Binary_Tree, Node
+from typing import List
 
-# FUNCTION TO PERFORM THE OPERATION
-def tree_gen(preorder, inorder):
-    # Getting the number of nodes
+from DataStructures.Tree import BinaryTree, Node
+
+
+def generate_tree(preorder: List[int], inorder: List[int]) -> BinaryTree:
     length = len(preorder)
-
-    # if the preorder and inorder expressions has different number of nodes RuntimeError is raised
     if length != len(inorder):
         raise RuntimeError
-
-    # if there are no nodes, an empty tree is returned
     if length == 0:
-        return Binary_Tree(None)
-
-    # starting with the root
+        return BinaryTree()
+    # generating the root
     root = preorder[0]
-    tree = Binary_Tree(root)
-
-    # if there are more nodes, the function is called recursively
+    tree = BinaryTree()
+    tree.root = Node(root)
+    # generating the rest of the tree
     if length > 1:
-        # finding the index of the root in the inorder expression
-        ind = inorder.index(root)
-
+        i = inorder.index(root)
         # partitioning the nodes as per the branch
-        temp_left = (inorder[:ind], preorder[1 : ind + 1])
-        temp_right = (inorder[ind + 1 :], preorder[ind + 1 :])
-
+        inorder_left, preorder_left = (inorder[:i], preorder[1 : i + 1])
+        inorder_right, preorder_right = (inorder[i + 1 :], preorder[i + 1 :])
         # creating a tree for each branch
-        tree_left = tree_gen(temp_left[1], temp_left[0])
-        tree_right = tree_gen(temp_right[1], temp_right[0])
-
+        tree_left = generate_tree(preorder_left, inorder_left)
+        tree_right = generate_tree(preorder_right, inorder_right)
         # attaching the sub-tree to their respective branch
         tree.root.left = tree_left.root
         tree.root.right = tree_right.root
-
-    # the final tree is returned
     return tree
 
 
-# DRIVER CODE
-test1 = tree_gen(
-    ["a", "b", "d", "e", "c", "f", "g"], ["d", "b", "e", "a", "f", "c", "g"]
-)
-print(test1)
+if __name__ == "__main__":
+    test1 = generate_tree(
+        ["a", "b", "d", "e", "c", "f", "g"], ["d", "b", "e", "a", "f", "c", "g"]
+    )
+    print(test1)
 
-test2 = tree_gen(["a", "b", "d", "e", "c", "f"], ["d", "b", "e", "a", "f", "c"])
-print(test2)
+    test2 = generate_tree(
+        ["a", "b", "d", "e", "c", "f"], ["d", "b", "e", "a", "f", "c"]
+    )
+    print(test2)
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n ^ 2)
+SPACE COMPLEXITY: O(n)
+"""
