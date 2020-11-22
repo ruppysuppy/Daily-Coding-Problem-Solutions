2484a1e0735eefc1a03ecff4ba44fd6aa1b91fb2
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-11-22 09:57:42 +0530

    Refactored solution 52

--- Solutions/052.py
+++ Solutions/052.py
@@ -1,100 +1,106 @@
 """
 Problem:
 
-This problem was asked by Google.
-Implement an LRU (Least Recently Used) cache. It should be able to be initialized with a cache size n, and contain the following methods:
-* set(key, value): sets key to value. 
-If there are already n items in the cache and we are adding a new item, then it should also remove the least recently used item.
-* get(key): gets the value at key. If no such key exists, return null.
+Implement an LRU (Least Recently Used) cache. It should be able to be initialized with
+a cache size n, and contain the following methods:
 
-Each operation should run in O(1) time.
+set(key, value): sets key to value. If there are already n items in the cache and we
+are adding a new item, then it should also remove the least recently used item.
+get(key): gets the value at key. If no such key exists, return null. Each operation
+should run in O(1) time.
 """
 
-# Node class for Double Linked List
-class Node:
-    def __init__(self, key, val):
+from __future__ import annotations
+from typing import Optional
+
+
+class DoubleLinkedListNode:
+    def __init__(self, key: int, val: int) -> None:
         self.key = key
         self.val = val
-        self.last = None
         self.next = None
+        self.prev = None
 
 
-# LRU Cache class
-class LRU_Cache:
-    # initialize method
-    def __init__(self, size):
-        # head and rear store the head and the rear of the double linked list
-        self.head = Node(None, None)
-        self.rear = Node(None, None)
-        self.head.next = self.rear
-        self.rear.last = self.head
-        # cache stores the keys mapped to the nodes of the double linked list
+class DoubleLinkedList:
+    def __init__(self) -> None:
+        self.head = DoubleLinkedListNode(None, None)
+        self.rear = DoubleLinkedListNode(None, None)
+        self.head.next, self.rear.prev = self.rear, self.head
+
+    def add_node(self, node: DoubleLinkedListNode) -> None:
+        temp = self.rear.prev
+        temp.next, node.prev = node, temp
+        self.rear.prev, node.next = node, self.rear
+
+    def remove_node(self, node: DoubleLinkedListNode) -> DoubleLinkedListNode:
+        temp_last, temp_next = node.prev, node.next
+        node.prev, node.next = None, None
+        temp_last.next, temp_next.prev = temp_next, temp_last
+        return node
+
+
+class LRUCache:
+    def __init__(self, capacity: int) -> None:
+        self.list = DoubleLinkedList()
+        self.capacity = capacity
+        self.num_keys = 0
+        self.hits = 0
+        self.miss = 0
         self.cache = {}
-        # size stores the maximum number of key value pairs the cache can hold
-        self.size = size
-        # elements stores the number of elements in the cache
-        self.elements = 0
 
-    # helper function to add node to the double linked list (rear holds the most recently used node)
-    def add(self, node):
-        node.next = self.rear
-        node.last = self.rear.last
-        self.rear.last.next = node
-        self.rear.last = node
+    def __repr__(self) -> str:
+        return (
+            f"CacheInfo(hits={self.hits}, misses={self.miss}, "
+            f"capacity={self.capacity}, current size={self.num_keys})"
+        )
 
-    # helper function to remove node to the double linked list (not deleted, but connection broken)
-    def remove(self, node):
-        node.last.next = node.next
-        node.next.last = node.last
-        node.next = None
-        node.last = None
+    def get(self, key: int) -> Optional[int]:
+        if key in self.cache:
+            self.hits += 1
+            # placing the node at the proper position in the linked list in case of
+            # cache hit
+            self.list.add_node(self.list.remove_node(self.cache[key]))
+            return self.cache[key].val
+        self.miss += 1
+        return None
 
-    # get: finds the value mapped to the key, returns None if key not present
-    def get(self, key):
-        # key not present
+    def set(self, key: int, value: int) -> None:
         if key not in self.cache:
-            return None
-
-        # the node pointed by the key is pushed to the end (most recently used)
-        node = self.cache[key]
-        self.remove(node)
-        self.add(node)
-
-        # the value of the node is returned
-        return node.val
-
-    # set: takes a key value pair and stores them
-    def set(self, key, val):
-        # if the cache is full, the least recently used key value pair (at the head of the linked list) is deleted
-        if self.size == self.elements:
-            node = self.head.next
-            self.remove(node)
-            self.elements -= 1
-            del self.cache[node.key]
-
-        # adding the new node to the linked list and cache and incrementing elements
-        node = Node(key, val)
-        self.add(node)
-        self.elements += 1
-        self.cache[key] = node
+            if self.num_keys >= self.capacity:
+                # replacement algorithm in case the cache is full
+                key_to_delete = self.list.head.next.key
+                self.list.remove_node(self.cache[key_to_delete])
+                del self.cache[key_to_delete]
+                self.num_keys -= 1
+            self.cache[key] = DoubleLinkedListNode(key, value)
+            self.list.add_node(self.cache[key])
+            self.num_keys += 1
+            return
+        # if the key is already present, its value is updated
+        node = self.list.remove_node(self.cache[key])
+        node.val = value
+        self.list.add_node(node)
 
 
-# DRIVER CODE
-cache = LRU_Cache(3)
+if __name__ == "__main__":
+    cache = LRUCache(3)
 
-print(cache.get("a"))
+    print(cache.get("a"))
 
-cache.set("a", 1)
-cache.set("b", 2)
-cache.set("c", 3)
+    cache.set("a", 1)
+    cache.set("b", 2)
+    cache.set("c", 3)
 
-print(cache.get("a"))
+    print(cache.get("a"))
 
-cache.set("d", 4)
-cache.set("e", 5)
+    cache.set("d", 4)
+    cache.set("e", 5)
 
-print(cache.get("a"))
-print(cache.get("b"))
-print(cache.get("c"))
-print(cache.get("d"))
-print(cache.get("e"))
+    print(cache.get("a"))
+    print(cache.get("b"))
+    print(cache.get("c"))
+    print(cache.get("d"))
+    print(cache.get("e"))
+
+    print(cache)
