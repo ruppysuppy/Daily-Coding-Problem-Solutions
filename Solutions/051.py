310308f6e551eee166d23797454197f36517bde4
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-11-22 09:33:10 +0530

    Refactored solution 51

--- Solutions/051.py
+++ Solutions/051.py
@@ -1,37 +1,42 @@
 """
 Problem:
 
-You have a function that generates perfectly random numbers between 1 and k (inclusive), where k is an input. 
-Write a function that shuffles a deck of cards represented as an array using only swaps.
+Given a function that generates perfectly random numbers between 1 and k (inclusive),
+where k is an input, write a function that shuffles a deck of cards represented as an
+array using only swaps.
+
 It should run in O(N) time.
+
 Hint: Make sure each one of the 52! permutations of the deck is equally likely.
 """
 
-# library import
 from random import randint
+from typing import List
 
-# a function that generates perfectly random numbers between 1 and k (inclusive), where k is an input.
-def rand_gen(k):
+# implementation of a function that generates perfectly random numbers between 1 and k
+def generate_random_number_in_range(k: int) -> int:
     return randint(1, k)
 
 
-# FUNCTION TO PERFORM THE OPERATION
-def swap():
-    # creating an array containing numbers from 1 to 52
-    arr = [i for i in range(1, 53)]
-
-    # Looping over the array
+def swap() -> List[int]:
+    # generating the card list
+    cards = [card_no for card_no in range(1, 53)]
+    # shuffling the cards
     for i in range(52):
-        # getting the index to swap (index is in the range [0, 51], so subtracting 1 from the generated random number)
-        temp = rand_gen(52) - 1
-
-        # swapping the elements at the current position with the element at the generated position
-        arr[i], arr[temp] = arr[temp], arr[i]
-
-    # returning the randomized sequence
-    return arr
+        swap_position = generate_random_number_in_range(52) - 1
+        cards[i], cards[swap_position] = cards[swap_position], cards[i]
+    return cards
 
 
-# DRIVER CODE
-print(*swap())
-print(*swap())
+if __name__ == "__main__":
+    print(*swap())
+    print(*swap())
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(1)
+SPACE COMPLEXITY: O(1)
+[n = 52 (constant)]
+"""
