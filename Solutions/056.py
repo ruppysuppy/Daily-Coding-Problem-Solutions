e1c2680c0900c8e923278e897329061a0f8ea167
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-11-24 09:17:37 +0530

    Refactored solution 56

--- Solutions/056.py
+++ Solutions/056.py
@@ -1,43 +1,41 @@
 """
 Problem:
 
-You are given an undirected graph represented as an adjacency matrix and an integer k. 
-Write a function to determine whether each vertex in the graph can be colored such that no two adjacent vertices share the same color.
-You can use at most k colors.
+Given an undirected graph represented as an adjacency matrix and an integer k, write a
+function to determine whether each vertex in the graph can be colored such that no two
+adjacent vertices share the same color using at most k colors.
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def check_can_color(adjacency_matrix, k):
-    # col_req: tracks the minimum number of color required
-    col_req = 0
-
-    # looping over the rows of the adjacency_matrix
-    for i in range(len(adjacency_matrix)):
-        # temp: tracks the minimum number of colors reuired to color the current node's neighbors
-        temp = 1
-
-        # looping over all neighbors
-        for j in range(len(adjacency_matrix[0])):
-            # if i == j => there is a loop in the graph (its not considered), else temp is incremented
-            if i != j:
-                if adjacency_matrix[i][j] != 0:
-                    temp += 1
-
-        # updating col_req as per requirement
-        if temp > col_req:
-            col_req = temp
-
-    # returns boolean (whether col_req is less than or equal to k)
-    return col_req <= k
+from typing import List
 
 
-# DRIVER CODE
-adjacency_matrix = [
-    [0, 1, 1, 1],
-    [1, 0, 1, 1],
-    [1, 1, 0, 1],
-    [1, 1, 1, 0],
-]
+def can_color(adjacency_matrix: List[List[int]], k: int) -> bool:
+    minimum_colors_required = 0
+    vertices = len(adjacency_matrix)
+    # generating the minimum number of colors required to color the graph
+    for vertex in range(vertices):
+        colors_required_for_current_vertex = sum(adjacency_matrix[vertex]) + 1
+        minimum_colors_required = max(
+            minimum_colors_required, colors_required_for_current_vertex
+        )
+    return minimum_colors_required <= k
 
-print(check_can_color(adjacency_matrix, 4))
-print(check_can_color(adjacency_matrix, 3))
+
+if __name__ == "__main__":
+    adjacency_matrix = [
+        [0, 1, 1, 1],
+        [1, 0, 1, 1],
+        [1, 1, 0, 1],
+        [1, 1, 1, 0],
+    ]
+
+    print(can_color(adjacency_matrix, 4))
+    print(can_color(adjacency_matrix, 3))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n ^ 2)
+SPACE COMPLEXITY: O(1)
+"""
