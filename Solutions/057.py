126273d369680d425a0b775ad0767393f0c306ab
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-11-25 08:48:34 +0530

    Refactored solution 57

--- Solutions/057.py
+++ Solutions/057.py
@@ -1,59 +1,57 @@
 """
 Problem:
 
-Given a string s and an integer k, break up the string into multiple lines such that each line has a length of k or less. 
-You must break it up so that words don't break across lines. Each line has to have the maximum possible amount of words. 
-If there's no way to break the text up, then return null.
-You can assume that there are no spaces at the ends of the string and that there is exactly one space between each word.
+Given a string s and an integer k, break up the string into multiple texts such that
+each text has a length of k or less. You must break it up so that words don't break
+across lines. If there's no way to break the text up, then return null.
 
-Example:
+You can assume that there are no spaces at the ends of the string and that there is
+exactly one space between each word.
 
-"the quick brown fox jumps over the lazy dog", 10 => ["the quick", "brown fox", "jumps over", "the lazy", "dog"]
+For example, given the string "the quick brown fox jumps over the lazy dog" and k = 10,
+you should return: ["the quick", "brown fox", "jumps over", "the lazy", "dog"]. No
+string in the list has a length of more than 10.
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def break_string(string, k):
-    # str_list: stores the list of words (input)
-    # word_list: stores the list of words (output)
-    # curr_len: stores the length of the current output
-    # curr_str: stores the current output string
-    str_list = string.split()
-    word_list = []
+from typing import List, Optional
+
+
+def break_string(string: str, k: int) -> Optional[List[str]]:
+    word_list = string.split()
+    result = []
     curr_len = 0
     curr_str = ""
-
-    # looping over the words (input)
-    for i in str_list:
-        temp = len(i)
-
-        # if the loop is just starting this conditional is entered
+    # generating the formatted text
+    for word in word_list:
+        current_word_length = len(word)
+        if current_word_length > k:
+            return None
         if curr_len == 0:
-            # if the word length is larger than k, None is returned, else curr_len and curr_str are updated
-            if temp < k:
-                curr_len = temp
-                curr_str = i
-            else:
-                return None
-
-        # if the word length is larger than k, None is returned, else word_list, curr_len and curr_str are updated
-        elif curr_len + temp + 1 > k:
-            if temp > k:
-                return None
-
-            word_list.append(curr_str)
-            curr_str = i
-            curr_len = temp
-
-        # the current word can be incorporated in the curr_str if its possible
+            # first iteration
+            curr_len = current_word_length
+            curr_str = word
+        elif curr_len + current_word_length + 1 > k:
+            # overflow condition
+            result.append(curr_str)
+            curr_str = word
+            curr_len = current_word_length
         else:
-            curr_len += temp
-            curr_str += " " + i
+            # normal addition to the string
+            curr_len += current_word_length
+            curr_str += " " + word
+    result.append(curr_str)
+    return result
 
-    # adding the final string and returning the word_list
-    word_list.append(curr_str)
-    return word_list
+
+if __name__ == "__main__":
+    print(break_string("the quick brown fox jumps over the lazy dog", 10))
+    print(break_string("the quick brown fox jumps over the lazy dog", 3))
+    print(break_string("the quick brown fox jumps over the lazy dog tenletters", 10))
 
 
-# DRIVER CODE
-print(break_string("the quick brown fox jumps over the lazy dog", 10))
-print(break_string("the quick brown fox jumps over the lazy dog", 3))
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(n)
+"""
