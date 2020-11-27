37097b739632d3731a2355a311343a26cc44def2
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-11-27 09:24:40 +0530

    Refactored solution 59

--- Solutions/059.py
+++ Solutions/059.py
@@ -1,158 +1,144 @@
 """
 Problem:
 
-Implement a file syncing algorithm for two computers over a low-bandwidth network. 
+Implement a file syncing algorithm for two computers over a low-bandwidth network.
 What if we know the files in the two computers are mostly the same?
 """
 
-# library import
+from __future__ import annotations
 from hashlib import sha256
 
-# using the hash function sha256 (other hash functions can be used too)
 hash_func = sha256
 
-# Merkle Node class (Parent for file and directory)
+
 class MerkleNode:
-    # initialize function
-    def __init__(self):
+    def __init__(self, name: str) -> None:
         self.parent = None
         self.node_hash = None
-
-    # add to directory function (used for both file and directory to add them as a sub-file/sub-directory)
-    def add_to_directory(self, dir_node):
-        # adding the node
-        self.parent = dir_node
-        dir_node.children.add(self)
-        dir_node.children_name.add(self.name)
-
-        # recalculating hash
-        while dir_node:
-            dir_node.recalculate_hash()
-            dir_node = dir_node.parent
-
-
-# Merkle Directory class
-class MerkleDirectory(MerkleNode):
-    # initialize function
-    def __init__(self, name):
-        MerkleNode.__init__(self)
-        self.children = set()
-        self.children_name = set()
-        self.is_dir = True
         self.name = name
-        # creating a file on directory initialize and recalculating hash
+
+    def __hash__(self) -> int:
+        return hash(self.name)
+
+    def __eq__(self, other: MerkleNode) -> bool:
+        if type(other) != MerkleNode:
+            return False
+        return self.node_hash == other.node_hash
+
+
+class MerkleDirectory(MerkleNode):
+    def __init__(self, name: str) -> None:
+        MerkleNode.__init__(self, name)
+        self.children = set()
+        self.is_dir = True
+        # creating a file on directory initialize and calculating hash
         new_file = MerkleFile("dir_init")
         new_file.add_to_directory(self)
 
-    # display functions (str and repr)
-    def __str__(self):
-        return f"Name: {self.name}\nChildren: {self.children}"
+    def __repr__(self) -> str:
+        return f"Name: {self.name}, Children: {self.children}, Hash: {self.node_hash}"
 
-    def __repr__(self):
-        return "\n" + self.__str__()
+    def add_to_directory(self, directory: MerkleDirectory) -> None:
+        # adding the node
+        self.parent = directory
+        directory.children.add(self)
+        # recalculating hash for all anscestors
+        while directory is not None:
+            directory.recalculate_hash()
+            directory = directory.parent
 
-    # function to recalculate hash for the current directory
-    def recalculate_hash(self):
+    def recalculate_hash(self) -> None:
         # concatinating all hashes and recalculating on the cumulative hash
-        if self.children:
-            cumulative_hash = ""
-            for child in self.children:
-                cumulative_hash += child.node_hash
-            self.node_hash = hash_func(cumulative_hash.encode()).hexdigest()
-        else:
-            self.node_hash = hash_func("".encode()).hexdigest()
+        cumulative_hash = ""
+        for child in self.children:
+            cumulative_hash += child.node_hash
+        self.node_hash = hash_func(cumulative_hash.encode()).hexdigest()
 
-    # function to synchronize 2 directories
-    def sync(self, other):
+    def synchronize(self, other: MerkleDirectory) -> None:
         # if the directories have the same hash, they are already synchronized
         if self.node_hash == other.node_hash:
             return
-
-        # copying from self to other
+        # updating other using self
         for node in self.children:
             if not node in other.children:
-                (type(node))(other.children.add(node))
-
-        # copying from other to self
+                type(node)(node.add_to_directory(other))
+        # updating self using other
         for node in other.children:
             if not node in self.children:
-                (type(node))(self.children.add(node))
-        # NOTE: Type-casting is used to get a deep copy instead of a shallow one
+                type(node)(node.add_to_directory(self))
 
 
-# Merkle File class
 class MerkleFile(MerkleNode):
-    # initialize function
-    def __init__(self, name):
-        MerkleNode.__init__(self)
+    def __init__(self, name: str) -> None:
+        MerkleNode.__init__(self, name)
         self.node_contents = ""
         self.is_dir = False
-        self.name = name
         self.node_hash = hash_func(self.node_contents.encode()).hexdigest()
 
-    # display functions (repr)
-    def __repr__(self):
-        return f"\nName: {self.name}\nContent: {self.node_contents}"
+    def __repr__(self) -> str:
+        return (
+            f"[ Name: {self.name}, Content: "
+            + f"{self.node_contents if self.node_contents else 'null'}, "
+            + f"Hash: {self.node_hash} ]"
+        )
 
-    # function to update the contents of the file
-    def update_contents(self, new_contents):
-        # contents updated and hash recaculated of all the anscestors of the current file
+    def add_to_directory(self, directory: MerkleDirectory) -> None:
+        # adding the node
+        self.parent = directory
+        directory.children.add(self)
+        # recalculating hash for all anscestors
+        while directory is not None:
+            directory.recalculate_hash()
+            directory = directory.parent
+
+    def update_contents(self, new_contents: str) -> None:
+        # contents updated and hash recaculated
         self.node_hash = hash_func(new_contents.encode()).hexdigest()
         self.node_contents = new_contents
         if self.parent:
             self.parent.recalculate_hash()
 
 
-# Computer class
 class Computer:
-    # initialize function
     def __init__(self):
         self.root = MerkleDirectory("root")
 
-    # synchronize function
-    def sync(self, new_comp):
-        if type(new_comp) == Computer:
-            print("Syncing computers...")
-            self.root.sync(new_comp.root)
-            print("Sync successful!\n")
+    def __repr__(self) -> str:
+        return str(self.root)
+
+    def synchronize(self, new_comp: Computer) -> None:
+        print("Syncing computers...")
+        self.root.synchronize(new_comp.root)
+        print("Sync successful!\n")
 
 
-# DRIVER CODE
-c1 = Computer()
-c2 = Computer()
+if __name__ == "__main__":
+    c1 = Computer()
+    c2 = Computer()
 
-print("COMPUTER 1:")
-print(c1.root)
-print("COMPUTER 2:")
-print(c2.root)
-print()
+    print("COMPUTER 1:")
+    print(c1)
+    print("COMPUTER 2:")
+    print(c2)
+    print()
 
-new_file = MerkleFile("check_file")
-new_file.update_contents("Check")
-new_file.add_to_directory(c1.root)
+    new_file = MerkleFile("check_file")
+    new_file.update_contents("Check")
+    new_file.add_to_directory(c1.root)
 
-new_dir = MerkleDirectory("check_dir")
-new_dir.add_to_directory(c2.root)
+    new_dir = MerkleDirectory("check_dir")
+    new_dir.add_to_directory(c2.root)
 
-print("COMPUTER 1:")
-print(c1.root)
-print("COMPUTER 2:")
-print(c2.root)
-print()
+    print("COMPUTER 1:")
+    print(c1)
+    print("COMPUTER 2:")
+    print(c2)
+    print()
 
-c1.sync(c2)
+    c1.synchronize(c2)
 
-print("COMPUTER 1:")
-print(c1.root)
-print("COMPUTER 2:")
-print(c2.root)
-print()
-
-new_file = MerkleFile("check_file2")
-new_file.update_contents("Check Again")
-new_file.add_to_directory(c1.root)
-
-print("COMPUTER 1:")
-print(c1.root)
-print("COMPUTER 2:")
-print(c2.root)
+    print("COMPUTER 1:")
+    print(c1)
+    print("COMPUTER 2:")
+    print(c2)
+    print()
