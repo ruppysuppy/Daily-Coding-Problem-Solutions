ac6b77711250571821c11387ede1ed25fa44c2b4
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-11-27 09:35:37 +0530

    Refactored solution 60

--- Solutions/060.py
+++ Solutions/060.py
@@ -1,44 +1,52 @@
 """
 Problem:
 
-Given a multiset of integers, return whether it can be partitioned into two subsets whose sums are the same.
+Given a multiset of integers, return whether it can be partitioned into two subsets
+whose sums are the same.
 
-Example:
-[15, 5, 20, 10, 35, 15, 10] => true (since we can split it up into {15, 5, 10, 15, 10} and {20, 35}, which both add up to 55)
-[15, 5, 20, 10, 35] => false
+For example, given the multiset {15, 5, 20, 10, 35, 15, 10}, it would return true,
+since we can split it up into {15, 5, 10, 15, 10} and {20, 35}, which both add up to 55.
+
+Given the multiset {15, 5, 20, 10, 35}, it would return false, since we can't split it
+up into two subsets that add up to the same sum.
 """
 
-# helper function to check for checking whether splitting into 2 subsets of equal sum is possible
-def split_helper(arr, start, stop, sum_inner, sum_outer):
-    # if the start and the stop pointers cross each other, the list cannot be split
+from typing import List
+
+
+def equal_sum_split_check_helper(
+    arr: List[int], start: int, stop: int, sum_inner: int, sum_outer: int
+) -> bool:
     if start >= stop:
         return False
-    # if (sum_inner = sum_outer), we have found the split
     elif sum_inner == sum_outer:
         return True
-
-    # checking for possible splits (once with adding 1st element to sum_outer, again with last element, giving rise to a recursion tree)
-    return split_helper(
+    # checking for all possible splits
+    return equal_sum_split_check_helper(
         arr, start + 1, stop, sum_inner - arr[start], sum_outer + arr[start]
-    ) or split_helper(
+    ) or equal_sum_split_check_helper(
         arr, start, stop - 1, sum_inner - arr[stop], sum_outer + arr[stop]
     )
 
 
-# FUNCTION FOR PERFORMING THE OPERATION
-def equal_sum_split_check(arr):
-    # checking if the sum is odd (it cannot be split)
-    # [even though theoretically, empty set can be broken into 2 empty sets, we have condidered the case for valid elements]
-    if not arr or sum(arr) % 2 == 1:
+def equal_sum_split_check(arr: List[int]) -> bool:
+    # cases where the array cannot be split
+    total_sum = sum(arr)
+    if not arr or total_sum % 2 == 1:
         return False
-
-    # sorting the list (pre-requisite for split_helper)
+    # sorting the array (pre-requisite for split_helper)
     arr.sort()
-
-    # returning the value returned by split_helper
-    return split_helper(arr, 0, len(arr) - 1, sum(arr), 0)
+    return equal_sum_split_check_helper(arr, 0, len(arr) - 1, total_sum, 0)
 
 
-# DRIVER CODE
-print(equal_sum_split_check([15, 5, 20, 10, 35, 15, 10]))
-print(equal_sum_split_check([15, 5, 20, 10, 35]))
+if __name__ == "__main__":
+    print(equal_sum_split_check([15, 5, 20, 10, 35, 15, 10]))
+    print(equal_sum_split_check([15, 5, 20, 10, 35]))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(2 ^ n)
+SPACE COMPLEXITY: O(n) [recursion depth]
+"""
