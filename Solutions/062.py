edde693e3af8d6a8b31ff64ab18a484a91ce0633
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-11-28 08:38:25 +0530

    Refactored solution 62

--- Solutions/062.py
+++ Solutions/062.py
@@ -1,35 +1,36 @@
 """
 Problem:
 
-There is an N by M matrix of zeroes. 
-Given N and M, write a function to count the number of ways of starting at the top-left corner and getting to the bottom-right corner. 
-You can only move right or down.
+There is an N by M matrix of zeroes. Given N and M, write a function to count the
+number of ways of starting at the top-left corner and getting to the bottom-right
+corner. You can only move right or down.
 
-Example:
+For example, given a 2 by 2 matrix, you should return 2, since there are two ways to
+get to the bottom-right:
 
-2, 2 => 2 (since there are two ways to get to the bottom-right:
-* Right, then down
-* Down, then right)
-
-5, 5 => 70
+Right, then down
+Down, then right
+Given a 5 by 5 matrix, there are 70 ways to get to the bottom-right.
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def num_ways(n, m):
-    # matrix creation (of shape n x m)
-    # the 1st row and 1st column has 1 as there is only 1 way to move to the position (go right / go down)
-    mat = [[(1 if (i == 0 or j == 0) else 0) for i in range(m)] for j in range(n)]
 
-    # looping over the other positions and calculating the number of ways that position can be reached
-    # the values are populated from top left to bottom right
+def get_num_ways(n: int, m: int) -> int:
+    matrix = [[(1 if (i == 0 or j == 0) else 0) for i in range(m)] for j in range(n)]
+
     for i in range(1, n):
         for j in range(1, m):
-            mat[i][j] = mat[i - 1][j] + mat[i][j - 1]
-
-    # the final result is at the mat[n-1][m-1]
-    return mat[n - 1][m - 1]
+            matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1]
+    return matrix[n - 1][m - 1]
 
 
-# DRIVER CODE
-print(num_ways(2, 2))
-print(num_ways(5, 5))
+if __name__ == "__main__":
+    print(get_num_ways(2, 2))
+    print(get_num_ways(5, 5))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n x m)
+SPACE COMPLEXITY: O(n x m)
+"""
