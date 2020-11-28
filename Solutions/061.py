3b07d91552955bd3b610e3852bf2010a228af545
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-11-28 08:33:45 +0530

    Refactored solution 61

--- Solutions/061.py
+++ Solutions/061.py
@@ -1,27 +1,32 @@
 """
 Problem:
 
-Implement integer exponentiation. That is, implement the pow(x, y) function, where x and y are integers and returns x^y.
+Implement integer exponentiation. That is, implement the pow(x, y) function, where x
+and y are integers and returns x^y.
+
 Do this faster than the naive method of repeated multiplication.
 
-Example:
-2, 10 => 1024
+For example, pow(2, 10) should return 1024.
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def pow(base, power):
-    # this function implements a method similar to binary search, the effective complexity is O(log(n))
-    # base case [x^0 = 1]
+
+def pow(base: int, power: int) -> int:
     if power == 0:
         return 1
-    # if the exponent is odd, the result is given by [base ^ exp = ((base * base) ^ floor(exp / 2)) * base]
-    elif power % 2 != 0:
+
+    if power % 2 != 0:
         return pow((base * base), power // 2) * base
-    # if the exponent is even, the result is given by [base ^ exp = (base * base) ^ floor(exp / 2)]
-    else:
-        return pow((base * base), power // 2)
+    return pow((base * base), power // 2)
 
 
-# DRIVER CODE
-print(pow(2, 10))
-print(pow(3, 4))
+if __name__ == "__main__":
+    print(pow(2, 10))
+    print(pow(3, 4))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(log(n))
+SPACE COMPLEXITY: O(log(n)) [recursion depth]
+"""
