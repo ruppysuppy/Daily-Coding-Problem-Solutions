a70b235c4975c0afd37a8d4b0dcd57e79ad3f9ec
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-11-29 08:52:59 +0530

    Refactored solution 63

--- Solutions/063.py
+++ Solutions/063.py
@@ -1,52 +1,57 @@
 """
 Problem:
 
-You are given a 2D matrix of characters and a target word.
-Write a function that returns whether the word can be found in the matrix by going left-to-right, or up-to-down.
+Given a 2D matrix of characters and a target word, write a function that returns
+whether the word can be found in the matrix by going left-to-right, or up-to-down.
 
-Example:
+For example, given the following matrix:
 
-Mat =  [['F', 'A', 'C', 'I'],
-        ['O', 'B', 'Q', 'P'],
-        ['A', 'N', 'O', 'B'],
-        ['M', 'A', 'S', 'S']]
-'FOAM' => true (since it's the leftmost column)
-'MASS' => true (since it's the last row)
+[['F', 'A', 'C', 'I'],
+ ['O', 'B', 'Q', 'P'],
+ ['A', 'N', 'O', 'B'],
+ ['M', 'A', 'S', 'S']]
+and the target word 'FOAM', you should return true, since it's the leftmost column.
+Similarly, given the target word 'MASS', you should return true, since it's the last
+row.
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def word_occorance_check(mat, word):
-    # getting the number of rows and columns
-    n = len(mat)
-    m = len(mat[0])
+from typing import List
 
-    # row check (searching for the word in every row)
+
+def check_word_occorance(matrix: List[List[str]], word: str) -> bool:
+    n = len(matrix)
+    m = len(matrix[0])
+    # check for word occourance in the rows
     for i in range(n):
-        temp = "".join(mat[i])
-
-        if word in temp:
+        row_string = "".join(matrix[i])
+        if word in row_string:
             return True
-
-    # column check (searching for the word in every column)
+    # check for word occourance in the columns
     for j in range(m):
-        temp = ""
+        column_string = ""
         for i in range(n):
-            temp += mat[i][j]
-
-        if word in temp:
+            column_string += matrix[i][j]
+        if word in column_string:
             return True
-
     return False
 
 
-# DRIVER CODE
-mat = [
-    ["F", "A", "C", "I"],
-    ["O", "B", "Q", "P"],
-    ["A", "N", "O", "B"],
-    ["M", "A", "S", "S"],
-]
+if __name__ == "__main__":
+    matrix = [
+        ["F", "A", "C", "I"],
+        ["O", "B", "Q", "P"],
+        ["A", "N", "O", "B"],
+        ["M", "A", "S", "S"],
+    ]
 
-print(word_occorance_check(mat, "FOAM"))
-print(word_occorance_check(mat, "MASS"))
-print(word_occorance_check(mat, "FORM"))
+    print(check_word_occorance(matrix, "FOAM"))
+    print(check_word_occorance(matrix, "MASS"))
+    print(check_word_occorance(matrix, "FORM"))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n x m)
+SPACE COMPLEXITY: O(n + m)
+"""
