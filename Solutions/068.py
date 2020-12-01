92f9bfd4ed3f0bf5613fe42748befe6782456d3b
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-01 09:09:32 +0530

    Refactored solution 68

--- Solutions/068.py
+++ Solutions/068.py
@@ -1,100 +1,72 @@
 """
 Problem:
 
-On our special chessboard, two bishops attack each other if they share the same diagonal. 
-This includes bishops that have another bishop located between them, i.e. bishops can attack through pieces.
-You are given N bishops, represented as (row, column) tuples on a M by M chessboard. 
-Write a function to count the number of pairs of bishops that attack each other. 
-The ordering of the pair doesn't matter: (1, 2) is considered the same as (2, 1).
+On our special chessboard, two bishops attack each other if they share the same
+diagonal. This includes bishops that have another bishop located between them, i.e.
+bishops can attack through pieces.
 
-Example:
+You are given N bishops, represented as (row, column) tuples on a M by M chessboard.
+Write a function to count the number of pairs of bishops that attack each other. The
+ordering of the pair doesn't matter: (1, 2) is considered the same as (2, 1).
 
-Input = 5, [(0, 0), (1, 2), (2, 2), (4, 0)]
-<The board would look like this:
+For example, given M = 5 and the list of bishops:
+
+(0, 0)
+(1, 2)
+(2, 2)
+(4, 0)
+The board would look like this:
+
 [b 0 0 0 0]
 [0 0 b 0 0]
 [0 0 b 0 0]
 [0 0 0 0 0]
-[b 0 0 0 0]>
-Output = 2 (since bishops 1 and 3 attack each other, as well as bishops 3 and 4)
+[b 0 0 0 0]
+You should return 2, since bishops 1 and 3 attack each other, as well as bishops 3 and
+4.
 """
 
-# helper function to calculate the diagonals
-def get_diagonal_pos(pos, board_size):
-    # breaking pos into row and column
-    row = pos[0]
-    col = pos[1]
-    # diagonal set stores the diagonal positions of the current bishop
+from typing import List, Set, Tuple
+
+Position = Tuple[int, int]
+
+
+def generate_diagonals(position: Position, board_size: int) -> Set[Position]:
+    row, col = position
     diagonals = set()
-
-    # calculating the upper left diagonal positions
-    curr_row = row - 1
-    curr_col = col - 1
-    for _ in range(board_size):
-        if curr_col < 0 or curr_row < 0:
-            break
-
-        diagonals.add((curr_row, curr_col))
-        curr_row -= 1
-        curr_col -= 1
-
-    # calculating the lower right diagonal positions
-    curr_row = row + 1
-    curr_col = col + 1
-    for _ in range(board_size):
-        if curr_col >= board_size or curr_row >= board_size:
-            break
-
-        diagonals.add((curr_row, curr_col))
-        curr_row += 1
-        curr_col += 1
-
-    # calculating the upper right diagonal positions
-    curr_row = row - 1
-    curr_col = col + 1
-    for _ in range(board_size):
-        if curr_col >= board_size or curr_row < 0:
-            break
-
-        diagonals.add((curr_row, curr_col))
-        curr_row -= 1
-        curr_col += 1
-
-    # calculating the lower left diagonal positions
-    curr_row = row + 1
-    curr_col = col - 1
-    for _ in range(board_size):
-        if curr_col < 0 or curr_row >= board_size:
-            break
-
-        diagonals.add((curr_row, curr_col))
-        curr_row += 1
-        curr_col -= 1
-
+    # upper left diagonal
+    for i, j in zip(range(row - 1, -1, -1), range(col - 1, -1, -1)):
+        diagonals.add((i, j))
+    # lower left diagonal
+    for i, j in zip(range(row + 1, board_size), range(col - 1, -1, -1)):
+        diagonals.add((i, j))
+    # upper right diagonal
+    for i, j in zip(range(row - 1, -1, -1), range(col + 1, board_size)):
+        diagonals.add((i, j))
+    # lower right diagonal
+    for i, j in zip(range(row + 1, board_size), range(col + 1, board_size)):
+        diagonals.add((i, j))
     return diagonals
 
 
-# FUNCTION TO PERFORM THE OPERATION
-def num_attacking(board_size, pos_list):
-    # count stores the number of attacking bishops
+def get_number_of_attacking_bishops(board_size: int, positions: List[Position]) -> int:
     count = 0
-
-    # looping over the list of bishop positions
-    for pos in pos_list:
-        # the diagonals are calculated (returns a set for O(1) access)
-        diagonals = get_diagonal_pos(pos, board_size)
-
-        # checking if any bishop lies on the diagonal of the current bishop
-        for pos in pos_list:
-            # if there is a bishop in the diagonal, count is incremented
-            # it considers odering (so (1, 2) and (2, 1) are considered different)
-            if pos in diagonals:
+    for index, position in enumerate(positions):
+        diagonals = generate_diagonals(position, board_size)
+        for position in positions[index:]:
+            if position in diagonals:
                 count += 1
-
-    # to eliminate ordering problem, (count/2) is returned
-    return count // 2
+    return count
 
 
-# DRIVER CODE
-pos_list = [(0, 0), (1, 2), (2, 2), (4, 0)]
-print(num_attacking(5, pos_list))
+if __name__ == "__main__":
+    positions = [(0, 0), (1, 2), (2, 2), (4, 0)]
+    print(get_number_of_attacking_bishops(5, positions))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n ^ 2)
+SPACE COMPLEXITY: O(n)
+"""
