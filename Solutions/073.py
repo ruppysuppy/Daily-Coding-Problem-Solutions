626a467b688d13ecb4f10c5abd610ea422c6e948
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-03 13:03:00 +0530

    Refactored solution 73

--- Solutions/073.py
+++ Solutions/073.py
@@ -4,49 +4,40 @@ Problem:
 Given the head of a singly linked list, reverse it in-place.
 """
 
-# importing the linked list
-from DataStructures.LinkedList import Linked_list
+from DataStructures.LinkedList import LinkedList
 
-# FUNCTION TO PERFORM THE OPERATION
-def reverse_inplace(LL):
-    # setting the rear pointer
-    LL.rear = LL.head
 
-    # using 3 pointers (ptr1 points the previous node, ptr2 the current node and ptr3 the next node)
-    ptr1 = None
-    ptr2 = LL.head
-    ptr3 = LL.head.next
-
-    # looping over till we reach the end of the linked list
-    while ptr2 != None:
-        # reversing the flow (ptr2 -> ptr1)
-        ptr2.next = ptr1
-
-        # updating the pointers to their next locations
-        ptr1 = ptr2
-        ptr2 = ptr3
-        if ptr3 == None:
+def reverse_inplace(ll: LinkedList) -> None:
+    ll.rear = ll.head
+    ptr_prev, ptr_curr, ptr_next = None, ll.head, ll.head.next
+    # reversing the flow
+    while ptr_curr is not None:
+        ptr_curr.next = ptr_prev
+        ptr_prev, ptr_curr = ptr_curr, ptr_next
+        if ptr_next is None:
             break
-        ptr3 = ptr3.next
-
-    # setting the head of the linked list
-    LL.head = ptr1
+        ptr_next = ptr_next.next
+    ll.head = ptr_prev
 
 
-# DRIVER CODE
-LL = Linked_list()
+if __name__ == "__main__":
+    ll = LinkedList()
+    for num in range(1, 6):
+        ll.add(num)
+    print(ll)
+    reverse_inplace(ll)
+    print(ll)
 
-LL.add(1)
-LL.add(2)
-LL.add(3)
-LL.add(4)
-LL.add(5)
-LL.add(6)
-LL.add(7)
-LL.add(8)
+    ll = LinkedList()
+    for num in range(1, 3):
+        ll.add(num)
+    print(ll)
+    reverse_inplace(ll)
+    print(ll)
 
-print(LL)
-
-reverse_inplace(LL)
-
-print(LL)
+    ll = LinkedList()
+    for num in range(1, 2):
+        ll.add(num)
+    print(ll)
+    reverse_inplace(ll)
+    print(ll)
