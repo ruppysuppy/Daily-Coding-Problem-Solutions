f12a756efa449017154b3e22daa36d047847fa4c
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-05 08:38:42 +0530

    Refactored solution 76

--- Solutions/076.py
+++ Solutions/076.py
@@ -1,49 +1,64 @@
 """
 Problem:
 
-You are given an N by M 2D matrix of lowercase letters. 
-Determine the minimum number of columns that can be removed to ensure that each row is ordered from top to bottom lexicographically. 
-That is, the letter at each column is lexicographically later as you go down each row. 
-It does not matter whether each row itself is ordered lexicographically.
+You are given an N by M 2D matrix of lowercase letters. Determine the minimum number of
+columns that can be removed to ensure that each row is ordered from top to bottom
+lexicographically. That is, the letter at each column is lexicographically later as you
+go down each row. It does not matter whether each row itself is ordered
+lexicographically.
 
-Example:
+For example, given the following table:
 
-Input = ['cba', 'daf', 'ghi'] (This is not ordered because of the 'a' in the center)
-Output = 1 (We can remove the second column to make it ordered: ['ca', 'df', 'gi'])
+cba
+daf
+ghi
+This is not ordered because of the a in the center. We can remove the second column to
+make it ordered:
 
-Input = ['abcdef']
-Output = 0 (there's only one row)
+ca
+df
+gi
+So your function should return 1, since we only needed to remove 1 column.
 
-Input = ['zyx', 'wvu', 'tsr']
-Output = 3 (since we would need to remove all the columns to order it)
+As another example, given the following table:
+
+abcdef
+Your function should return 0, since the rows are already ordered (there's only one
+row).
+
+As another example, given the following table:
+
+zyx
+wvu
+tsr
+Your function should return 3, since we would need to remove all the columns to order
+it.
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def calc(mat):
-    # setting the deault variables
-    # num_elements stores the number of elements
-    # length stores the length of each row
-    # count stores number of columns to remove
-    num_elements, length = len(mat), len(mat[0])
-    count = 0
+from typing import List
 
-    # looping over wach column
-    for i in range(length):
-        # checking if the columns are lexicographically arranged
-        for j in range(num_elements - 1):
-            # if the column is not lexicographical, count is incremented and control breaks out of the loop
-            if mat[j][i] > mat[j + 1][i]:
+
+def get_minimum_column_removals(matrix: List[List[int]]) -> int:
+    rows, columns = len(matrix), len(matrix[0])
+    count = 0
+    for column in range(columns):
+        # checking if the column is lexicographical
+        for row in range(rows - 1):
+            if matrix[row][column] > matrix[row + 1][column]:
                 count += 1
                 break
-
     return count
 
 
-# DRIVER CODE
-inp1 = ["cba", "daf", "ghi"]
-inp2 = ["abcdef"]
-inp3 = ["zyx", "wvu", "tsr"]
+if __name__ == "__main__":
+    print(get_minimum_column_removals(["cba", "daf", "ghi"]))
+    print(get_minimum_column_removals(["abcdef"]))
+    print(get_minimum_column_removals(["zyx", "wvu", "tsr"]))
 
-print(calc(inp1))
-print(calc(inp2))
-print(calc(inp3))
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n x m)
+SPACE COMPLEXITY: O(1)
+"""
