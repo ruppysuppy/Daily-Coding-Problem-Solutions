b4407405d94b605b364b2902e68c4249f8acf83b
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-06 08:42:02 +0530

    Refactored solution 78

--- Solutions/078.py
+++ Solutions/078.py
@@ -1,61 +1,72 @@
 """
 Problem:
 
-Given k sorted singly linked lists, write a function to merge all the lists into one sorted singly linked list.
+Given k sorted singly linked lists, write a function to merge all the lists into one
+sorted singly linked list.
 """
 
-# importing LinkedList from the DataStructures
-from DataStructures.LinkedList import Linked_list
+from sys import maxsize
+from typing import List
 
-# FUNCTION TO PERFORM THE OPERATION
-def merge_sorted_linked_list(list_of_LL, k):
-    # pos list to store the current location on each of the linked list
-    # sorted_list stores the final sorted linked list
-    pos = [LL.head for LL in list_of_LL]
-    sorted_list = Linked_list()
+from DataStructures.LinkedList import LinkedList, Node
 
-    # lopping over the pos list till all the position pointers reach the end (None) of the Linked List
-    while any(pos):
-        # initializing a random large minimum value (pererably use sys.INT_MAX)
-        minimum = 9999
 
-        # looping over the position pointers
+def merge_sorted_linked_list(list_of_LL: List[LinkedList]) -> LinkedList:
+    k = len(list_of_LL)
+    position_arr = [LL.head for LL in list_of_LL]
+    sorted_ll = LinkedList()
+    while any(position_arr):
+        # finding the node with minimum value
+        minimum = maxsize
         for i in range(k):
-            # if the pointer is not None and the value is less than the minimum, position & minimum is updated
-            if pos[i] != None:
-                if pos[i].val < minimum:
-                    minimum = pos[i].val
+            if position_arr[i] is not None:
+                if position_arr[i].val < minimum:
+                    minimum = position_arr[i].val
                     position = i
+        # generating new node
+        if sorted_ll.head is None:
+            sorted_ll.add(position_arr[position].val)
+            curr_position = sorted_ll.head
+        else:
+            curr_position.next = Node(position_arr[position].val)
+            curr_position = curr_position.next
+        sorted_ll.length += 1
+        position_arr[position] = position_arr[position].next
+    # resetting rear
+    sorted_ll.rear = curr_position
+    return sorted_ll
 
-        # the value is added to the sorted Linked List
-        sorted_list.add(pos[position].val)
-        # the position pointer is moved to the next value
-        pos[position] = pos[position].next
 
-    return sorted_list
+if __name__ == "__main__":
+    LL1 = LinkedList()
+    LL1.add(2)
+    LL1.add(25)
+    LL1.add(70)
+
+    LL2 = LinkedList()
+    LL2.add(5)
+    LL2.add(8)
+    LL2.add(14)
+    LL2.add(15)
+    LL2.add(21)
+    LL2.add(48)
+
+    LL3 = LinkedList()
+    LL3.add(0)
+    LL3.add(90)
+
+    print(LL1)
+    print(LL2)
+    print(LL3)
+
+    List = [LL1, LL2, LL3]
+    print(merge_sorted_linked_list(List))
 
 
-# DRIVER CODE
-LL1 = Linked_list()
-LL1.add(2)
-LL1.add(25)
-LL1.add(70)
+"""
+SPECS:
 
-LL2 = Linked_list()
-LL2.add(5)
-LL2.add(8)
-LL2.add(14)
-LL2.add(15)
-LL2.add(21)
-LL2.add(48)
-
-LL3 = Linked_list()
-LL3.add(0)
-LL3.add(90)
-
-print(LL1)
-print(LL2)
-print(LL3)
-
-List = [LL1, LL2, LL3]
-print(merge_sorted_linked_list(List, len(List)))
+TIME COMPLEXITY: O(n + k)
+SPACE COMPLEXITY: O(n + k)
+[n = total number of nodes]
+"""
