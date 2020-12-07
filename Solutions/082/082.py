4293a534bfefca820f3f512b0e7ea4796f29be0b
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-07 08:30:58 +0530

    Refactored solution 82

--- Solutions/082/082.py
+++ Solutions/082/082.py
@@ -1,51 +1,41 @@
 """
 Problem:
 
-Using a read7() method that returns 7 characters from a file, implement readN(n) which reads n characters.
-For example, given a file with the content "Hello world", three read7() returns "Hello w", "orld" and then "".
+Using a read7() method that returns 7 characters from a file, implement readN(n) which
+reads n characters.
+
+For example, given a file with the content "Hello world", three read7() returns
+"Hello w", "orld" and then "".
 """
-# global varibles
-# COUNT7 stores the number of characters read from the file (used in read7)
-COUNT7 = 0
-# DATA stores the unreturned data (used in readN)
-DATA = ""
 
-# read7 function
-def read7():
-    # declaring COUNT7 as global variable
-    global COUNT7
+# COUNT_7 stores the number of characters already read from the file
+# STASHED_TEXT stores the unreturned data (used in readN)
+COUNT_7 = 0
+STASHED_TEXT = ""
 
-    # reading the next 7 characters
-    with open("data_82.txt", "r") as f:
-        f.seek(COUNT7, 0)
+
+def read7(filename: str = "data_082.txt") -> str:
+    global COUNT_7
+    with open(filename, "r") as f:
+        f.seek(COUNT_7, 0)
         data = f.read(7)
-
-    # incrementing COUNT7 by 7
-    COUNT7 += 7
-
-    # returning the data
+    COUNT_7 += 7
     return data
 
 
-# FUNCTION TO PERFORM THE OPERATION
-def readN(n):
-    # declaring DATA as global variable
-    global DATA
-
-    # reading ceiling(n / 7) * 7 characters
-    for _ in range(n // 7):
-        DATA += read7()
-    DATA += read7()
-
-    # storing the characters which are not needed and returning the characters asked for
-    temp = DATA[:n]
-    DATA = DATA[n:]
-
-    return temp
+def readN(n: int, filename: str = "data_082.txt") -> str:
+    global STASHED_TEXT
+    for _ in range((n // 7) + 1):
+        STASHED_TEXT += read7(filename)
+    text = STASHED_TEXT[:n]
+    STASHED_TEXT = STASHED_TEXT[n:]
+    return text
 
 
-# DRIVER CODE
-print(readN(3))
-print(readN(10))
-print(readN(15))
-print(readN(25))
+if __name__ == "__main__":
+    print(readN(3))
+    print(readN(10))
+    print(readN(15))
+    print(readN(25))
+    print(readN(1000))
+    print(readN(1000))
