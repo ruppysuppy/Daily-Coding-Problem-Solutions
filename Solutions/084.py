8f5a6626744f100ced8ebf7daca881e70b872c6e
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-08 08:52:47 +0530

    Refactored solution 84

--- Solutions/084.py
+++ Solutions/084.py
@@ -1,30 +1,34 @@
 """
 Problem:
 
-Given a matrix of 1s and 0s, return the number of "islands" in the matrix. 
-A 1 represents land and 0 represents water, so an island is a group of 1s that are neighboring and their perimeter is surrounded by water.
+Given a matrix of 1s and 0s, return the number of "islands" in the matrix. A 1
+represents land and 0 represents water, so an island is a group of 1s that are
+neighboring and their perimeter is surrounded by water.
 
-Example:
+For example, this matrix has 4 islands.
 
-Input = 1 0 0 0 0
-        0 0 1 1 0
-        0 1 1 0 0
-        0 0 0 0 0
-        1 1 0 0 1
-        1 1 0 0 1
-Output = 4 (this matrix has 4 islands)
+1 0 0 0 0
+0 0 1 1 0
+0 1 1 0 0
+0 0 0 0 0
+1 1 0 0 1
+1 1 0 0 1
 """
 
-# helper function to return the valid neighbours of a point
-def get_neighbours(pos, grid_shape):
-    # breaking the values
-    n, m = grid_shape
-    i, j = pos
-    # res stores the final list to be returned
-    res = []
+from typing import List, Tuple
 
-    # getting all positions surrounding the point (invalid ones included too)
-    pos_list = [
+from DataStructures.Queue import Queue
+
+GridShape = Tuple[int, int]
+Matrix = List[List[int]]
+Position = Tuple[int, int]
+
+
+def get_neighbours(position: Position, grid_shape: GridShape) -> List[Position]:
+    n, m = grid_shape
+    i, j = position
+    neighbours = []
+    position_list = [
         (i - 1, j - 1),
         (i - 1, j),
         (i - 1, j + 1),
@@ -34,56 +38,56 @@ def get_neighbours(pos, grid_shape):
         (i + 1, j),
         (i + 1, j + 1),
     ]
-
-    # adding the valid positions to the res list
-    for pos_test in pos_list:
-        y_test, x_test = pos_test
-        if (x_test >= 0 and x_test < m) and (y_test >= 0 and y_test < n):
-            res.append(pos_test)
-
-    return res
+    for curr_position in position_list:
+        y, x = curr_position
+        if 0 <= x < m and 0 <= y < n:
+            neighbours.append(curr_position)
+    return neighbours
 
 
-# helper function to remove the island
-def remove_island(mat, pos):
+def remove_island(matrix: Matrix, position: Position, grid_shape: GridShape) -> None:
     # using bfs to remove the islands
-    queue = [pos]
+    queue = Queue()
+    queue.enqueue(position)
 
-    # looping till there are no 1s (island part) surrounding the given position
-    while queue:
-        # getting the next position to be processed and breaking it
-        curr = queue.pop(0)
-        i, j = curr
+    while not queue.is_empty():
+        curr_position = queue.dequeue()
+        i, j = curr_position
+        if matrix[i][j] == 1:
+            matrix[i][j] = 0
+            for neighbour in get_neighbours((i, j), grid_shape):
+                y, x = neighbour
+                if matrix[y][x] == 1:
+                    queue.enqueue(neighbour)
 
-        # if the position contains a 1, its replaced with 0 and the neighbours added to the queue
-        if mat[i][j] == 1:
-            mat[i][j] = 0
-            queue.extend(get_neighbours((i, j), (len(mat), len(mat[0]))))
 
-
-# FUNCTION TO PERFORM THE OPERATION
-def island_count(mat):
-    # initializing the island count to 0
+def island_count(matrix: Matrix) -> int:
     count = 0
+    n, m = len(matrix), len(matrix[0])
 
-    # looping over the matrix
-    for i in range(len(mat)):
-        for j in range(len(mat[0])):
-            # if an island is encountered, remove_island function is called and the island count incremented
-            if mat[i][j] == 1:
-                remove_island(mat, (i, j))
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j] == 1:
+                remove_island(matrix, (i, j), (n, m))
                 count += 1
-
     return count
 
 
-# DRIVER CODE
-mat = [
-    [1, 0, 0, 0, 0],
-    [0, 0, 1, 1, 0],
-    [0, 1, 1, 0, 0],
-    [0, 0, 0, 0, 0],
-    [1, 1, 0, 0, 1],
-    [1, 1, 0, 0, 1],
-]
-print(island_count(mat))
+if __name__ == "__main__":
+    matrix = [
+        [1, 0, 0, 0, 0],
+        [0, 0, 1, 1, 0],
+        [0, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+        [1, 1, 0, 0, 1],
+        [1, 1, 0, 0, 1],
+    ]
+    print(island_count(matrix))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n x m)
+SPACE COMPLEXITY: O(n x m)
+"""
