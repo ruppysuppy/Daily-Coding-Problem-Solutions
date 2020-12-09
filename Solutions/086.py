7190d67595cb8f5dd18dd4b5dbbba48370a8a209
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-09 08:23:48 +0530

    Refactored solution 86

--- Solutions/086.py
+++ Solutions/086.py
@@ -1,44 +1,52 @@
 """
 Problem:
 
-You are given a string of parentheses. 
-Write a function to compute the minimum number of parentheses to be removed to make the string valid 
-(i.e. each open parenthesis is eventually closed)
+Given a string of parentheses, write a function to compute the minimum number of
+parentheses to be removed to make the string valid (i.e. each open parenthesis is
+eventually closed).
 
-Example:
-
-Input = "()())()"
-Output = 1
-
-Input = ")("
-Output = 2
+For example, given the string "()())()", you should return 1. Given the string ")(",
+you should return 2, since we must remove all of them.
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def num_parentheses_remove(expression, stack=[], num_removed=0):
-    # returning num_removed if the expression and stack is empty
-    if not expression and not stack:
+from copy import deepcopy
+
+from DataStructures.Stack import Stack
+
+
+def get_min_parentheses_remove(
+    expression: str, stack: Stack = Stack(), num_removed: int = 0
+) -> int:
+    if not expression and stack.is_empty():
         return num_removed
-    # returning num_removed+stack size if the expression and stack isn't empty
     elif not expression:
         return len(stack) + num_removed
-
-    # if the last paranthesis can be balanced, its opening part is removed from the stack
-    if (expression[0] == ")") and (stack and stack[-1] == "("):
+    if (expression[0] == ")") and (not stack.is_empty() and stack.peek() == "("):
         stack.pop()
-        return num_parentheses_remove(expression[1:], stack, num_removed)
-
-    # getting the number of changes if the current paranthesis is added to stack
-    num_added_to_stack = num_parentheses_remove(
-        expression[1:], stack + [expression[0]], num_removed
+        return get_min_parentheses_remove(expression[1:], stack, num_removed)
+    # calulating the modifications for parenthesis added to stack
+    stack_copy = deepcopy(stack)
+    stack_copy.push(expression[0])
+    modifications_parenthesis_added_to_stack = get_min_parentheses_remove(
+        expression[1:], stack_copy, num_removed
     )
-    # getting the number of changes if the current paranthesis is ignored (removed)
-    num_ignored = num_parentheses_remove(expression[1:], stack, num_removed + 1)
+    # calulating the modifications for parenthesis removed
+    modifications_parenthesis_ignored = get_min_parentheses_remove(
+        expression[1:], stack, num_removed + 1
+    )
+    return min(
+        modifications_parenthesis_added_to_stack, modifications_parenthesis_ignored
+    )
 
-    # returning the minimum of the 2 (num_added_to_stack, num_ignored)
-    return min(num_added_to_stack, num_ignored)
+
+if __name__ == "__main__":
+    print(get_min_parentheses_remove("()())()"))
+    print(get_min_parentheses_remove(")("))
 
 
-# DIVER CODE
-print(num_parentheses_remove("()())()"))
-print(num_parentheses_remove(")("))
+"""
+SPECS:
+
+TIME COMPLEXITY: O(2 ^ n)
+SPACE COMPLEXITY: O(n)
+"""
