4973e664d1ed4b7e62a27d95464cd94b88e52930
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-10 09:08:34 +0530

    Refactored solution 89

--- Solutions/089.py
+++ Solutions/089.py
@@ -2,59 +2,68 @@
 Problem:
 
 Determine whether a tree is a valid binary search tree.
-A binary search tree is a tree with two children, left and right, and satisfies the BST constraint.
 
-BST constraint: the key in the left child must be less than or equal to the root and the key in the right child must be greater than or equal to the root.
+A binary search tree is a tree with two children, left and right, and satisfies the
+constraint that the key in the left child must be less than or equal to the root and
+the key in the right child must be greater than or equal to the root.
 """
 
-# importing tree from datastructure
-from DataStructures.Tree import Binary_Tree, Binary_Search_Tree, Node
+from DataStructures.Tree import BinaryTree, BinarySearchTree, Node
 
-# helper function for the main operation
-def bst_check_helper(node):
-    # if the passed node (only applicable for the root) is empty, True is returned [recursion base case]
-    if node == None:
-        return True
 
-    # if its a leaf node, True is returned [recursion base case]
-    if node.left == None and node.right == None:
+def is_binary_search_tree_helper(node: Node) -> bool:
+    if node is None:
         return True
-    # if the children nodes exist but doesn't follow the BST constraint, False is returned
+    if node.left is None and node.right is None:
+        return True
     elif (node.left and node.left.val > node.val) or (
         node.right and node.right.val < node.val
     ):
         return False
-    # returning the value for the bst check on both left and right children
-    return bst_check_helper(node.left) and bst_check_helper(node.right)
+    return is_binary_search_tree_helper(node.left) and is_binary_search_tree_helper(
+        node.right
+    )
 
 
-# FUNCTION TO PERFORM THE OPERATION
-def bst_check(tree):
-    return bst_check_helper(tree.root)
+def is_binary_search_tree(tree: BinaryTree) -> bool:
+    return is_binary_search_tree_helper(tree.root)
 
 
-# DRIVER CODE
-tree = Binary_Search_Tree()
-tree.add(5)
-tree.add(9)
-tree.add(1)
-tree.add(4)
-tree.add(10)
-tree.add(3)
-tree.add(2)
-tree.add(10)
-tree.add(7)
-print(f"BST Check Status: {bst_check(tree)}")
+if __name__ == "__main__":
+    tree1 = BinarySearchTree()
 
-tree = Binary_Search_Tree()
-print(f"BST Check Status: {bst_check(tree)}")
+    tree1.add(5)
+    tree1.add(9)
+    tree1.add(1)
+    tree1.add(4)
+    tree1.add(10)
+    tree1.add(3)
+    tree1.add(2)
+    tree1.add(10)
+    tree1.add(7)
 
-tree = Binary_Tree(5)
-tree.root.left = Node(5)
-tree.root.right = Node(5)
-print(f"BST Check Status: {bst_check(tree)}")
+    print(is_binary_search_tree(tree1))
 
-tree = Binary_Tree(5)
-tree.root.left = Node(6)
-tree.root.right = Node(5)
-print(f"BST Check Status: {bst_check(tree)}")
+    tree2 = BinaryTree()
+    tree2.root = Node(5)
+
+    tree2.root.left = Node(4)
+    tree2.root.right = Node(6)
+
+    print(is_binary_search_tree(tree2))
+
+    tree3 = BinaryTree()
+    tree3.root = Node(5)
+
+    tree3.root.left = Node(6)
+    tree3.root.right = Node(4)
+
+    print(is_binary_search_tree(tree3))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(log(n)) [recursion depth]
+"""
