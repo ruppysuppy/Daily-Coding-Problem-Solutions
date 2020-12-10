b50b1d1c8cfbdf4ff8f236a0dedcebb2bc6a9a5d
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-10 08:59:49 +0530

    Refactored solution 88

--- Solutions/088.py
+++ Solutions/088.py
@@ -1,37 +1,33 @@
 """
 Problem:
 
-Implement division of two positive integers without using the division, multiplication, or modulus operators. 
-Return the quotient as an integer, ignoring the remainder.
+Implement division of two positive integers without using the division, multiplication,
+or modulus operators. Return the quotient as an integer, ignoring the remainder.
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def div(dividend, divisor):
-    # if the divisor is 0, division is not possible
-    if divisor == 0:
-        return None
 
-    # quotient stores the result of the division
+def divide(dividend: int, divisor: int) -> int:
     quotient = 0
-
-    # while the dividend is larger than 0, it might be divided my the divisor
     while dividend > 0:
-        # divisor is subtracted from the dividend
         dividend -= divisor
-
-        # if the dividend is still larger than 0, the quotient is incremented
         if dividend >= 0:
             quotient += 1
-
-    # the quotient is returned
     return quotient
 
 
-# DRIVER CODE
-print(div(1, 0))
-print(div(1, 1))
-print(div(0, 1))
-print(div(12, 3))
-print(div(13, 3))
-print(div(25, 5))
-print(div(25, 7))
+if __name__ == "__main__":
+    print(divide(1, 0))
+    print(divide(1, 1))
+    print(divide(0, 1))
+    print(divide(12, 3))
+    print(divide(13, 3))
+    print(divide(25, 5))
+    print(divide(25, 7))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(1)
+"""
