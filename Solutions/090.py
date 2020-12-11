f452163f36b843efb6af3f258fa654b4aa48cdea
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-11 08:17:44 +0530

    Refactored solution 90

--- Solutions/090.py
+++ Solutions/090.py
@@ -1,44 +1,43 @@
 """
 Problem:
 
-Given an integer n and a list of integers l, write a function that randomly generates a number from 0 to n-1 that isn't in l (uniform).
+Given an integer n and a list of integers l, write a function that randomly generates a
+number from 0 to n-1 that isn't in l (uniform).
 """
 
-# matplotlib is used to plt the distribution (not mandatory)
-# randint is used to generate the random number
 import matplotlib.pyplot as plt
 from random import randint
+from typing import List
 
-# FUNCTION TO PERFORM THE OPERATION
-def generate_num_not_in_data(n, data):
-    # a random number is generated in the range [0, n-1]
+
+def generate_num_not_in_data(n: int, data: List[int]) -> int:
     num = randint(0, n - 1)
-
-    # if the number is not present in the supplied list, its returned; else the function is called recursively
     if num in data:
         return generate_num_not_in_data(n, data)
-
     return num
 
 
-# DRIVER CODE
-data = set([1, 3, 5])
-results = {}
+if __name__ == "__main__":
+    data = [1, 3, 5]
+    results = {}
+    for i in range(100_000):
+        val = generate_num_not_in_data(7, data)
+        if val in results:
+            results[val] += 1
+        else:
+            results[val] = 1
 
-for i in range(100000):
-    val = generate_num_not_in_data(7, data)
+    x, y = [], []
+    for i in results:
+        x.append(i)
+        y.append(results[i])
+    plt.bar(x=x, height=y, edgecolor="black")
+    plt.show()
 
-    if val in results:
-        results[val] += 1
-    else:
-        results[val] = 1
 
-x = []
-y = []
+"""
+SPECS:
 
-for i in results:
-    x.append(i)
-    y.append(results[i])
-
-plt.bar(x=x, height=y, edgecolor="black")
-plt.show()
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(1)
+"""
