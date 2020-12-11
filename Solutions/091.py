ece6b307515b4484d44275c8efdb7ef99356cb65
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-11 08:21:33 +0530

    Refactored solution 91

--- Solutions/091.py
+++ Solutions/091.py
@@ -1,7 +1,8 @@
 """
 Problem:
 
-What does the below code snippet print out? How can we fix the anonymous functions to behave as we'd expect?
+What does the below code snippet print out? How can we fix the anonymous functions to
+behave as we'd expect?
 
 functions = []
 for i in range(10):
@@ -11,9 +12,10 @@ for f in functions:
     print(f())
 """
 
-# The code is expected to print 9, 9, ..., 9 (10 times)
-# This is beacuse the value of i is set to 9 and is not changed during iteration
-# Considering we plan to print 0 to 9, we can set and update the value of i every iteration
+# The code is expected to print 9, 9, ..., 9 (10 times) as the value of i is set to 9
+# at the end of the 1st loop and is not changed during iterating through the 2nd loop.
+# Considering we plan to print 0 to 9, we can set and update the value of i every
+# iteration
 
 functions = []
 for i in range(10):
