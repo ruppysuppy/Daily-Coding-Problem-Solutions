6a6cf9c0b8dca5ab63cb4395fdd7654c0726b5f2
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-12 08:25:17 +0530

    Refactored solution 92, 93 and Tree DataStructure

--- Solutions/092.py
+++ Solutions/092.py
@@ -1,14 +1,14 @@
 """
 Problem:
 
-We're given a hashmap with a key courseId and value a list of courseIds, which represents that the prerequsite of courseId is courseIds. 
-Return a sorted ordering of courses such that we can finish all courses.
+We're given a hashmap with a key courseId and value a list of courseIds, which
+represents that the prerequsite of courseId is courseIds. Return a sorted ordering of
+courses such that we can finish all courses.
+
 Return null if there is no such ordering.
 
-Example:
-
-Input = {'CSC300': ['CSC100', 'CSC200'], 'CSC200': ['CSC100'], 'CSC100': []}
-Output = ['CSC100', 'CSC200', 'CSCS300']
+For example, given {'CSC300': ['CSC100', 'CSC200'], 'CSC200': ['CSC100'], 'CSC100': []},
+should return ['CSC100', 'CSC200', 'CSCS300'].
 """
 
 from typing import Dict, List, Optional, Set, Tuple
