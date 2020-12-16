0a8c862614797af7107443b675720a400c822a67
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-16 08:55:38 +0530

    Refactored solution 101

--- Solutions/101.py
+++ Solutions/101.py
@@ -1,64 +1,63 @@
 """
 Problem:
 
-Given an even number (greater than 2), return two prime numbers whose sum will be equal to the given number.
+Given an even number (greater than 2), return two prime numbers whose sum will be equal
+to the given number.
+
 A solution will always exist. See Goldbach’s conjecture.
 
+Example:
 
-EXAMPLE:
+numut: 4 Output: 2 + 2 = 4 If there are more than one solution possible, return the
+lexicographically smaller solution.
 
-Input: 4 
-Output: 2, 2 (2 + 2 = 4. If there are more than one solution possible, return the lexicographically smaller solution)
+If [a, b] is one solution with a <= b, and [c, d] is another solution with c <= d, then
 
-(If [a, b] is one solution with a <= b, and [c, d] is another solution with c <= d, 
-then [a, b] < [c, d] if a < c or a==c and b < d)
+[a, b] < [c, d]
+if a < c or a==c and b < d.
 """
 
-# function to check if a number is prime
-def prime(num):
-    # if the number is 2 or 3, True is returned
-    if num == 2 or num == 3:
-        return True
+from typing import Tuple
 
-    # looping over all its potential divisors
+
+def is_prime(num: int) -> bool:
+    # time complexity: O(log(n))
     for i in range(2, int(num ** 0.5) + 1):
-        # if it is divisble by any number False is returned
         if num % i == 0:
             return False
-    # if no divisor is found, True is returned
     return True
 
 
-# FUNCTION TO PERFORM THE OPERATION
-def num_sum(n):
-    # checking if n-2 is prime
-    if n > 2 and prime(n - 2):
-        return (2, n - 2)
-    # checking if n-3 is prime
-    if n > 3 and prime(n - 3):
-        return (3, n - 3)
-
-    # looping over all possible prime numbers [using formula all prime numbers are of the form (6n + 1) or (6n - 1)]
-    for i in range(6, n // 2, 6):
-        # checking if (6n - 1) satisfies the result
-        if prime(i - 1):
-            if prime(n - i + 1):
-                return ((i - 1), (n - i + 1))
-        # checking if (6n + 1) satisfies the result
-        elif prime(i + 1):
-            if prime(n - i - 1):
-                return ((i + 1), (n - i - 1))
+def get_prime_sum(num: int) -> Tuple[int, int]:
+    if num > 2 and is_prime(num - 2):
+        return 2, num - 2
+    if num > 3 and is_prime(num - 3):
+        return 3, num - 3
+    # all prime numbers are of the form (6n + 1) or (6n - 1)
+    for i in range(6, num // 2, 6):
+        if is_prime(i - 1) and is_prime(num - i + 1):
+            return (i - 1), (num - i + 1)
+        elif is_prime(i + 1) and is_prime(num - i - 1):
+            return (i + 1), (num - i - 1)
 
 
-# DRIVER CODE
-inp = 4
-res = num_sum(inp)
-print(f"{inp} = {res[0]} + {res[1]}")
+if __name__ == "__main__":
+    num = 4
+    num_1, num_2 = get_prime_sum(num)
+    print(f"{num} = {num_1} + {num_2}")
 
-inp = 10
-res = num_sum(inp)
-print(f"{inp} = {res[0]} + {res[1]}")
+    num = 10
+    num_1, num_2 = get_prime_sum(num)
+    print(f"{num} = {num_1} + {num_2}")
 
-inp = 100
-res = num_sum(inp)
-print(f"{inp} = {res[0]} + {res[1]}")
+    num = 100
+    num_1, num_2 = get_prime_sum(num)
+    print(f"{num} = {num_1} + {num_2}")
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n x log(n))
+SPACE COMPLEXITY: O(1)
+"""
