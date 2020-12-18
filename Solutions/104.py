039b139160cc851597fbbd33e33628bd348a7708
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-18 08:43:03 +0530

    Refactored solution 104

--- Solutions/104.py
+++ Solutions/104.py
@@ -3,79 +3,59 @@ Problem:
 
 Determine whether a doubly linked list is a palindrome. What if it’s singly linked?
 
-Example:
-
-Input = 1 -> 4 -> 3 -> 4 -> 1 
-Output = true
-
-Input = 1 -> 4 
-Output = false
+For example, 1 -> 4 -> 3 -> 4 -> 1 returns true while 1 -> 4 returns false.
 """
 
-# local import from the DataStructures module
-from DataStructures.LinkedList import Linked_list
+from DataStructures.LinkedList import LinkedList
 
-# FUNCTION TO PERFORM THE OPERATION
-def palindrome(self):
-    # if the linked list is empty, False is returned
-    if self.head == None:
-        return False
 
-    # if the list contains only 1 element, True is returned
-    elif self.rear == self.head:
+def is_palindrome(ll: LinkedList) -> bool:
+    if ll.head is None:
+        return True
+    elif ll.rear == ll.head:
         return True
 
-    else:
-        # pos1 stores the addresses for comparison from the beginning
-        pos1 = self.head
-        # pos2 stores the addresses for comparison from the end
-        pos2 = self.rear
-
-        # comparing all the values from the beginning to the end
-        for i in range((self.length + 1) // 2):
-            # if there's a mismatch, False is returned
-            if pos1.val != pos2.val:
-                return False
-
-            # updating the end pointer (pos2)   [operation done for Single Linked List, so its of O(n^2), for Double Linked List, it would be O(n)]
-            pos = pos1
-            for _ in range((self.length - (2 * i)) - 2):
-                pos = pos.next
-            pos2 = pos
-
-            # updating the beginning pointer (pos1)
-            pos1 = pos1.next
-
-        return True
+    pos1 = ll.head
+    pos2 = ll.rear
+    for i in range((ll.length + 1) // 2):
+        if pos1.val != pos2.val:
+            return False
+        # updating the end pointer
+        pos = pos1
+        for _ in range((ll.length - (2 * i)) - 2):
+            pos = pos.next
+        pos2 = pos
+        # updating the start pointer
+        pos1 = pos1.next
+    return True
 
 
-# adding the function to the Linked List class
-setattr(Linked_list, "palindrome", palindrome)
+if __name__ == "__main__":
+    LL = LinkedList()
+    for i in [1, 4, 3, 2, 3, 4, 1]:
+        LL.add(i)
+    print("Palindrome: {}\t\tList: {}".format(is_palindrome(LL), LL))
 
-# DRIVER CODE
-LL = Linked_list()
-LL.add(1)
-LL.add(4)
-LL.add(3)
-LL.add(2)
-LL.add(3)
-LL.add(4)
-LL.add(1)
+    LL = LinkedList()
+    for i in [1, 4, 3]:
+        LL.add(i)
+    print("Palindrome: {}\t\tList: {}".format(is_palindrome(LL), LL))
 
-print("Palindrome: {}\t\tList: {}".format(LL.palindrome(), LL))
+    LL = LinkedList()
+    for i in [1]:
+        LL.add(i)
+    print("Palindrome: {}\t\tList: {}".format(is_palindrome(LL), LL))
 
-LL = Linked_list()
-LL.add(1)
-LL.add(4)
-LL.add(3)
+    LL = LinkedList()
+    print("Palindrome: {}\t\tList: {}".format(is_palindrome(LL), LL))
 
-print("Palindrome: {}\t\tList: {}".format(LL.palindrome(), LL))
 
-LL = Linked_list()
-LL.add(1)
+"""
+SPECS:
 
-print("Palindrome: {}\t\tList: {}".format(LL.palindrome(), LL))
-
-LL = Linked_list()
-
-print("Palindrome: {}\t\tList: {}".format(LL.palindrome(), LL))
+TIME COMPLEXITY: O(n ^ 2)
+SPACE COMPLEXITY: O(1)
+[This problem can be reduced to O(n) time & space by caching the Linked List in an
+array]
+[If a Double Linked List is used, the problem is reduced to O(n) time & O(1) space]
+"""
