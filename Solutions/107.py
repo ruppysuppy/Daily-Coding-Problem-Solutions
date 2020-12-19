30ec7f26e0b7083e858c38d5f0e70cb284d6535d
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-19 08:38:26 +0530

    Refactored solution 107

--- Solutions/107.py
+++ Solutions/107.py
@@ -1,56 +1,57 @@
 """
 Problem:
 
-Print the nodes in a binary tree level-wise. 
+Print the nodes in a binary tree level-wise. For example, the following should print
+1, 2, 3, 4, 5.
 
-Example:
-
-Input =   1
-         / \
-        2   3
-           / \
-          4   5
-Output = 1, 2, 3, 4, 5
+  1
+ / \
+2   3
+   / \
+  4   5
 """
 
-# local import from the DataStructures
-from DataStructures.Tree import Binary_Tree, Node
+from typing import List
 
-# bfs function to get all the nodes level-wise
-def lvl_wise_nodes(self):
-    # queue to store the order of processing
-    queue = [self.root]
-    # ans stores the visited nodes
+from DataStructures.Queue import Queue
+from DataStructures.Tree import BinaryTree, Node
+
+
+def get_lvl_wise_nodes(tree: BinaryTree) -> List[Node]:
+    # using bfs to generate the list of nodes by level
+    if not tree.root:
+        return []
+
+    queue = Queue()
+    queue.enqueue(tree.root)
     ans = []
-
-    # looping till all nodes are processed
-    while queue != []:
-        # getting the current node
-        pos = queue.pop(0)
-
-        # if the node has a left child, its added to the queue for processing
-        if pos.left != None:
-            queue.append(pos.left)
-
-        # if the node has a right child, its added to the queue for processing
-        if pos.right != None:
-            queue.append(pos.right)
-
-        # adding the current node's value to the ans
-        ans.append(pos.val)
-
+    while not queue.is_empty():
+        node = queue.dequeue()
+        if node.left is not None:
+            queue.enqueue(node.left)
+        if node.right is not None:
+            queue.enqueue(node.right)
+        ans.append(node.val)
     return ans
 
 
-# adding the function to the Binary Tree class
-setattr(Binary_Tree, "lvl_wise_nodes", lvl_wise_nodes)
+if __name__ == "__main__":
+    tree = BinaryTree()
+    tree.root = Node(1)
 
-# DRIVER CODE
-tree = Binary_Tree(1)
-tree.root.left = Node(2)
-tree.root.right = Node(3)
-tree.root.right.left = Node(4)
-tree.root.right.right = Node(5)
+    tree.root.left = Node(2)
+    tree.root.right = Node(3)
 
-print(f"Tree: {tree}")
-print(f"Level wise result: {tree.lvl_wise_nodes()}")
+    tree.root.right.left = Node(4)
+    tree.root.right.right = Node(5)
+
+    print(f"Tree: {tree}")
+    print(f"Level wise result: {get_lvl_wise_nodes(tree)}")
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(n)
+"""
