ab5f5366383dd377f126602f5e43223261697ec6
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-19 09:04:08 +0530

    Refactored solution 109

--- Solutions/109.py
+++ Solutions/109.py
@@ -1,33 +1,35 @@
 """
 Problem:
 
-Given an unsigned 8-bit integer, swap its even and odd bits. 
-The 1st and 2nd bit should be swapped, the 3rd and 4th bit should be swapped, and so on.
+Given an unsigned 8-bit integer, swap its even and odd bits. The 1st and 2nd bit should
+be swapped, the 3rd and 4th bit should be swapped, and so on.
+
+For example, 10101010 should be 01010101. 11100010 should be 11010001.
+
 Bonus: Can you do this in one line?
-
-Example:
-
-Input = 10101010 
-Output = 01010101
-
-Input = 11100010 
-Output = 11010001
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def swap_bits(num):
-    # displaying the number in binary
-    print("Num:", bin(num))
 
-    # 85 is the filter '01010101'
+def swap_bits(num: int) -> int:
+    # (left shift digits at odd position) BITWISE-OR (right shift digits at even
+    # position)
+    # NOTE: If the value of filter mask is placed in the expression, it reduces to one
+    # liner
     filter_mask = 85
-    # (using the mask to get the digits at odd position and left shifting) BITWISE-OR (using the mask to get the digits at even position and right shifting)
     return ((num & filter_mask) << 1) | ((num & (filter_mask << 1)) >> 1)
 
 
-# DRIVER CODE
-print("Swapped:", bin(swap_bits(0)))
-print("Swapped:", bin(swap_bits(255)))
-print("Swapped:", bin(swap_bits(210)))
-print("Swapped:", bin(swap_bits(170)))
-print("Swapped:", bin(swap_bits(226)))
+if __name__ == "__main__":
+    print("Swapped:", bin(swap_bits(0)))
+    print("Swapped:", bin(swap_bits(255)))
+    print("Swapped:", bin(swap_bits(210)))
+    print("Swapped:", bin(swap_bits(170)))
+    print("Swapped:", bin(swap_bits(226)))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(1)
+"""
