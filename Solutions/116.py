021a2984f6ede605c7b819b6a3964a4150fd5826
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-21 09:26:01 +0530

    Refactored solution 116

--- Solutions/116.py
+++ Solutions/116.py
@@ -2,91 +2,56 @@
 Problem:
 
 Generate a finite, but an arbitrarily large binary tree quickly in O(1).
+
 That is, generate() should return a tree whose size is unbounded but finite.
 """
 
-# local import from the DataStructures module
-from DataStructures.Tree import Node, Binary_Tree
-
-# library imports
 from random import random, randint
 
-# matplotlib is used to plot the distribution (not mandatory)
 import matplotlib.pyplot as plt
 
-# helper function to generate the random branches
-def generate_helper(self, probability=0.5, probability_branch=0.5):
-    # generating a random number
-    prob_res = random()
+from DataStructures.Tree import BinaryTree, Node
 
-    # if the number is larger than the probabilty, the branch generation is stopped
-    if prob_res > probability:
+
+def generate_helper(
+    node: Node,
+    probability_add_children: float = 0.5,
+    probability_add_branch: float = 0.5,
+) -> None:
+    if random() > probability_add_children:
         return
-
-    # generating random numbers for left and right branch
-    prob_res_left = random()
-    prob_res_right = random()
-
-    # generating the left branch based on probability
-    if prob_res_left < probability_branch:
-        self.left = Node(randint(1, 1000))
-        self.left.generate_helper(probability, probability_branch)
-    # generating the right branch based on probability
-    if prob_res_right < probability_branch:
-        self.right = Node(randint(1, 1000))
-        self.right.generate_helper(probability, probability_branch)
+    # generating the left branch
+    if random() < probability_add_branch:
+        node.left = Node(randint(1, 1000))
+        generate_helper(node.left, probability_add_children, probability_add_branch)
+    # generating the right branch
+    if random() < probability_add_branch:
+        node.right = Node(randint(1, 1000))
+        generate_helper(node.right, probability_add_children, probability_add_branch)
 
 
-# len function helper (member of Node class)
-def tree_length_helper(self):
-    # getting the the left height
-    if self.left:
-        left = self.left.tree_length_helper()
-    else:
-        left = 0
-
-    # getting the the right height
-    if self.right:
-        right = self.right.tree_length_helper()
-    else:
-        right = 0
-
-    # returning the sum of left, right and 1 (as the subtree consists of left and right children and the node itself)
-    return left + right + 1
-
-
-# len function for the binary tree class
-def __len__(self):
-    if self.root:
-        return self.root.tree_length_helper()
-    return 0
-
-
-# adding the necessary functions to the classses
-setattr(Node, "generate_helper", generate_helper)
-setattr(Node, "tree_length_helper", tree_length_helper)
-setattr(Binary_Tree, "__len__", __len__)
-
-# FUNCTION TO PERFORM THE OPERATION
-def generate():
-    # creating the binary tree
-    tree = Binary_Tree()
-
-    # adding the root node and generating the random branches
+def generate() -> BinaryTree:
+    tree = BinaryTree()
     tree.root = Node(randint(1, 1000))
-    tree.root.generate_helper(0.7, 0.7)
-
-    # returning the tree
+    generate_helper(tree.root, 0.7, 0.7)
+    # suggestion: don't use higher values for probability, it will lead to recursion
+    # error
     return tree
 
 
-# DRIVER CODE
-res = []
+if __name__ == "__main__":
+    tree_length_list = []
+    for i in range(1000):
+        tree_length_list.append(len(generate()))
+    plt.hist(tree_length_list)
+    plt.show()
 
-for i in range(1000):
-    res.append(len(generate()))
 
-plt.xlim(right=2500, left=100)
-plt.ylim(top=100)
-plt.hist(res, bins=50)
-plt.show()
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(n)
+[n nodes cannot be generated in O(1) time, but since n is finite it may be considered
+constant]
+"""
