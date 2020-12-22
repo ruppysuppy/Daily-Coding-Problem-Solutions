2e6130d9b2a089f25a2fd0b0889cb96a5fb2beee
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-22 09:11:56 +0530

    Refactored solution 118

--- Solutions/118.py
+++ Solutions/118.py
@@ -1,72 +1,61 @@
 """
 Problem:
 
-Given a sorted list of integers, square the elements and give the output in sorted order.
+Given a sorted list of integers, square the elements and give the output in sorted
+order.
 
-Example:
-
-Input = [-9, -2, 0, 2, 3]
-Output = [0, 4, 4, 9, 81]
+For example, given [-9, -2, 0, 2, 3], return [0, 4, 4, 9, 81].
 """
 
-# helper function to merge 2 sorted list into 1 sorted list
-def merge_sorted_lists(arr1, arr2):
-    # pointers to store the index of the current position in their respective arrays
-    ptr1 = 0
-    ptr2 = 0
-    # lengths of the respective array
-    length1 = len(arr1)
-    length2 = len(arr2)
-    # result stores the complete sorted array
-    result = []
+from typing import List
 
-    # iterating till the end of one of the arrays
+Array = List[int]
+
+
+def merge_sorted_lists(arr1: Array, arr2: Array) -> Array:
+    ptr1, length1 = 0, len(arr1)
+    ptr2, length2 = 0, len(arr2)
+    merged_sorted_array = []
+    # generating merged sorted list
     while ptr1 < length1 and ptr2 < length2:
-        # finding the smaller element of the 2 arrays' current position and adding it to the result
         if arr1[ptr1] < arr2[ptr2]:
-            result.append(arr1[ptr1])
+            merged_sorted_array.append(arr1[ptr1])
             ptr1 += 1
-
         else:
-            result.append(arr2[ptr2])
+            merged_sorted_array.append(arr2[ptr2])
             ptr2 += 1
-
-    # extending the result by the left over part of the unfinished array
-    result.extend(arr1[ptr1:])
-    result.extend(arr2[ptr2:])
-
-    return result
+    merged_sorted_array.extend(arr1[ptr1:])
+    merged_sorted_array.extend(arr2[ptr2:])
+    return merged_sorted_array
 
 
-# FUNCTION TO PERFORM THE OPERATION
-def sort_square(arr):
-    # pos_start stores the starting index of the positive numbers
-    pos_start = 0
-    # length stores the length of the array
+def sort_squared_elements(arr: Array) -> Array:
+    last_negative_position = 0
     length = len(arr)
-
-    # looping over the array
     for i in range(length):
-        # if the element is poitive, pos_start is updated and the control breaks out of the loop
         if arr[i] > 0:
-            pos_start = i
+            last_negative_position = i
             break
-    # if all elements are negative, pos_start is set to the length of the array
     else:
-        pos_start = length
+        last_negative_position = length
 
-    # generating the poitive and negative parts
-    negative_part = [elem * elem for elem in arr[:pos_start][::-1]]
-    positive_part = [elem * elem for elem in arr[pos_start:]]
-
-    # returning the final sorted list
+    negative_part = [elem * elem for elem in arr[:last_negative_position]][::-1]
+    positive_part = [elem * elem for elem in arr[last_negative_position:]]
     return merge_sorted_lists(positive_part, negative_part)
 
 
-# DRIVER CODE
-print(sort_square([]))
-print(sort_square([0]))
-print(sort_square([-1, 1]))
-print(sort_square([0, 2, 3]))
-print(sort_square([-9, -2, 0]))
-print(sort_square([-9, -2, 0, 2, 3]))
+if __name__ == "__main__":
+    print(sort_squared_elements([]))
+    print(sort_squared_elements([0]))
+    print(sort_squared_elements([-1, 1]))
+    print(sort_squared_elements([0, 2, 3]))
+    print(sort_squared_elements([-9, -2, 0]))
+    print(sort_squared_elements([-9, -2, 0, 2, 3]))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(n)
+"""
