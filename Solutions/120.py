fec01fae8d6a0d183cd72f42d79d1696af7bd33a
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-23 09:02:06 +0530

    Refactored solution 120

--- Solutions/120.py
+++ Solutions/120.py
@@ -1,51 +1,51 @@
 """
 Problem:
 
-Implement the singleton pattern with a twist. First, instead of storing one instance, store two instances. 
-And in every even call of getInstance(), return the first instance and in every odd call of getInstance(), return the second instance.
+Implement the singleton pattern with a twist. First, instead of storing one instance,
+store two instances. And in every even call of getInstance(), return the first instance
+and in every odd call of getInstance(), return the second instance.
 """
 
-# class to handle the operations
-class Twisted_Singleton:
-    # instances store the two Twisted_Singleton instances
-    # is odd stores the whether the last call was odd
-    # flag stores if the data can be initialized
-    instances = []
-    is_odd = False
-    flag = True
+from __future__ import annotations
 
-    # default initialize function
-    def __init__(self, instance_num):
+
+class Twisted_Singleton:
+    _instance1, _instance2 = None, None
+    _is_odd = True
+    _is_initialized = False
+
+    def __init__(self, instance_num: int) -> None:
         self.instance_num = instance_num
 
-    # initialize function to create all necessary data
+    def __repr__(self) -> str:
+        return str(self.instance_num)
+
     @staticmethod
-    def initialize():
-        # only if the flag is set, will the data be initialized (used to ensure it cannot be reinitialized)
-        if Twisted_Singleton.flag:
-            Twisted_Singleton.instances.append(Twisted_Singleton(1))
-            Twisted_Singleton.instances.append(Twisted_Singleton(2))
-            Twisted_Singleton.flag = False
+    def initialize() -> None:
+        if not Twisted_Singleton._is_initialized:
+            Twisted_Singleton._instance1 = Twisted_Singleton(1)
+            Twisted_Singleton._instance2 = Twisted_Singleton(2)
+            Twisted_Singleton._is_initialized = True
 
-    # FUNCTION TO PERFORM THE OPERATION
     @staticmethod
-    def getInstance():
-        # getting the current is odd
-        Twisted_Singleton.is_odd = not Twisted_Singleton.is_odd
+    def getInstance() -> Twisted_Singleton:
+        if not Twisted_Singleton._is_initialized:
+            Twisted_Singleton.initialize()
 
-        # returning the necessary data
-        if Twisted_Singleton.is_odd:
-            return Twisted_Singleton.instances[0].instance_num
+        if Twisted_Singleton._is_odd:
+            instance = Twisted_Singleton._instance1
+        else:
+            instance = Twisted_Singleton._instance2
+        Twisted_Singleton._is_odd = not Twisted_Singleton._is_odd
+        return instance
 
-        return Twisted_Singleton.instances[1].instance_num
 
+if __name__ == "__main__":
+    Twisted_Singleton.initialize()
 
-# DRIVER CODE
-Twisted_Singleton.initialize()
-
-print(Twisted_Singleton.getInstance())
-print(Twisted_Singleton.getInstance())
-print(Twisted_Singleton.getInstance())
-print(Twisted_Singleton.getInstance())
-print(Twisted_Singleton.getInstance())
-print(Twisted_Singleton.getInstance())
+    print(Twisted_Singleton.getInstance())
+    print(Twisted_Singleton.getInstance())
+    print(Twisted_Singleton.getInstance())
+    print(Twisted_Singleton.getInstance())
+    print(Twisted_Singleton.getInstance())
+    print(Twisted_Singleton.getInstance())
