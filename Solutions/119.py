457df5edd069ad51894d2ebe751ee972d4c87032
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-23 08:50:42 +0530

    Refactored solution 119

--- Solutions/119.py
+++ Solutions/119.py
@@ -1,46 +1,51 @@
 """
 Problem:
 
-Given a set of closed intervals, find the smallest set of numbers that covers all the intervals. 
-If there are multiple smallest sets, return any of them.
+Given a set of closed intervals, find the smallest set of numbers that covers all the
+intervals. If there are multiple smallest sets, return any of them.
 
-Example:
-
-Input = [[0, 3], [2, 6], [3, 4], [6, 9]]
-Output = [3, 6]
+For example, given the intervals [0, 3], [2, 6], [3, 4], [6, 9], one set of numbers
+that covers all these intervals is {3, 6}.
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def get_spanning_interval(intervals):
-    # setting the start of the spanning interval
-    start = intervals[0][1]
-    # setting the end of the spanning interval
-    end = start
-    # pos stores the number of intervals included in the first interval
-    pos = 1
+from typing import List, Optional, Tuple
 
-    # getting the number of intervals included in the 1st interval
+
+def get_spanning_interval(intervals: List[List[int]]) -> Optional[Tuple]:
+    if not intervals:
+        return
+    start = intervals[0][1]
+    end = start
+    pos = 1
+    # updating start
     for interval in intervals[1:]:
-        if interval[0] < start and interval[1] < start:
-            start = interval[1]
+        interval_start, interval_end = interval
+        if interval_start < start and interval_end < start:
+            start = interval_end
             pos += 1
         else:
             break
-
-    # updating the value of end by iterating through the intervals and checking
+    # updating end
     for interval in intervals[pos:]:
-        if interval[0] > end:
-            end = interval[0]
-
-    # returning a tuple of start and end
+        interval_start, _ = interval
+        if interval_start > end:
+            end = interval_start
     return start, end
 
 
-# DRIVER CODE
-print(get_spanning_interval([[0, 3]]))
-print(get_spanning_interval(([[0, 3], [2, 6]])))
-print(get_spanning_interval(([[0, 3], [2, 6], [3, 4]])))
-print(get_spanning_interval(([[0, 3], [2, 6], [3, 4], [6, 7]])))
-print(get_spanning_interval(([[0, 3], [2, 6], [3, 4], [6, 9]])))
-print(get_spanning_interval(([[0, 3], [2, 6], [3, 4], [6, 100]])))
-print(get_spanning_interval([[0, 4], [1, 2], [5, 6]]))
+if __name__ == "__main__":
+    print(get_spanning_interval([[0, 3]]))
+    print(get_spanning_interval([[0, 3], [2, 6]]))
+    print(get_spanning_interval([[0, 3], [2, 6], [3, 4]]))
+    print(get_spanning_interval([[0, 3], [2, 6], [3, 4], [6, 7]]))
+    print(get_spanning_interval([[0, 3], [2, 6], [3, 4], [6, 9]]))
+    print(get_spanning_interval([[0, 3], [2, 6], [3, 4], [6, 100]]))
+    print(get_spanning_interval([[0, 4], [1, 2], [5, 6]]))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(1)
+"""
