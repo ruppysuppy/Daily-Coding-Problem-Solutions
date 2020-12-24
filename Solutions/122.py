a68343c3f3e2a06d92ffad4cdef44619e98fa8c8
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-24 09:04:29 +0530

    Refactored solution 122

--- Solutions/122.py
+++ Solutions/122.py
@@ -1,36 +1,50 @@
 """
 Problem:
 
-You are given a 2-d matrix where each cell represents number of coins in that cell. 
-Assuming we start at matrix[0][0], and can only move right or down, find the maximum number of coins you can collect by the bottom right corner.
+You are given a 2-d matrix where each cell represents number of coins in that cell.
+Assuming we start at matrix[0][0], and can only move right or down, find the maximum
+number of coins you can collect by the bottom right corner.
 
-Example:
+For example, in this matrix
 
-Input  [[0, 3, 1, 1],
-        [2, 0, 0, 4],
-        [1, 5, 3, 1]]
-Output = 12 (0 + 2 + 1 + 5 + 3 + 1 = 12 coins)
+0 3 1 1
+2 0 0 4
+1 5 3 1
+The most we can collect is 0 + 2 + 1 + 5 + 3 + 1 = 12 coins.
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def get_max_coins(mat):
-    # getting n and m (rows and columns)
-    n = len(mat)
-    m = len(mat[0])
+from typing import List
 
-    # iterating through the matrix
+
+def get_max_coins(matrix: List[List[int]]) -> int:
+    n = len(matrix)
+    m = len(matrix[0])
+    # generating the maximum number of coins using dynamic programming
     for i in range(1, n):
         for j in range(1, m):
-            # updating the values of the matrix
-            mat[i][j] = max(mat[i - 1][j], mat[i][j - 1]) + mat[i][j]
-
-    # returning the result at the last column of the last row
-    return mat[n - 1][m - 1]
+            matrix[i][j] += max(matrix[i - 1][j], matrix[i][j - 1])
+    return matrix[n - 1][m - 1]
 
 
-# DRIVER CODE
-mat = [[0, 3, 1, 1], [2, 0, 0, 4], [1, 5, 3, 1]]
-print(get_max_coins(mat))
+if __name__ == "__main__":
+    matrix = [
+        [0, 3, 1, 1],
+        [2, 0, 0, 4],
+        [1, 5, 3, 1]
+    ]
+    print(get_max_coins(matrix))
 
-mat = [[0, 3, 1, 1], [2, 8, 9, 4], [1, 5, 3, 1]]
-print(get_max_coins(mat))
+    matrix = [
+        [0, 3, 1, 1],
+        [2, 8, 9, 4],
+        [1, 5, 3, 1]
+    ]
+    print(get_max_coins(matrix))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n x m)
+SPACE COMPLEXITY: O(1) [modifying the matrix in place]
+"""
