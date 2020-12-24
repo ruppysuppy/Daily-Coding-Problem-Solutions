a801e045138bcf6b64e81dbdc9892e2f2019acda
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-24 08:59:19 +0530

    Refactored solution 121

--- Solutions/121.py
+++ Solutions/121.py
@@ -3,47 +3,43 @@ Problem:
 
 Given a string which we can delete at most k, return whether you can make a palindrome.
 
-Example:
-
-Input = 'waterrfetawx', 2
-Output = True ('f' and 'x  can be deleted to yield 'waterretaw')
+For example, given 'waterrfetawx' and a k of 2, you could delete f and x to get
+'waterretaw'.
 """
 
-# function to check if a string is palindrome
-def is_palindrome(string):
+
+def is_palindrome(string: str) -> bool:
     return string == string[::-1]
 
 
-# FUNCTION TO PERFORM THE OPERATION
-def can_make_palindrome(string, k):
-    # base case 1 for recursion: if the string is a palindrome, True is returned
+def can_make_palindrome(string: str, k: int) -> bool:
     if is_palindrome(string):
         return True
-
-    # base case 2 for recursion: if k = 0, False is returned
     if not k:
         return False
-
-    # iterating over the string
+    # checking all possible combinations of the string
     for i in range(len(string)):
-        # testing all string if they are palindrome (eleminating the i'th character)
-        # recursively called function, so all possible combinations of removing k characters will be found till one is palindrome
-        # if a palindrome is encountered, True is returned
         if can_make_palindrome(string[:i] + string[i + 1 :], k - 1):
             return True
-
-    # if no palindrome can be formed, False is returned
     return False
 
 
-# DRIVER CODE
-print(can_make_palindrome("a", 0))
-print(can_make_palindrome("aaa", 2))
-print(can_make_palindrome("add", 0))
-print(can_make_palindrome("waterrfetawx", 3))
-print(can_make_palindrome("waterrfetawx", 2))
-print(can_make_palindrome("waterrfetawx", 1))
-print(can_make_palindrome("malayalam", 0))
-print(can_make_palindrome("malayalam", 1))
-print(can_make_palindrome("asdf", 5))
-print(can_make_palindrome("asdf", 2))
+if __name__ == "__main__":
+    print(can_make_palindrome("a", 0))
+    print(can_make_palindrome("aaa", 2))
+    print(can_make_palindrome("add", 0))
+    print(can_make_palindrome("waterrfetawx", 3))
+    print(can_make_palindrome("waterrfetawx", 2))
+    print(can_make_palindrome("waterrfetawx", 1))
+    print(can_make_palindrome("malayalam", 0))
+    print(can_make_palindrome("malayalam", 1))
+    print(can_make_palindrome("asdf", 5))
+    print(can_make_palindrome("asdf", 2))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(2 ^ n)
+SPACE COMPLEXITY: O(n)
+"""
