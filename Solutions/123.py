40a82d7215f8d0466ec993bea56a5249d19d87b1
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-25 08:43:47 +0530

    Refactored solution 123

--- Solutions/123.py
+++ Solutions/123.py
@@ -1,96 +1,86 @@
 """
 Problem:
 
-Given a string, return whether it represents a number. 
+Given a string, return whether it represents a number. Here are the different kinds of
+numbers:
 
-Here are the different kinds of numbers:
-* "10", a positive integer
-* "-10", a negative integer
-* "10.1", a positive real number
-* "-10.1", a negative real number
-* "1e5", a number in scientific notation
-
+"10", a positive integer
+"-10", a negative integer
+"10.1", a positive real number
+"-10.1", a negative real number
+"1e5", a number in scientific notation
 And here are examples of non-numbers:
-* "a"
-* "x 1"
-* "a -2"
-* "-"
+
+"a"
+"x 1"
+"a -2"
+"-"
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def check(string):
-    # is Valid checks whether the number is valid
-    # num_neg checks the number of occourance of "-"
-    # num_pt checks the number of occourance of "."
-    # num_e checks the number of occourance of "e"
-    # hasNum checks the occourance of a number
-    isValid = True
-    num_neg = 0
-    num_pt = 0
-    num_e = 0
-    hasNum = False
 
-    # looping over the string
-    for i in string:
-        # if the current character is not a number
-        if not (i.isdigit()):
-            # checking the number of the corresponding symbol, if the number of the same symbol is greater than 1, it is not valid [exceptions are there]
-            if i == "-":
-                if num_neg >= 1:
-                    # if the string contains an 'e' we can have 2 '-'s (for mantissa and exponent) [exception]
-                    if num_neg == 1 and num_e == 1:
-                        num_neg += 1
+def check_valid_number_representation(string: str) -> bool:
+    is_valid = True
+    has_number = False
+    num_negatives, num_points, num_e = 0, 0, 0
+
+    for char in string:
+        if not (char.isdigit()):
+            if char == "-":
+                if num_negatives >= 1:
+                    # if the string contains an 'e', 2 '-'s are allowed (for mantissa
+                    # and exponent)
+                    if num_negatives == 1 and num_e == 1:
+                        num_negatives += 1
                         continue
-
-                    isValid = False
+                    is_valid = False
                     break
-                else:
-                    num_neg += 1
-
-            elif i == ".":
-                if num_pt >= 1:
-                    # if the string contains an 'e' we can have 2 '.'s (for mantissa and exponent) [exception]
-                    if num_pt == 1 and num_e == 1:
-                        num_pt += 1
+                num_negatives += 1
+            elif char == ".":
+                if num_points >= 1:
+                    # if the string contains an 'e', 2 '.'s are allowed (for mantissa
+                    # and exponent)
+                    if num_points == 1 and num_e == 1:
+                        num_points += 1
                         continue
-
-                    isValid = False
+                    is_valid = False
                     break
-                else:
-                    num_pt += 1
-
-            elif i == "e":
+                num_points += 1
+            elif char == "e":
+                # a number can have only 1 'e'
                 if num_e >= 1:
-                    isValid = False
+                    is_valid = False
                     break
-                else:
-                    num_e += 1
-
-            # if the character is a space, its ignored
-            elif i == " ":
+                num_e += 1
+            elif char == " ":
+                # spaces are ignored
                 pass
-
-            # any other character makes the string invalid
             else:
-                isValid = False
+                # any other character makes the number invalid
+                is_valid = False
                 break
-
-        # if the current character is a number hasNum is set to True
         else:
-            hasNum = True
+            # current character is a number
+            has_number = True
+    return is_valid and has_number
 
-    return isValid and hasNum
+
+if __name__ == "__main__":
+    print(check_valid_number_representation("10"))
+    print(check_valid_number_representation("-10"))
+    print(check_valid_number_representation("10.1"))
+    print(check_valid_number_representation("-10.1"))
+    print(check_valid_number_representation("1e5"))
+    print(check_valid_number_representation("1e-5"))
+    print(check_valid_number_representation("-1.6 e -5.2"))
+    print(check_valid_number_representation("a"))
+    print(check_valid_number_representation("x 1"))
+    print(check_valid_number_representation("a -2"))
+    print(check_valid_number_representation("-"))
 
 
-# DRIVER CODE
-print(check("10"))
-print(check("-10"))
-print(check("10.1"))
-print(check("-10.1"))
-print(check("1e5"))
-print(check("1e-5"))
-print(check("-1.6 e -5.2"))
-print(check("a"))
-print(check("x 1"))
-print(check("a -2"))
-print(check("-"))
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(1)
+"""
