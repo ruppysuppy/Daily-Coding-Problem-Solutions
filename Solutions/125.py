657cbca189176f5b8163afcaa66eeec0ce5f3b76
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-26 08:48:51 +0530

    Refactored solution 125

--- Solutions/125.py
+++ Solutions/125.py
@@ -1,90 +1,76 @@
 """
 Problem:
 
-Given the root of a binary search tree, and a target K, return two nodes in the tree whose sum equals K.
+Given the root of a binary search tree, and a target K, return two nodes in the tree
+whose sum equals K.
 
+For example, given the following tree and K of 20
 
-EXAMPLE:
-
-Input = 20,      10
-               /   \
-             5      15
-                   /  \
-                 11    15
-Output = 5, 15
+    10
+   /   \
+ 5      15
+       /  \
+     11    15
+Return the nodes 5 and 15.
 """
 
-# local import from the Datastructure module
-from DataStructures.Tree import Binary_Tree, Node
+from typing import Generator, Optional, Tuple
 
-# traversal helper function for the node class (generator function for inorder traversal)
-def traverse_helper(self):
-    # if left child exists
-    if self.left:
-        # the function is called recursively to generate all children
-        for val in self.left.traverse_helper():
-            # the node's value is yielded (to keep the function in memory)
+from DataStructures.Tree import BinaryTree, Node
+
+
+def inorder_traverse_generator(node: Node) -> Generator[int, None, None]:
+    if node.left:
+        for val in inorder_traverse_generator(node.left):
             yield val
-
-    # the current node's value is yielded (to keep the function in memory)
-    yield self.val
-
-    # if right child exists
-    if self.right:
-        # the function is called recursively to generate all children
-        for val in self.right.traverse_helper():
-            # the node's value is yielded (to keep the function in memory)
+    yield node.val
+    if node.right:
+        for val in inorder_traverse_generator(node.right):
             yield val
 
 
-# traverse function for the tree class
-def traverse(self):
-    # the tree has nodes, the traversal enerator is returned
-    if self.root:
-        return self.root.traverse_helper()
-    # None is returned if the tree is empty
+def get_inorder_traverse_generator(
+    tree: BinaryTree,
+) -> Optional[Generator[int, None, None]]:
+    if tree.root:
+        return inorder_traverse_generator(tree.root)
     return None
 
 
-# adding the necessary functions to the respective classes
-setattr(Node, "traverse_helper", traverse_helper)
-setattr(Binary_Tree, "traverse", traverse)
-
-# FUNCTION TO PERFORM THE OPERATION
-def get_target_sum(tree, k):
-    # getting the generator
-    generator = tree.traverse()
-
-    # if the generator is None, the tree is empty, None is returned
+def get_target_sum(tree: BinaryTree, k: int) -> Tuple[Optional[int], Optional[int]]:
+    generator = get_inorder_traverse_generator(tree)
     if not generator:
-        return None
-
-    # previous is a set of previously seen values
+        return None, None
+    # checking for the target sum
     previous = set()
-
-    # getting the values from the generator
     for val in generator:
-        # if (k - val) is in the set, (k - val) and the current value is returned
         if (k - val) in previous:
             return (k - val), val
-        # if (k - val) is not found, the current valu is added to the set
         previous.add(val)
-
-    # if the target sum cannot be reached by 2 values in the tree, a tuple of 2 None is returned
     return None, None
 
 
-# DRIVER CODE
-tree = Binary_Tree()
-tree.root = Node(10)
-tree.root.left = Node(5)
-tree.root.right = Node(15)
-tree.root.right.left = Node(11)
-tree.root.right.right = Node(15)
+if __name__ == "__main__":
+    tree = BinaryTree()
+    tree.root = Node(10)
 
-print(get_target_sum(tree, 15))
-print(get_target_sum(tree, 20))
-print(get_target_sum(tree, 21))
-print(get_target_sum(tree, 25))
-print(get_target_sum(tree, 30))
-print(get_target_sum(tree, 35))
+    tree.root.left = Node(5)
+    tree.root.right = Node(15)
+
+    tree.root.right.left = Node(11)
+    tree.root.right.right = Node(15)
+
+    print(get_target_sum(tree, 15))
+    print(get_target_sum(tree, 20))
+    print(get_target_sum(tree, 21))
+    print(get_target_sum(tree, 25))
+    print(get_target_sum(tree, 30))
+    print(get_target_sum(tree, 35))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(n)
+"""
