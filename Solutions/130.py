23c8fe4ab816f535e5c64a655d1e239232beaed2
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-26 09:41:04 +0530

    Refactored solution 130

--- Solutions/130.py
+++ Solutions/130.py
@@ -1,62 +1,77 @@
 """
 Problem:
 
-You are given an array of numbers representing the stock prices of a company in chronological order and an integer k. 
-Return the maximum profit you can make from k buys and sells. 
-You must buy the stock before you can sell it, and you must sell the stock before you can buy it again.
+Given an array of numbers representing the stock prices of a company in chronological
+order and an integer k, return the maximum profit you can make from k buys and sells.
+You must buy the stock before you can sell it, and you must sell the stock before you
+can buy it again.
 
-Example:
-
-Input = [5, 2, 4, 0, 1], 2
-Output = 3
+For example, given k = 2 and the array [5, 2, 4, 0, 1], you should return 3.
 """
 
-# helper function for the computation
-def max_profit_helper(arr, curr_index, curr_profit, buys_left, sells_left):
-    # if the end of the array is reached or no more sells can be performed
-    # current profit is returned (base case for recursion)
-    if curr_index == len(arr) or sells_left == 0:
-        return curr_profit
+from typing import List
 
-    # if the number of buys left and sells left are equal, we have to buy a stock
+
+def get_max_profit_helper(
+    arr: List[int],
+    curr_index: int,
+    curr_profit: int,
+    buys_left: int,
+    sells_left: int,
+    length: int,
+) -> int:
+    # if the end of the array is reached or no more sells can be performed current
+    # profit is returned (base case for recursion)
+    if curr_index == length or sells_left == 0:
+        return curr_profit
+    # if the number of 'buys' and 'sells' left are equal, the stock needs to be bought
     if buys_left == sells_left:
-        # returning the max profit we can obtain
         return max(
             # wait for a different deal
-            max_profit_helper(arr, curr_index + 1, curr_profit, buys_left, sells_left),
+            get_max_profit_helper(
+                arr, curr_index + 1, curr_profit, buys_left, sells_left, length
+            ),
             # buy at the current price
-            max_profit_helper(
+            get_max_profit_helper(
                 arr,
                 curr_index + 1,
                 curr_profit - arr[curr_index],
                 buys_left - 1,
                 sells_left,
+                length,
             ),
         )
-    # if the number of buys left and sells left are inequal, we have to sell a stock
-    else:
-        # returning the max profit we can obtain
-        return max(
-            # wait and hold for selling at a different price
-            max_profit_helper(arr, curr_index + 1, curr_profit, buys_left, sells_left),
-            # sell at the current price
-            max_profit_helper(
-                arr,
-                curr_index + 1,
-                curr_profit + arr[curr_index],
-                buys_left,
-                sells_left - 1,
-            ),
-        )
+    # if the number of 'buys' and 'sells' left are inequal, the stock needs to be sold
+    return max(
+        # wait and hold for selling at a different price
+        get_max_profit_helper(
+            arr, curr_index + 1, curr_profit, buys_left, sells_left, length,
+        ),
+        # sell at the current price
+        get_max_profit_helper(
+            arr,
+            curr_index + 1,
+            curr_profit + arr[curr_index],
+            buys_left,
+            sells_left - 1,
+            length,
+        ),
+    )
 
 
-# FUNCTION TO PERFORM THE OPERATION
-def max_profit(arr, k):
-    # offloading the computation to the helper function
-    return max_profit_helper(arr, 0, 0, k, k)
+def get_max_profit(arr: List[int], k: int) -> int:
+    return get_max_profit_helper(arr, 0, 0, k, k, len(arr))
 
 
-# DRIVER CODE
-print(max_profit([5, 2, 4, 0, 1], 2))
-print(max_profit([5, 2, 4], 2))
-print(max_profit([5, 2, 4], 1))
+if __name__ == "__main__":
+    print(get_max_profit([5, 2, 4, 0, 1], 2))
+    print(get_max_profit([5, 2, 4], 2))
+    print(get_max_profit([5, 2, 4], 1))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(2 ^ n)
+SPACE COMPLEXITY: O(n)
+"""
