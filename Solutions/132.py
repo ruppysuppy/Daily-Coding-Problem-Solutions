9a6855fe63f0226e5d8bb4a1fd94b513bd91e744
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-26 10:04:59 +0530

    Rectified problem statement of question 132

--- Solutions/132.py
+++ Solutions/132.py
@@ -1,13 +1,13 @@
 """
 Problem:
 
-Design and implement a HitCounter class that keeps track of requests (or hits). 
+Design and implement a HitCounter class that keeps track of requests (or hits). It
+should support the following operations:
 
-It should support the following operations:
 * record(timestamp): records a hit that happened at timestamp
 * total(): returns the total number of hits recorded
-* range(lower, upper): returns the number of hits that occurred between timestamps lower and upper (inclusive)
-
+* range(lower, upper): returns the number of hits that occurred between timestamps lower
+                       and upper (inclusive)
 Follow-up: What if our system has limited memory?
 """
 
