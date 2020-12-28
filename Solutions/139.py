1a2a0abaa63aa2136ef1f3f3105cdcba62ac3ba3
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-28 08:42:10 +0530

    Refactored solution 139

--- Solutions/139.py
+++ Solutions/139.py
@@ -1,8 +1,10 @@
 """
 Problem:
 
-Given an iterator with methods next() and hasNext(), create a wrapper iterator, PeekableInterface, which also implements peek(). 
-peek shows the next element that would be returned on next().
+Given an iterator with methods next() and hasNext(), create a wrapper iterator,
+PeekableInterface, which also implements peek(). peek shows the next element that would
+be returned on next().
+
 Here is the interface:
 
 class PeekableInterface(object):
@@ -19,12 +21,12 @@ class PeekableInterface(object):
         pass
 """
 
-# PeekableInterface
+from typing import Any, Iterable
+
+
 class PeekableInterface(object):
-    def __init__(self, iterator):
-        # setting the iterator
+    def __init__(self, iterator: Iterable[Any]) -> None:
         self.iterator = iterator
-        # setting next_value and has_next as per data
         try:
             self.next_val = next(self.iterator)
             self.has_next = True
@@ -32,62 +34,52 @@ class PeekableInterface(object):
             self.next_val = None
             self.has_next = False
 
-    def peek(self):
-        # returning the next value upon peeking
+    def peek(self) -> Any:
         return self.next_val
 
-    def next(self):
-        # checking if the iterator has values
+    def next(self) -> Any:
         if self.has_next:
-            # storing the next value in a temporary variable
-            temp = self.next_val
+            curr_elem = self.next_val
             try:
-                # getting the next value
                 self.next_val = next(self.iterator)
             except StopIteration:
-                # if StopIteration error occours, the iterator has been exhausted
-                # next_value and has_next are updated
                 self.next_val = None
                 self.has_next = False
-            # returning temp
-            return temp
-        # returning None if the iterator has been exhausted
-        else:
-            return None
+            return curr_elem
+        return None
 
-    def hasNext(self):
-        # returning has_next
+    def hasNext(self) -> bool:
         return self.has_next
 
 
-# DRIVER CODE
-sample_list = [1, 2, 3, 4, 5]
-iterator = iter(sample_list)
-peekable = PeekableInterface(iterator)
+if __name__ == "__main__":
+    sample_list = [1, 2, 3, 4, 5]
+    iterator = iter(sample_list)
+    peekable = PeekableInterface(iterator)
 
-print(peekable.peek())
-print(peekable.hasNext())
+    print(peekable.peek())
+    print(peekable.hasNext())
 
-print(peekable.next())
-print(peekable.next())
-print(peekable.next())
+    print(peekable.next())
+    print(peekable.next())
+    print(peekable.next())
 
-print(peekable.peek())
-print(peekable.hasNext())
+    print(peekable.peek())
+    print(peekable.hasNext())
 
-print(peekable.next())
-print(peekable.hasNext())
-print(peekable.peek())
-print(peekable.next())
+    print(peekable.next())
+    print(peekable.hasNext())
+    print(peekable.peek())
+    print(peekable.next())
 
-print(peekable.hasNext())
-print(peekable.peek())
+    print(peekable.hasNext())
+    print(peekable.peek())
 
-print()
+    print()
 
-sample_list = []
-iterator = iter(sample_list)
-peekable = PeekableInterface(iterator)
+    sample_list = []
+    iterator = iter(sample_list)
+    peekable = PeekableInterface(iterator)
 
-print(peekable.peek())
-print(peekable.hasNext())
+    print(peekable.peek())
+    print(peekable.hasNext())
