924ab3d6928b9c6cfbadb24c2a12ef54785526f8
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-28 08:51:59 +0530

    Refactored solution 140

--- Solutions/140.py
+++ Solutions/140.py
@@ -1,47 +1,44 @@
 """
 Problem:
 
-You are given an array of integers in which two elements appear exactly once and all other elements appear exactly twice. 
-Find the two elements that appear only once.
+Given an array of integers in which two elements appear exactly once and all other
+elements appear exactly twice, find the two elements that appear only once.
+
+For example, given the array [2, 4, 6, 8, 10, 2, 6, 10], return 4 and 8. The order does
+not matter.
+
 Follow-up: Can you do this in linear time and constant space?
-
-Example:
-
-Input = [2, 4, 6, 8, 10, 2, 6, 10]
-Output = 4, 8 (The order does not matter)
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def get_uniques(arr):
-    # checking if the array has at least 2 elements
-    if len(arr) >= 2:
-        # getting the xor of all the values
-        xor_res = 0
-
-        for val in arr:
-            xor_res = xor_res ^ val
-
-        # getting the rightmost rest bit (as at least 1 bit needs to differ for 2 numbers to be different)
-        rightmost_set_bit = xor_res & ~(xor_res - 1)
-        num1 = 0
-        num2 = 0
-
-        # using the rightmost set bit as mask to segregate the array of numbers into 2 sets
-        # performing xor for num1 and num2 based on the set to which they belong to
-        # the 2 sets are based on whether a number has rightmost_set_bit 1 or 0
-        for val in arr:
-            if val & rightmost_set_bit:
-                num1 = num1 ^ val
-            else:
-                num2 = num2 ^ val
-
-        return num1, num2
-
-    # if the array is empty or contains 1 element, 2 'None's are returned
-    else:
-        return None, None
+from typing import List, Tuple
 
 
-# DRIVER CODE
-print(get_uniques([2, 4, 6, 8, 10, 2, 6, 10]))
-print(get_uniques([2, 4, 8, 8, 10, 2, 6, 10]))
+def get_uniques(arr: List[int]) -> Tuple[int, int]:
+    xor_result = 0
+    for val in arr:
+        xor_result = xor_result ^ val
+    # using the rightmost set bit as mask to segregate the array of numbers into 2 sets
+    # performing xor for num1 and num2 based on the set to which they belong to (the 2
+    # sets are based on whether a number has rightmost_set_bit of the xor result 1 or 0)
+    rightmost_set_bit = xor_result & ~(xor_result - 1)
+    num1, num2 = 0, 0
+    for val in arr:
+        if val & rightmost_set_bit:
+            num1 = num1 ^ val
+        else:
+            num2 = num2 ^ val
+    return num1, num2
+
+
+if __name__ == "__main__":
+    print(get_uniques([2, 4, 6, 8, 10, 2, 6, 10]))
+    print(get_uniques([2, 4, 8, 8, 10, 2, 6, 10]))
+    print(get_uniques([2, 3, 8, 8, 10, 2, 1, 10]))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(1)
+"""
