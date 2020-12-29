f0f63c048751e08f6a52d71d7c3fa5ca6c4af5a9
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-12-29 08:32:08 +0530

    Refactored solution 142

--- Solutions/142.py
+++ Solutions/142.py
@@ -1,58 +1,48 @@
 """
 Problem:
 
-You're given a string consisting solely of (, ), and *. * can represent either a (, ), or an empty string. 
-Determine whether the parentheses are balanced.
+You're given a string consisting solely of (, ), and *. * can represent either a (, ),
+or an empty string. Determine whether the parentheses are balanced.
 
-Example:
-
-Input = (()*
-Output = balanced
-
-Input = (*)
-Output = balanced
-
-Input = )*(
-Output = not balanced
+For example, (()* and (*) are balanced. )*( is not balanced.
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def can_balance(string, stack=None):
-    # creating the stack if it has not been passed (1st call)
-    if stack == None:
-        stack = []
+from copy import deepcopy
 
-    # if both the string and stack are empty, the parenthesis is balanced (base case for recursion)
-    if not string and not stack:
+from DataStructures.Stack import Stack
+
+
+def can_balance_parentheses(string: str, stack: Stack = Stack()) -> bool:
+    if not string and stack.is_empty():
         return True
-    # if the string is empty and stack isn't, the parenthesis is not balanced (base case for recursion)
     elif not string:
         return False
-
-    # if the 1st element is an opening parenthesis, its added to the stack and the function called recursively
+    # checking if the parentheses can be balanced
     if string[0] == "(":
-        stack.append("(")
-        return can_balance(string[1:], stack)
-
-    # if the 1st element is an closing parenthesis, if the parenthesis can be balanced the function called recursively
-    # else False is returned
+        stack.push("(")
+        return can_balance_parentheses(string[1:], stack)
     elif string[0] == ")":
-        if stack and stack[-1] == "(":
+        if not stack.is_empty() and stack.peek() == "(":
             stack.pop()
-            return can_balance(string[1:], stack)
-        else:
-            return False
-
-    # if the 1st element is '*', all the possible combinations are checked and if any of them can be balanced, True is returned
+            return can_balance_parentheses(string[1:], stack)
+        return False
     elif string[0] == "*":
         return (
-            can_balance("(" + string[1:], list(stack))
-            or can_balance(")" + string[1:], list(stack))
-            or can_balance(string[1:], list(stack))
+            can_balance_parentheses("(" + string[1:], deepcopy(stack))
+            or can_balance_parentheses(")" + string[1:], deepcopy(stack))
+            or can_balance_parentheses(string[1:], deepcopy(stack))
         )
 
 
-# DRIVER CODE
-print(can_balance("(()*"))
-print(can_balance("(*)"))
-print(can_balance(")*("))
+if __name__ == "__main__":
+    print(can_balance_parentheses("(()*", Stack()))
+    print(can_balance_parentheses("(*)", Stack()))
+    print(can_balance_parentheses(")*(", Stack()))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(3 ^ n)
+SPACE COMPLEXITY: O(3 ^ n)
+"""
