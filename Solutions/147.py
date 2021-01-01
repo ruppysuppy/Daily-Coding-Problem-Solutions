bfd3368205d0ff25cd51994d64f9b68066f62cd1
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-01 08:41:38 +0530

    Refactored solution 147

--- Solutions/147.py
+++ Solutions/147.py
@@ -1,30 +1,36 @@
 """
 Problem:
 
-Given a list, sort it using this method: reverse(lst, i, j), which reverses lst from i to j.
+Given a list, sort it using this method: reverse(lst, i, j), which sorts lst from i to
+j.
 """
 
-# reverse function implementation
-def reverse(lst, i, j):
+from typing import List
+
+
+def reverse(lst: List[int], i: int, j: int) -> None:
     lst[i : j + 1] = lst[i : j + 1][::-1]
 
 
-# FUNCTION TO PERFORM THE OPERATION
-def sort(lst):
-    # using basic bubble sort to sort the entire list
+def bubble_sort(lst: List[int]) -> List[int]:
     length = len(lst)
-
     for i in range(length - 1):
         for j in range(length - i - 1):
-            # reversing the items if the value at position j is larger than that at position i
             if lst[j] > lst[j + 1]:
                 reverse(lst, j, j + 1)
-
     return lst
 
 
-# DRIVER CODE
-print(sort([0, 6, 4, 2, 5, 3, 1]))
-print(sort([0, 6, 4, 2, 5, 3, 1, 10, 9]))
-print(sort([0, 6, 4, 2, 5, 3, 1, 2, 3]))
-print(sort([0, 6, 4, 2, 5, 3, 1, 11]))
+if __name__ == "__main__":
+    print(bubble_sort([0, 6, 4, 2, 5, 3, 1]))
+    print(bubble_sort([0, 6, 4, 2, 5, 3, 1, 10, 9]))
+    print(bubble_sort([0, 6, 4, 2, 5, 3, 1, 2, 3]))
+    print(bubble_sort([0, 6, 4, 2, 5, 3, 1, 11]))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n ^ 2)
+SPACE COMPLEXITY: O(1) [since reverse() is being used on adjacent indices]
+"""
