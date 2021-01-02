faf187bb793d6a3cc5d4aadb4d337abfe5997e7e
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-02 09:57:28 +0530

    Refactored solution 153

--- Solutions/153.py
+++ Solutions/153.py
@@ -1,53 +1,52 @@
 """
 Problem:
 
-Find an efficient algorithm to find the smallest distance (measured in number of words) between any two given words in a string.
+Find an efficient algorithm to find the smallest distance (measured in number of words)
+between any two given words in a string.
 
-Example:
-
-Input = "hello", "world", "dog cat hello cat dog dog hello cat world"
-Output = 1 (as there's only one word "cat" in between the two words)
+For example, given words "hello", and "world" and a text content of "dog cat hello cat
+dog dog hello cat world", return 1 because there's only one word "cat" in between the
+two words.
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def dist_calc(text, word1, word2):
-    # getting the list of words
+
+def calculate_distance(text: str, word1: str, word2: str) -> int:
     word_list = text.split()
-    # getting the number of words
     length = len(word_list)
-    # initializing dist, pos and last
-    dist = None
-    pos = None
-    last = None
-
-    # iterating over the list of words
+    distance, position, last_match = None, None, None
+    # searching for the smallest distance
     for i in range(length):
-        # if the current word is a match
-        if word_list[i] == word1 or word_list[i] == word2:
-            # if the last has not been set, its updated along with pos
-            if last == None:
-                last = word_list[i]
-                pos = i
-            # if the same word is encountered again, pos is updated
-            elif last == word_list[i]:
-                pos = i
+        if word_list[i] in (word1, word2):
+            if last_match in (word_list[i], None):
+                last_match = word_list[i]
+                position = i
+                continue
+            current_distance = i - position - 1
+            last_match = word_list[i]
+            position = i
+            if distance == None:
+                distance = current_distance
             else:
-                # the last and pos values are updated and temp stores the current distance
-                temp = temp = i - pos - 1
-                last = word_list[i]
-                pos = i
-
-                # the minimum distance is stored in dist
-                if dist == None:
-                    dist = temp
-                else:
-                    dist = min(dist, temp)
-
-    return dist
+                distance = min(distance, current_distance)
+    return distance
 
 
-# DRIVER CODE
-print(dist_calc("dog cat hello cat dog dog hello cat world", "hello", "world"))
-print(dist_calc("dog cat hello cat dog dog hello cat world", "world", "dog"))
-print(dist_calc("hello world", "hello", "world"))
-print(dist_calc("hello", "hello", "world"))
+if __name__ == "__main__":
+    print(
+        calculate_distance(
+            "dog cat hello cat dog dog hello cat world", "hello", "world"
+        )
+    )
+    print(
+        calculate_distance("dog cat hello cat dog dog hello cat world", "world", "dog")
+    )
+    print(calculate_distance("hello world", "hello", "world"))
+    print(calculate_distance("hello", "hello", "world"))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(1)
+"""
