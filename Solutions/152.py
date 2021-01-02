d377b4fea7c6fbafe2f3763cb3c8fe6606b1653f
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-02 09:40:58 +0530

    Refactored solution 152

--- Solutions/152.py
+++ Solutions/152.py
@@ -1,41 +1,39 @@
 """
 Problem:
 
-You are given n numbers as well as n probabilities that sum up to 1. 
-Write a function to generate one of the numbers with its corresponding probability.
+You are given n numbers as well as n probabilities that sum up to 1. Write a function
+to generate one of the numbers with its corresponding probability.
+
+For example, given the numbers [1, 2, 3, 4] and probabilities [0.1, 0.5, 0.2, 0.2],
+your function should return 1 10% of the time, 2 50% of the time, and 3 and 4 20% of
+the time.
+
 You can generate random numbers between 0 and 1 uniformly.
-
-Example:
-
-Input = [1, 2, 3, 4], [0.1, 0.5, 0.2, 0.2] 
-Output = your function should return 1 10% of the time, 2 50% of the time, and 3 and 4 20% of the time.
 """
 
-from random import random
 import matplotlib.pyplot as plt
+from random import random
+from typing import List
 
 
 class RandomGenerator:
-    def __init__(self, numbers, probabilities):
+    def __init__(self, numbers: List[int], probabilities: List[float]) -> None:
         self.numbers = numbers
         self.probabilities = probabilities
 
-    def generate(self):
-        temp = random()
+    def generate(self) -> int:
+        check = random()
         cumulative = 0
-
         for pos in range(len(self.probabilities)):
             cumulative += self.probabilities[pos]
-
-            if cumulative >= temp:
+            if cumulative >= check:
                 return self.numbers[pos]
 
 
-generator = RandomGenerator([1, 2, 3, 4], [0.1, 0.5, 0.2, 0.2])
-res = []
-
-for _ in range(1, 100000):
-    res.append(generator.generate())
-
-plt.hist(res)
-plt.show()
+if __name__ == "__main__":
+    generator = RandomGenerator([1, 2, 3, 4], [0.1, 0.5, 0.2, 0.2])
+    nums = []
+    for _ in range(1, 100_000):
+        nums.append(generator.generate())
+    plt.hist(nums)
+    plt.show()
