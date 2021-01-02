b06e4eb2a03584aa2295b7ca4fcadd55a47cbef0
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-02 08:48:12 +0530

    Refactored solution 149

--- Solutions/149.py
+++ Solutions/149.py
@@ -1,45 +1,37 @@
 """
 Problem:
 
-Given a list of numbers L, implement a method sum(i, j) which returns the sum from the sublist L[i:j] (including i, excluding j).
-You can assume that you can do some pre-processing. sum() should be optimized over the pre-processing step.
+Given a list of numbers L, implement a method sum(i, j) which returns the sum from the
+sublist L[i:j] (including i, excluding j).
 
+For example, given L = [1, 2, 3, 4, 5], sum(1, 3) should return sum([2, 3]), which is 5.
 
-EXAMPLE:
-
-Input = [1, 2, 3, 4, 5], 1, 3 
-Output = 5 (sum([2, 3]))
+You can assume that you can do some pre-processing. sum() should be optimized over the
+pre-processing step.
 """
 
-# class to optimize the array and perform the sum over range
+from typing import List
+
+
 class SubarraySumOptimizer:
-    # default initialization function
-    def __init__(self, arr):
-        # creating a preprocessed array
+    def __init__(self, arr: List[int]) -> None:
+        # runs in O(n) time, O(n) space
         self.preprocessed_arr = [0 for _ in range(len(arr) + 1)]
-        # sum current stores the sum till the current element
-        sum_curr = 0
-
-        # preprocessing the array
         for i in range(len(arr)):
-            sum_curr += arr[i]
-            self.preprocessed_arr[i + 1] = sum_curr
+            self.preprocessed_arr[i + 1] = self.preprocessed_arr[i] + arr[i]
 
-    # FUNCTION TO PERFORM THE OPERATION
-    def sum(self, start, end):
-        # checking if the query is valid, returns 0 for invalid query
+    def sum(self, start: int, end: int) -> int:
+        # runs in O(1) time, O(1) space
+        # NOTE: the sum is supposed to return the sum in the range [start, end)
         if (start < 0) or (end > len(self.preprocessed_arr) - 1) or (start > end):
             return 0
-
-        # returning the difference (as the array has been preprocessed)
         return self.preprocessed_arr[end] - self.preprocessed_arr[start]
 
 
-# DRIVER CODE
-sso = SubarraySumOptimizer([1, 2, 3, 4, 5])
+if __name__ == "__main__":
+    sso = SubarraySumOptimizer([1, 2, 3, 4, 5])
 
-print(sso.sum(1, 3))
-print(sso.sum(0, 5))
-print(sso.sum(0, 4))
-print(sso.sum(3, 4))
-print(sso.sum(3, 3))
+    print(sso.sum(1, 3))
+    print(sso.sum(0, 5))
+    print(sso.sum(0, 4))
+    print(sso.sum(3, 4))
