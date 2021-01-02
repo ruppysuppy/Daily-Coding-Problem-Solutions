fdc22c28f416d3ba2bda90e3f29abbb62b6353ea
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-02 08:55:42 +0530

    Refactored solution 150

--- Solutions/150.py
+++ Solutions/150.py
@@ -1,29 +1,33 @@
 """
 Problem:
 
-Given a list of points, a central point, and an integer k, find the nearest k points from the central point.
+Given a list of points, a central point, and an integer k, find the nearest k points
+from the central point.
 
-Example:
-
-Input = [(0, 0), (5, 4), (3, 1)], (1, 2), 2, 
-Output = [(0, 0), (3, 1)]
+For example, given the list of points [(0, 0), (5, 4), (3, 1)], the central point
+(1, 2), and k = 2, return [(0, 0), (3, 1)].
 """
 
-# helper function to calculate the distance
-def get_dist(point1, point2):
-    # finding the distance using Pythagora's theorem
+from typing import List, Tuple
+
+Position = Tuple[int, int]
+
+
+def get_distance(point1: Position, point2: Position) -> float:
     return ((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2) ** 0.5
 
 
-# FUNCTION TO PERFORM THE OPERATION
-def KNN(arr, center, k):
-    # getting the points sorted by distance and returning the first k values
-    return sorted(arr, key=lambda pt: get_dist(pt, center))[:k]
+def KNN(arr: List[Position], center: Position, k: int) -> List[Position]:
+    return sorted(arr, key=lambda position: get_distance(position, center))[:k]
 
 
-# DRIVER CODE
-arr = [(0, 0), (5, 4), (3, 1)]
-center = (1, 2)
-k = 2
+if __name__ == "__main__":
+    print(KNN([(0, 0), (5, 4), (3, 1)], (1, 2), 2))
 
-print(KNN(arr, center, k))
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n x log(n))
+SPACE COMPLEXITY: O(k)
+"""
