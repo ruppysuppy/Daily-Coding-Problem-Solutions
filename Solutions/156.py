f0f4924a078009b279c1571f7ec566969c1c4467
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-03 08:44:10 +0530

    Refactored solution 156

--- Solutions/156.py
+++ Solutions/156.py
@@ -3,37 +3,33 @@ Problem:
 
 Given a positive integer n, find the smallest number of squared integers which sum to n.
 
-Example:
+For example, given n = 13, return 2 since 13 = 3^2 + 2^2 = 9 + 4.
 
-Input = 13
-Output = 2 (since 13 = 3^2 + 2^2 = 9 + 4)
-
-Input = 27
-Output = 3 (since 27 = 3^2 + 3^2 + 3^2 = 9 + 9 + 9)
+Given n = 27, return 3 since 27 = 3^2 + 3^2 + 3^2 = 9 + 9 + 9.
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def min_sq_num(num, accumulator=0):
-    # base case for recursion 1
+
+def min_square_num(num: int, accumulator: int = 0) -> int:
     if num == 0:
         return accumulator
-    # base case for recursion 2
     elif num == 1:
         return accumulator + 1
 
-    else:
-        # getting the largest square number that is smaller than the current number
-        largest_sq_divisor = int(num ** 0.5) ** 2
-
-        # updating the number and incrementing accumulator
-        num = num - largest_sq_divisor
-        accumulator += 1
-
-        # calling the function recursively
-        return min_sq_num(num, accumulator)
+    largest_square_divisor = int(num ** 0.5) ** 2
+    num = num - largest_square_divisor
+    accumulator += 1
+    return min_square_num(num, accumulator)
 
 
-# DRIVER CODE
-print(min_sq_num(25))  # (5 ^ 2)
-print(min_sq_num(13))  # (2 ^ 2) + (3 ^ 2)
-print(min_sq_num(27))  # (5 ^ 2) + (1 ^ 2) + (1 ^ 2)
+if __name__ == "__main__":
+    print(min_square_num(25))  # (5 ^ 2)
+    print(min_square_num(13))  # (2 ^ 2) + (3 ^ 2)
+    print(min_square_num(27))  # (5 ^ 2) + (1 ^ 2) + (1 ^ 2)
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(log(n))
+"""
