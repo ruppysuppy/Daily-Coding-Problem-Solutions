d178fbad82bddd99b39c044ff99a3fe6417b6540
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-03 08:41:31 +0530

    Refactored solution 155

--- Solutions/155.py
+++ Solutions/155.py
@@ -1,42 +1,42 @@
 """
 Problem:
 
-Given a list of elements, find the majority element, which appears more than half the times (> floor(len(lst) / 2.0)).
+Given a list of elements, find the majority element, which appears more than half the
+times (> floor(len(lst) / 2.0)).
+
 You can assume that such an element exists.
 
-Example:
-
-Input = [1, 2, 1, 1, 3, 4, 0]
-Output = 1
+For example, given [1, 2, 1, 1, 3, 4, 0], return 1.
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def majority_element(arr):
-    # getting the length
-    length = len(arr)
+from typing import List, Optional
 
-    # returning the values based on length
+
+def majority_element(arr: List[int]) -> Optional[int]:
+    length = len(arr)
     if not length:
         return
     elif length < 3:
         return arr[0]
-
-    # freq hash map to store the frequency of each element
-    freq = {}
-
-    # iterating through the array and generating freq
+    # getting the majority element by generating the frequencies
+    frequency = {}
     for elem in arr:
-        if elem in freq:
-            freq[elem] += 1
-        else:
-            freq[elem] = 1
-
-    # iterating through freq and finding the majority element
-    for elem in freq:
-        if freq[elem] > (length // 2):
+        if elem not in frequency:
+            frequency[elem] = 0
+        frequency[elem] += 1
+    for elem in frequency:
+        if frequency[elem] > (length // 2):
             return elem
 
 
-# DRIVER CODE
-print(majority_element([1, 2, 1, 1, 1, 4, 0]))
-print(majority_element([1, 1, 1, 3, 3, 3, 4, 1, 1]))
+if __name__ == "__main__":
+    print(majority_element([1, 2, 1, 1, 1, 4, 0]))
+    print(majority_element([1, 1, 1, 3, 3, 3, 4, 1, 1]))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(n)
+"""
