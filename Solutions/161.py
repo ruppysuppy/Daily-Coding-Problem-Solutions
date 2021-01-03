6c385820069d7e47273aa04354c9f7ee046a0141
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-03 09:35:06 +0530

    Refactored solution 161

--- Solutions/161.py
+++ Solutions/161.py
@@ -3,25 +3,25 @@ Problem:
 
 Given a 32-bit integer, return the number with its bits reversed.
 
-Example:
-
-Input = 1111 0000 1111 0000 1111 0000 1111 0000
-Output = 0000 1111 0000 1111 0000 1111 0000 1111
+For example, given the binary number 1111 0000 1111 0000 1111 0000 1111 0000, return
+0000 1111 0000 1111 0000 1111 0000 1111.
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def complement_1s(num):
-    # res stores the resultant number
-    res = ""
 
-    # iterating through the number and getting each digit
-    for i in num:
-        # inverting the number and adding it to the result
-        res += str(int(not int(i)))
-
-    # returning the result
-    return res
+def complement_1s(num: str) -> str:
+    result = ""
+    for digit in num:
+        result += str(int(not int(digit)))
+    return result
 
 
-# DRIVER CODE
-print(complement_1s("11110000111100001111000011110000"))
+if __name__ == "__main__":
+    print(complement_1s("11110000111100001111000011110000"))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(1)
+"""
