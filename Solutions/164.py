0946bccec5eac57a4407e53126cafd856448ec09
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-04 08:34:06 +0530

    Refactored solution 164

--- Solutions/164.py
+++ Solutions/164.py
@@ -1,24 +1,30 @@
 """
 Problem:
 
-You are given an array of length n + 1 whose elements belong to the set {1, 2, ..., n}. 
-By the pigeonhole principle, there must be a duplicate. 
-Find it in linear time and space.
+You are given an array of length n + 1 whose elements belong to the set {1, 2, ..., n}.
+By the pigeonhole principle, there must be a duplicate. Find it in linear time and
+space.
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def find_duplicate(arr):
-    # seen set to get access to the seen values in O(1) time
-    seen = set()
-
-    # iterating through the array and populating seen
-    for i in arr:
-        # when a previously seen element is encountered again, its returned
-        if i in seen:
-            return i
-        seen.add(i)
+from typing import List
 
 
-# DRIVER CODE
-print(find_duplicate([1, 2, 4, 6, 5, 3, 2]))
-print(find_duplicate([3, 1, 4, 2, 3]))
+def find_duplicate(arr: List[int]) -> int:
+    seen_numbers = set()
+    for num in arr:
+        if num in seen_numbers:
+            return num
+        seen_numbers.add(num)
+
+
+if __name__ == "__main__":
+    print(find_duplicate([1, 2, 4, 6, 5, 3, 2]))
+    print(find_duplicate([3, 1, 4, 2, 3]))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(n)
+"""
