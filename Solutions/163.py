55c0d838b1e2f2c768494414771a93e62f440253
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-04 08:32:05 +0530

    Refactored solution 163

--- Solutions/163.py
+++ Solutions/163.py
@@ -1,49 +1,51 @@
 """
 Problem:
 
-Given an arithmetic expression in Reverse Polish Notation, write a program to evaluate it.
-The expression is given as a list of numbers and operands. 
+Given an arithmetic expression in Reverse Polish Notation, write a program to evaluate
+it.
+
+The expression is given as a list of numbers and operands. For example: [5, 3, '+']
+should return 5 + 3 = 8.
+
+For example, [15, 7, 1, 1, '+', '-', '/', 3, '*', 2, 1, 1, '+', '+', '-'] should return
+5, since it is equivalent to ((15 / (7 - (1 + 1))) * 3) - (2 + (1 + 1)) = 5.
+
 You can assume the given expression is always valid.
-
-Example:
-
-Input = [5, 3, '+']
-Output = 8 [5 + 3]
-
-Input = [15, 7, 1, 1, '+', '-', '/', 3, '*', 2, 1, 1, '+', '+', '-'] 
-Output = 5 [((15 / (7 - (1 + 1))) * 3) - (2 + (1 + 1))]
 """
 
-# acceptable functions
+from typing import List, Union
+
+from DataStructures.Stack import Stack
+
 FUNCTIONS = {
     "+": lambda a, b: a + b,
     "-": lambda a, b: a - b,
     "*": lambda a, b: a * b,
-    "/": lambda a, b: a / b,  # use // instead of / for integer division
+    "/": lambda a, b: a / b,
 }
 
-# FUNCTION TO PERFORM THE OPERATION
-def calculate(expression_list):
-    # getting the functions
-    global FUNCTIONS
-    # delaring a stack
-    stack = []
 
-    # iterating through the expressions list
+def calculate(expression_list: List[Union[int, str]]) -> Union[float, int]:
+    stack = Stack()
+    # calculating the expression
     for expression in expression_list:
-        # if the expression is an operation, the operation is performed and the result added to the stack
         if expression in FUNCTIONS:
             a = stack.pop()
             b = stack.pop()
-            stack.append(FUNCTIONS[expression](a, b))
-        # if the expression is an operand, its added to the stack
+            stack.push(FUNCTIONS[expression](a, b))
         else:
-            stack.append(expression)
-
-    # returning the final result
+            stack.push(expression)
     return stack[0]
 
 
-# DRIVER CODE
-print(calculate([5, 3, "+"]))
-print(calculate([15, 7, 1, 1, "+", "-", "/", 3, "*", 2, 1, 1, "+", "+", "-"]))
+if __name__ == "__main__":
+    print(calculate([5, 3, "+"]))
+    print(calculate([15, 7, 1, 1, "+", "-", "/", 3, "*", 2, 1, 1, "+", "+", "-"]))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(n)
+"""
