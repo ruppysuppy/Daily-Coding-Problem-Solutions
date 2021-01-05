a984465c2a10121e6d5b7c7adb2b10ab2732e703
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-05 08:26:23 +0530

    Refactored solution 165

--- Solutions/165.py
+++ Solutions/165.py
@@ -1,48 +1,41 @@
 """
 Problem:
 
-Given an array of integers, return a new array where each element in the new array is the number of smaller elements to the right of that element in the original input array.
+Given an array of integers, return a new array where each element in the new array is
+the number of smaller elements to the right of that element in the original input array.
 
-Example:
+For example, given the array [3, 4, 9, 6, 1], return [1, 1, 2, 1, 0], since:
 
-Input = [3, 4, 9, 6, 1]
-Output = [1, 1, 2, 1, 0]
-(
-Since:
-* There is 1 smaller element to the right of 3
-* There is 1 smaller element to the right of 4
-* There are 2 smaller elements to the right of 9
-* There is 1 smaller element to the right of 6
-* There are no smaller elements to the right of 1
-)
+There is 1 smaller element to the right of 3
+There is 1 smaller element to the right of 4
+There are 2 smaller elements to the right of 9
+There is 1 smaller element to the right of 6
+There are no smaller elements to the right of 1
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def smaller_elem_arr_construct(arr):
-    # using brute force to solve the problem [O(n^2) algorithm]
-    # small array stores the result
-    small_arr = []
-    # getting the length of the input array
+from typing import List
+
+
+def get_smaller_elements_arr(arr: List[int]) -> List[int]:
+    smaller_elements_arr = []
     length = len(arr)
 
-    # iterating through the input array
     for i in range(length):
-        # getting the smaller elements to the right count
         smaller_elements = 0
-
-        # updating smaller_elements
         for j in range(i + 1, length):
             if arr[i] > arr[j]:
                 smaller_elements += 1
-
-        # updating smaller_arr
-        small_arr.append(smaller_elements)
-
-    # returning the resultant array
-    return small_arr
+        smaller_elements_arr.append(smaller_elements)
+    return smaller_elements_arr
 
 
-# DRIVER CODE
-print(smaller_elem_arr_construct([3, 4, 9, 6, 1]))
+if __name__ == "__main__":
+    print(get_smaller_elements_arr([3, 4, 9, 6, 1]))
 
-# NOTE: This problem can be solved in O(nlogn) using AVL Tree, but the algorithm is complex to understand
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n ^ 2)
+SPACE COMPLEXITY: O(n)
+"""
