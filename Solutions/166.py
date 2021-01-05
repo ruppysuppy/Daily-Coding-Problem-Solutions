da632f8c0838b47f9e4ea2692157e52579174f97
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-05 08:34:22 +0530

    Refactored solution 166

--- Solutions/166.py
+++ Solutions/166.py
@@ -1,71 +1,69 @@
 """
 Problem:
 
-Implement a 2D iterator class. It will be initialized with an array of arrays, and should implement the following methods:
-* next(): returns the next element in the array of arrays. If there are no more elements, raise an exception.
-* has_next(): returns whether or not the iterator still has elements left.
+Implement a 2D iterator class. It will be initialized with an array of arrays, and
+should implement the following methods:
+
+next(): returns the next element in the array of arrays. If there are no more
+elements, raise an exception.
+has_next(): returns whether or not the iterator still has elements left.
+For example, given the input [[1, 2], [3], [], [4, 5, 6]], calling next() repeatedly
+should output 1, 2, 3, 4, 5, 6.
+
 Do not use flatten or otherwise clone the arrays. Some of the arrays can be empty.
-
-Example:
-
-Input = [[1, 2], [3], [], [4, 5, 6]]
-Output = 1, 2, 3, 4, 5, 6 (calling next() repeatedly should output this sequence)
 """
 
-# iterator class
-class iterator:
-    # initialization function
-    def __init__(self, mat):
-        # storing the matrix and the generator
-        self.mat = mat
-        self.generator = self.generator_func(mat)
+from typing import Generator, List, Optional
+
+
+class Iterator2D:
+    def __init__(self, iteratable2d: List[List[int]]) -> None:
+        self.iteratable2d = iteratable2d
+        self.generator = Iterator2D.generator_func(iteratable2d)
         self.next_value = next(self.generator)
 
-    # generator function to generate the values from the matrix
-    def generator_func(self, matrix):
-        for array in matrix:
-            for element in array:
+    def __repr__(self) -> str:
+        return str(self.iteratable2d)
+
+    @staticmethod
+    def generator_func(iteratable2d: List[List[int]]) -> Generator[int, None, None]:
+        for iteratable in iteratable2d:
+            for element in iteratable:
                 yield element
 
-    # string function to display the matrix
-    def __str__(self):
-        return str(self.mat)
+    def has_next(self) -> bool:
+        return self.next_value is not None
 
-    # has next function implementation
-    def has_next(self):
-        # checking if the elements are left in the iterator
-        return self.next_value != None
-
-    # next function implementation
-    def next(self):
-        # storing the next value for returning
-        temp = self.next_value
-
-        # updating the next value
+    def next(self) -> Optional[int]:
+        curr_value = self.next_value
         try:
             self.next_value = next(self.generator)
         except StopIteration:
             self.next_value = None
-
-        # returning the stored value
-        return temp
+        return curr_value
 
 
-# DRIVER CODE
-iter_obj = iterator([[1, 2], [3], [], [4, 5, 6]])
-print(iter_obj)
+if __name__ == "__main__":
+    iter_obj = Iterator2D([[1, 2], [3], [], [4, 5, 6]])
+    print(iter_obj)
 
-print(iter_obj.has_next())
-print(iter_obj.next())
-print(iter_obj.has_next())
-print(iter_obj.next())
-print(iter_obj.has_next())
-print(iter_obj.next())
-print(iter_obj.has_next())
-print(iter_obj.next())
-print(iter_obj.has_next())
-print(iter_obj.next())
-print(iter_obj.has_next())
-print(iter_obj.next())
-print(iter_obj.has_next())
-print(iter_obj.next())
+    print(iter_obj.has_next())
+    print(iter_obj.next())
+
+    print(iter_obj.has_next())
+    print(iter_obj.next())
+
+    print(iter_obj.has_next())
+    print(iter_obj.next())
+
+    print(iter_obj.has_next())
+    print(iter_obj.next())
+
+    print(iter_obj.has_next())
+    print(iter_obj.next())
+
+    print(iter_obj.has_next())
+    print(iter_obj.next())
+
+    print(iter_obj.has_next())
+    print(iter_obj.next())
