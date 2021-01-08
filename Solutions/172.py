7ebbe2798b18507d2fdae16414c0353b9218c431
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-08 08:48:39 +0530

    Refactored solution 172

--- Solutions/172.py
+++ Solutions/172.py
@@ -1,42 +1,53 @@
 """
 Problem:
 
-Given a string s and a list of words words, where each word is the same length, find all starting indices of substrings in s that is a concatenation of every word in words exactly once.
+Given a string s and a list of words words, where each word is the same length, find
+all starting indices of substrings in s that is a concatenation of every word in words
+exactly once.
+
+For example, given s = "dogcatcatcodecatdog" and words = ["cat", "dog"], return [0, 13],
+since "dogcat" starts at index 0 and "catdog" starts at index 13.
+
+Given s = "barfoobazbitbyte" and words = ["dog", "cat"], return [] since there are no
+substrings composed of "dog" and "cat" in s.
+
 The order of the indices does not matter.
-
-Example:
-s = "dogcatcatcodecatdog"
-words = ["cat", "dog"]
-Output = [0, 13] (since "dogcat" starts at index 0 and "catdog" starts at index 13)
-
-s = "barfoobazbitbyte"
-words = ["dog", "cat"]
-Output = [] (since there are no substrings composed of "dog" and "cat" in s)
 """
 
-# importing permutation (generating all possible permutations)
-from itertools import permutations
-
-# importing regex function
+from itertools import permutations as generate_permutations
 from re import finditer
-
-# FUNCTION TO PERFORM THE OPERATION
-def get_indices(s, words):
-    # getting all permutations
-    permutation_list = [x + y for (x, y) in list(permutations(words))]
-    # res list to store the results
-    res = []
-
-    # checking the occurance of every permutation using regex
-    for permutation in permutation_list:
-        res.extend([match.start() for match in finditer(permutation, s)])
-
-    # sorting the result and returning it
-    res.sort()
-    return res
+from typing import Iterable, List
 
 
-# DRIVER CODE
-print(get_indices("barfoobazbitbyte", ["dog", "cat"]))
-print(get_indices("dogcatcatcodecatdog", ["cat", "dog"]))
-print(get_indices("dogcatcatcodecatdogcat", ["cat", "dog"]))
+def concat_iteratable(iterateable: Iterable[str]) -> str:
+    concated_value = ""
+    for elem in iterateable:
+        concated_value += elem
+    return concated_value
+
+
+def get_permutation_match_indices(s: str, words: List[str]) -> List[int]:
+    permutations = [
+        concat_iteratable(permutation)
+        for permutation in list(generate_permutations(words))
+    ]
+    indices = []
+    for permutation in permutations:
+        indices.extend([match.start() for match in finditer(permutation, s)])
+    return indices
+
+
+if __name__ == "__main__":
+    print(get_permutation_match_indices("barfoobazbitbyte", ["dog", "cat"]))
+    print(get_permutation_match_indices("dogcatcatcodecatdog", ["cat", "dog"]))
+    print(get_permutation_match_indices("dogcatcatcodecatdogcat", ["cat", "dog"]))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(2 ^ m + 2 ^ n)
+SPACE COMPLEXITY: O(n)
+[n = number of characters in input string
+ m = number of match words]
+"""
