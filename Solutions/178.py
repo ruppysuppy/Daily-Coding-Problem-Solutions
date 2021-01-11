0e0df44ee13f6fa38b55b63e2cda00bac55342cd
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-11 08:23:42 +0530

    Refactored solution 178

--- Solutions/178.py
+++ Solutions/178.py
@@ -1,72 +1,66 @@
 """
 Problem:
 
-Alice wants to join her school's Probability Student Club. 
-Membership dues are computed via one of two simple probabilistic games.
+Alice wants to join her school's Probability Student Club. Membership dues are computed
+via one of two simple probabilistic games.
 
-The first game: roll a die repeatedly. Stop rolling once you get a five followed by a six. 
-Your number of rolls is the amount you pay, in dollars.
+The first game: roll a die repeatedly. Stop rolling once you get a five followed by a
+six. Your number of rolls is the amount you pay, in dollars.
 
 The second game: same, except that the stopping condition is a five followed by a five.
 
-Which of the two games should Alice elect to play? Does it even matter?
-Write a program to simulate the two games and calculate their expected value.
+Which of the two games should Alice elect to play? Does it even matter? Write a program
+to simulate the two games and calculate their expected value.
 """
 
-# importing randint to simulate dice roll and sleep for displaying the simulation
 from random import randint
 from time import sleep
+from typing import Tuple
 
-# function to simulate dice roll
-def dice_roll():
+
+def roll_dice() -> int:
     return randint(1, 6)
 
 
-# FUNCTION TO PERFORM THE OPERATION
-def game(stopping_cond, display=False):
-    # temp1 and temp2 stores the last 2 throws
-    # counter stores the number of throws
-    temp1 = 0
-    temp2 = 0
-    counter = 0
-
+def simulate_game(stopping_condition: Tuple[int, int], display: bool = False) -> int:
+    last_throw, second_last_throw = 0, 0
+    required_second_last_throw, required_last_throw = stopping_condition
+    number_of_throws = 0
     # simulation the game
-    while temp1 != stopping_cond[1] or temp2 != stopping_cond[0]:
-        # rolling the dice and updating temp1 and temp2
-        val = dice_roll()
-        temp2 = temp1
-        temp1 = val
-        counter += 1
-
-        # displaying the simulation if the user whished to see it
+    while (
+        last_throw != required_last_throw
+        or second_last_throw != required_second_last_throw
+    ):
+        current_roll = roll_dice()
+        second_last_throw, last_throw = last_throw, current_roll
+        number_of_throws += 1
         if display:
             sleep(0.1)
-            print(f"On {counter}th throw, value: {val}")
-
-    # displaying the simulation if the user whished to see it
+            print(f"On {number_of_throws}th throw, value: {current_roll}")
     if display:
         sleep(0.1)
-        print(f"Total Value: {counter}\n")
-
-    # returning the number of rolls
-    return counter
+        print(f"Total Throws: {number_of_throws}\n")
+    return number_of_throws
 
 
-# DRIVER CODE
+if __name__ == "__main__":
+    print("Game 1 (5, 6):")
+    simulate_game((5, 6), True)
+    print("Game 2 (5, 5):")
+    simulate_game((5, 5), True)
 
-# simulation
-print("Game 1 (5, 6):")
-game((5, 6), True)
-print("Game 2 (5, 5):")
-game((5, 5), True)
+    g1 = 0
+    g2 = 0
+    for i in range(10_000):
+        g1 += simulate_game((5, 6))
+        g2 += simulate_game((5, 5))
+    print("Expectation of Game 1: {:.1f}".format(g1 / 10_000))
+    print("Expectation of Game 2: {:.1f}".format(g2 / 10_000))
 
-# expected value calculation
-g1 = 0
-g2 = 0
 
-for i in range(10000):
-    g1 += game((5, 6))
-    g2 += game((5, 5))
+"""
+SPECS:
 
-print("Expectation of Game 1: {:.1f}".format(g1 / 10000))
-print("Expectation of Game 2: {:.1f}".format(g2 / 10000))
+TIME COMPLEXITY: O(1)
+SPACE COMPLEXITY: O(1)
+"""
