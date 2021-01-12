760bc2bf82f0ca73ed0ffec2e1583afb36400f4e
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-12 09:01:00 +0530

    Refactored solution 181

--- Solutions/181.py
+++ Solutions/181.py
@@ -1,60 +1,57 @@
 """
 Problem:
 
-Given a string, split it into as few strings as possible such that each string is a palindrome.
+Given a string, split it into as few strings as possible such that each string is a
+palindrome.
 
-Example:
+For example, given the input string "racecarannakayak", return
+["racecar", "anna", "kayak"].
 
-Input = "racecarannakayak"
-Output = ["racecar", "anna", "kayak"]
-
-Input = "abc"
-Output = ["a", "b", "c"]
+Given the input string "abc", return ["a", "b", "c"].
 """
 
-# function to check if a string is palindrome
-def is_palindrome(string):
+from typing import List
+
+
+def is_palindrome(string: str) -> bool:
     return string and string == string[::-1]
 
 
-# helper function to perform the computations
-def split_into_strings_helper(string, current, previous):
-    # base cases for recursion
-
-    # the entire string has been partitioned into palindromes
-    # (previous contains the last palindrome)
+def split_into_string_list_helper(
+    string: str, current: str, palindrome_list: List[str]
+) -> List[str]:
     if not string and not current:
-        return previous
-    # the string has been exhausted, but could not obtain a palindrome using the last characters
-    # (each character in the string is used as a palindrome)
+        return palindrome_list
     elif not string:
-        return previous + list(current)
-
-    # generating temp
-    temp = current + string[0]
-
-    # checking if temp is a palindrome
-    if is_palindrome(temp):
-        # adding temp to the list of palindromes
-        temp_arr_1 = split_into_strings_helper(string[1:], "", previous + [temp])
+        return palindrome_list + list(current)
+    # generating the palindrome list
+    curr = current + string[0]
+    if is_palindrome(curr):
+        # adding curr to the list of palindromes
+        palindrome_list_1 = split_into_string_list_helper(
+            string[1:], "", palindrome_list + [curr]
+        )
         # checking if a larger palindrome can be obtained
-        temp_arr_2 = split_into_strings_helper(string[1:], temp, previous)
-
-        # returning the shorter list
-        return min(temp_arr_1, temp_arr_2, key=lambda List: len(List))
-
-    # if the temp is not a palindrome, the search for a palindrome continues
-    else:
-        return split_into_strings_helper(string[1:], temp, previous)
+        palindrome_list_2 = split_into_string_list_helper(
+            string[1:], curr, palindrome_list
+        )
+        return min(palindrome_list_1, palindrome_list_2, key=lambda List: len(List))
+    return split_into_string_list_helper(string[1:], curr, palindrome_list)
 
 
-# FUNCTION TO PERFORM THE OPERATION
-def split_into_strings(string):
-    # calling the helper function
-    return split_into_strings_helper(string, "", [])
+def split_into_string_list(string: str) -> List[str]:
+    return split_into_string_list_helper(string, "", [])
 
 
-# DRIVER CODE
-print(split_into_strings("racecarannakayak"))
-print(split_into_strings("abc"))
-print(split_into_strings("abbbc"))
+if __name__ == "__main__":
+    print(split_into_string_list("racecarannakayak"))
+    print(split_into_string_list("abc"))
+    print(split_into_string_list("abbbc"))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(2 ^ n)
+SPACE COMPLEXITY: O(n)
+"""
