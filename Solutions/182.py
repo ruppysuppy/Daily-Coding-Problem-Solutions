fe43cf74ecc6e0b9e12795ad5f99673af3549e95
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-13 08:21:33 +0530

    Refactored solution 182

--- Solutions/182.py
+++ Solutions/182.py
@@ -1,64 +1,62 @@
 """
 Problem:
 
-A graph is minimally-connected if it is connected and there is no edge that can be removed while still leaving the graph connected. 
-For example, any binary tree is minimally-connected.
-Given an undirected graph, check if the graph is minimally-connected. 
-You can choose to represent the graph as either an adjacency matrix or adjacency list.
+A graph is minimally-connected if it is connected and there is no edge that can be
+removed while still leaving the graph connected. For example, any binary tree is
+minimally-connected.
+
+Given an undirected graph, check if the graph is minimally-connected. You can choose to
+represent the graph as either an adjacency matrix or adjacency list.
 """
 
-# importing deepcopy to generate a deep copy of a dictionary
 from copy import deepcopy
 
-# local import for graph class from Datastructures module
-from DataStructures.Graph import Graph_Undirected_Unweighted
+from DataStructures.Graph import GraphUndirectedUnweighted
+from DataStructures.Queue import Queue
 
-# FUNCTION TO PERFROM THE OPERATION
-def is_minimally_connected(self):
-    # creating a copy for the graph
-    graph_copy = Graph_Undirected_Unweighted()
-    graph_copy.connections = deepcopy(self.connections)
 
-    # getting a random node to start from
-    # (the graph is undirected, so random selection is not a problem)
-    for node in self.connections:
+def is_minimally_connected(graph: GraphUndirectedUnweighted) -> bool:
+    graph_copy = GraphUndirectedUnweighted()
+    graph_copy.connections, graph_copy.nodes = deepcopy(graph.connections), graph.nodes
+    # getting a random node for starting the traversal
+    for node in graph.connections:
         start = node
         break
-
     # running bfs and checking if a node is visited more than once
     # (redundant edges present => not a minimally connected graph)
     visited = set([start])
-    queue = [start]
-
-    while queue:
-        node = queue.pop(0)
-
-        for child_node in graph_copy.connections[node]:
-            graph_copy.connections[child_node].remove(node)
-            queue.append(child_node)
-
-            if child_node in visited:
+    queue = Queue()
+    queue.enqueue(start)
+    while not queue.is_empty():
+        node = queue.dequeue()
+        for neighbour in graph_copy.connections[node]:
+            graph_copy.connections[neighbour].remove(node)
+            queue.enqueue(neighbour)
+            if neighbour in visited:
                 return False
-            else:
-                visited.add(child_node)
-
+            visited.add(neighbour)
     return True
 
 
-# adding the function to the graph class
-setattr(Graph_Undirected_Unweighted, "is_minimally_connected", is_minimally_connected)
+if __name__ == "__main__":
+    graph = GraphUndirectedUnweighted()
 
-# DRIVER CODE
-graph = Graph_Undirected_Unweighted()
+    graph.add_edge(1, 2)
+    graph.add_edge(1, 3)
+    graph.add_edge(3, 4)
 
-graph.add_edge(1, 2)
-graph.add_edge(1, 3)
-graph.add_edge(3, 4)
+    print(graph)
+    print(is_minimally_connected(graph))
 
-print(graph)
-print(graph.is_minimally_connected())
+    graph.add_edge(1, 4)
 
-graph.add_edge(1, 4)
+    print(graph)
+    print(is_minimally_connected(graph))
 
-print(graph)
-print(graph.is_minimally_connected())
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n x e)
+SPACE COMPLEXITY: O(n + e)
+"""
