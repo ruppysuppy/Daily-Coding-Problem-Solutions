8f6f5a478279e82cb2c738f66eae7d819b34c25f
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-16 08:53:08 +0530

    Refactored solution 191

--- Solutions/191.py
+++ Solutions/191.py
@@ -1,48 +1,47 @@
 """
 Problem:
 
-Given a collection of intervals, find the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
-Intervals can "touch", such as [0, 1] and [1, 2], but they won't be considered overlapping.
+Given a collection of intervals, find the minimum number of intervals you need to
+remove to make the rest of the intervals non-overlapping.
+
+Intervals can "touch", such as [0, 1] and [1, 2], but they won't be considered
+overlapping.
+
+For example, given the intervals (7, 9), (2, 4), (5, 8), return 1 as the last interval
+can be removed and the first two won't overlap.
+
 The intervals are not necessarily sorted in any order.
-
-Example:
-
-Input = (7, 9), (2, 4), (5, 8)
-Output = 1 (as the last interval can be removed and the first two won't overlap)
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def num_overlap(arr):
-    # cache to store whether a time frame falls in the input time interval
-    cache = [0 for _ in range(max(max(arr)) + 1)]
-    # variable to store the overlap count
+from typing import List
+
+
+def num_overlap(arr: List[int]) -> int:
+    time_slot_usage = [False for _ in range(max(arr, key=lambda x: x[1])[1] + 1)]
     overlap_count = 0
 
-    # iterating through the intervals
     for interval in arr:
-        # breaking the interval into start and end
         start, end = interval
-        # flag checks for overlap
-        flag = True
-
-        # iterating through the interval range
-        # (to end-1 as 2 intervals may have the same end and start time without overlaping)
+        overlap_flag = True
         for i in range(start, end):
-            # checking if the value in the cache is 0 (no overlap)
-            if cache[i] == 0:
-                cache[i] = 1
-            else:
-                # if an overlap occurs, overlap count is incremented
-                # flag is unset so that the overlap is counted once
-                if flag:
-                    overlap_count += 1
-                    flag = False
-
-    # returning overlap count
+            if not time_slot_usage[i]:
+                time_slot_usage[i] = True
+            elif overlap_flag:
+                overlap_count += 1
+                overlap_flag = False
     return overlap_count
 
 
-# DRIVER CODE
-print(num_overlap([[0, 1], [1, 2]]))
-print(num_overlap([(7, 9), (2, 4), (5, 8)]))
-print(num_overlap([(7, 9), (2, 4), (5, 8), (1, 3)]))
+if __name__ == "__main__":
+    print(num_overlap([[0, 1], [1, 2]]))
+    print(num_overlap([(7, 9), (2, 4), (5, 8)]))
+    print(num_overlap([(7, 9), (2, 4), (5, 8), (1, 3)]))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(n)
+[n = maximum ending time]
+"""
