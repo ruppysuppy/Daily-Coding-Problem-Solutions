83290d49927415c3e36e9e8ad3d6bce019e05270
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-17 09:05:23 +0530

    Refactored solutions 196 to 200

--- Solutions/198.py
+++ Solutions/198.py
@@ -1,56 +1,55 @@
 """
 Problem:
 
-Given a set of distinct positive integers, find the largest subset such that every pair of elements in the subset (i, j) satisfies either i % j = 0 or j % i = 0.
+Given a set of distinct positive integers, find the largest subset such that every pair
+of elements in the subset (i, j) satisfies either i % j = 0 or j % i = 0.
 
-Example:
-
-Input = [3, 5, 10, 20, 21]
-Output = [5, 10, 20]
-
-Input = [1, 3, 6, 24]
-Output = [1, 3, 6, 24]
+For example, given the set [3, 5, 10, 20, 21], you should return [5, 10, 20]. Given
+[1, 3, 6, 24], return [1, 3, 6, 24].
 """
 
-# helper function to perform the computation
-def get_largest_subset_helper(arr, prev_num=1, curr_ind=0, prev_subset=[]):
-    # if the end of the array has been reached, the previous subsets are returned (base for recursion)
-    if curr_ind == len(arr):
+from typing import List
+
+
+def get_largest_subset_helper(
+    arr: List[int],
+    length: int,
+    prev_num: int = 1,
+    curr_ind: int = 0,
+    prev_subset: List[int] = [],
+) -> List[int]:
+    if curr_ind == length:
         return prev_subset
 
-    # getting the current element
     curr_elem = arr[curr_ind]
-
-    # calling the helper function recursively
     res = get_largest_subset_helper(arr, prev_num, curr_ind + 1, prev_subset)
-
-    # checking if the element is divisible by the last element (since it would contain all other smaller factors too)
     if curr_elem % prev_num == 0:
         # generating the alternate result (with the element added)
         alternate_res = get_largest_subset_helper(
             arr, curr_elem, curr_ind + 1, prev_subset + [curr_elem]
         )
-
-        # returning the longer result
         return max(alternate_res, res, key=lambda result: len(result))
-
-    # returning the result in case its not divisible
     return res
 
 
-# FUNCTION TO PERFORM THE OPERATION
-def get_largest_subset(arr):
-    # sorting the array
+def get_largest_subset(arr: List[int]) -> List[int]:
     arr.sort()
-    # calling the helper function on the array
-    return get_largest_subset_helper(arr, prev_subset=[])
+    return get_largest_subset_helper(arr, len(arr), prev_subset=[])
 
 
-# DRIVER CODE
-print(get_largest_subset([]))
-print(get_largest_subset([2]))
-print(get_largest_subset([2, 3]))
-print(get_largest_subset([3, 5, 10, 20, 21]))
-print(get_largest_subset([1, 3, 6, 24]))
-print(get_largest_subset([3, 9, 15, 30]))
-print(get_largest_subset([2, 3, 9, 15, 30]))
+if __name__ == "__main__":
+    print(get_largest_subset([]))
+    print(get_largest_subset([2]))
+    print(get_largest_subset([2, 3]))
+    print(get_largest_subset([3, 5, 10, 20, 21]))
+    print(get_largest_subset([1, 3, 6, 24]))
+    print(get_largest_subset([3, 9, 15, 30]))
+    print(get_largest_subset([2, 3, 9, 15, 30]))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(2 ^ n)
+SPACE COMPLEXITY: O(n)
+"""
