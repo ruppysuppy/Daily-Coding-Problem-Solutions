124a059b7cfcfd53c49c112c14285124aee4cc92
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-18 08:27:26 +0530

    Refactored solution 201

--- Solutions/201.py
+++ Solutions/201.py
@@ -1,57 +1,57 @@
 """
 Problem:
 
-You are given an array of arrays of integers, where each array corresponds to a row in a triangle of numbers. 
-We define a path in the triangle to start at the top and go down one row at a time to an adjacent value, eventually ending with an entry on the bottom row. 
-The weight of the path is the sum of the entries.
-Write a program that returns the weight of the maximum weight path.
+You are given an array of arrays of integers, where each array corresponds to a row in
+a triangle of numbers. For example, [[1], [2, 3], [1, 5, 1]] represents the triangle:
 
-Example:
-
-Input = [[1], [2, 3], [1, 5, 1]]
-(represents the triangle:
   1
  2 3
-1 5 1)
-Output = [1 -> 3 -> 5]
+1 5 1
+We define a path in the triangle to start at the top and go down one row at a time to
+an adjacent value, eventually ending with an entry on the bottom row. For example,
+1 -> 3 -> 5. The weight of the path is the sum of the entries.
+
+Write a program that returns the weight of the maximum weight path.
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def calc_path(triangle):
-    # getting the number of rows
+from typing import List
+
+
+def calc_path(triangle: List[List[int]]) -> List[int]:
     rows = len(triangle)
 
-    # few checks
     if rows == 0:
         return []
     elif rows == 1:
         return triangle[0]
 
-    # traingle copy used for dynamic programming
+    # using dynamic programming to get the maximum weight
+    # elements stored as (weight, path)
     dp = [list(row) for row in triangle]
-
-    # generating the 2nd last row
     for i in range(len(dp[-2])):
         dp[-2][i] = (
             (max(dp[-1][i], dp[-1][i + 1]) + dp[-2][i]),
             [max(dp[-1][i], dp[-1][i + 1]), dp[-2][i]],
         )
-
-    # using dynamic programming to get the maximum weight (data is generated from the 3rd row from base to the peak)
     for i in range(rows - 3, -1, -1):
         for j in range(i + 1):
-            # getting the maximum weight path from the current position
-            # elements stored as (weight, path)
             dp[i][j] = (
                 (max(dp[i + 1][j][0], dp[i + 1][j + 1][0]) + dp[i][j]),
                 max((dp[i + 1][j], dp[i + 1][j + 1]), key=lambda elem: elem[0])[1]
                 + [dp[i][j]],
             )
-
-    # getting the path
     return dp[0][0][1][::-1]
 
 
-# DRIVER CODE
-print(calc_path([[1], [2, 3], [1, 5, 1]]))
-print(calc_path([[1], [2, 3], [7, 5, 1]]))
+if __name__ == "__main__":
+    print(calc_path([[1], [2, 3], [1, 5, 1]]))
+    print(calc_path([[1], [2, 3], [7, 5, 1]]))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(n)
+[n = number of items in the triangle]
+"""
