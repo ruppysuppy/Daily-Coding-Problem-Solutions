02dba91396976f927a8aae7f611ba8e9874a15f5
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-19 08:30:17 +0530

    Refactored solution 203

--- Solutions/203.py
+++ Solutions/203.py
@@ -1,46 +1,44 @@
 """
 Problem:
 
-Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand. 
-Find the minimum element in O(log N) time. You may assume the array does not contain duplicates.
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you
+beforehand. Find the minimum element in O(log N) time. You may assume the array does
+not contain duplicates.
 
-Example:
-
-Input = [5, 7, 10, 3, 4]
-Output = 3
+For example, given [5, 7, 10, 3, 4], return 3.
 """
 
-# helper function to perform the computation
-def find_pivot_helper(arr, low, high):
-    # base case for recursion
+from typing import List
+
+
+def find_pivot_helper(arr: List[int], low: int, high: int) -> int:
     if low == high:
         return high
 
-    # getting the mid
     mid = (high + low) // 2
-
-    # checking if the mid is the pivot
     if mid < high and arr[mid] > arr[mid + 1]:
         return mid
-    # checking if mid-1 is the pivot
     elif mid > low and arr[mid] < arr[mid - 1]:
         return mid - 1
-    # checking if the pivot is on the right of mid
     elif arr[mid] > arr[high]:
         return find_pivot_helper(arr, mid + 1, high)
-    # if the pivot is on the left of mid
     return find_pivot_helper(arr, low, mid - 1)
 
 
-# FUNCTION TO PERFORM THE OPERATION
-def find_pivot(arr):
-    # getting the length of the array
+def find_pivot(arr: List[int]) -> int:
     length = len(arr)
-    # finding the pivot
-    piv = find_pivot_helper(arr, 0, length)
-    # returning the 1st index in the pivoted array
-    return (piv + 1) % length
+    # the pivot returns the last index of the rotated array
+    pivot = find_pivot_helper(arr, 0, length)
+    return (pivot + 1) % length
 
 
-# DRIVER CODE
-print(find_pivot([5, 7, 10, 3, 4]))
+if __name__ == "__main__":
+    print(find_pivot([5, 7, 10, 3, 4]))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(log(n))
+SPACE COMPLEXITY: O(log(n)) [python doesn't support tail recursion optimization]
+"""
