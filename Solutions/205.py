ad490afc5c98237ea4924f65f57a09af2525230c
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-20 08:43:44 +0530

    Refactored solution 205

--- Solutions/205.py
+++ Solutions/205.py
@@ -1,54 +1,49 @@
 """
 Problem:
 
-Given an integer, find the next permutation of it in absolute order. 
-
-Example:
-
-Input = 48975
-Output = 49578
+Given an integer, find the next permutation of it in absolute order. For example, given
+48975, the next permutation would be 49578.
 """
 
-# function used in problem 95 (used as a helper function in this problem)
-def get_next_helper(arr):
-    # getting the length of arr
-    length = len(arr)
+from typing import List
 
-    # if arr has 0 or 1 elements, its returned
+
+def get_next_helper(arr: List[int]) -> List[int]:
+    length = len(arr)
     if length < 2:
         return arr
-
-    # looping arr from the end to the beginning
+    # finding the last element arranged in ascending order
     for index in range(length - 1, -1, -1):
-        # finding the last element arranged in ascending order
         if index > 0 and arr[index - 1] < arr[index]:
             break
-
-    # if index is 0 (arr is sorted in descending order), arr is reversed
+    # if index is 0, arr is sorted in descending order
     if index == 0:
         arr.reverse()
-
-    else:
-        # finding the next permutation
-        # looping over arr from the end to the index
-        for k in range(length - 1, index - 1, -1):
-            # getting the element to swap
-            if arr[k] > arr[index - 1]:
-                arr[k], arr[index - 1] = arr[index - 1], arr[k]
-                break
-
-        # arranging the other elements in proper order
-        sub_array = arr[index:]
-        sub_array.reverse()
-        arr[index:] = sub_array
-
+        return arr
+    # finding the next permutation
+    for k in range(length - 1, index - 1, -1):
+        if arr[k] > arr[index - 1]:
+            arr[k], arr[index - 1] = arr[index - 1], arr[k]
+            break
+    # arranging the other elements in proper order
+    size = (length - 1) + index
+    for i in range(index, (size + 1) // 2):
+        arr[i], arr[size - i] = arr[size - i], arr[i]
     return arr
 
 
-# FUNCTION TO PERFORM THE OPERATION
-def get_next(num):
+def get_next(num: int) -> int:
     return int("".join(get_next_helper(list(str(num)))))
 
 
-# DRIVER CODE
-print(get_next(48975))
+if __name__ == "__main__":
+    print(get_next(48975))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(n)
+[n = number of digits]
+"""
