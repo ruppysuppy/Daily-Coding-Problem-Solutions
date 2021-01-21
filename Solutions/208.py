9377ee8f4d5e23772bc79418ed8c2651fb741fc1
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-21 08:45:05 +0530

    Refactored solution 208

--- Solutions/208.py
+++ Solutions/208.py
@@ -1,46 +1,45 @@
 """
 Problem:
 
-Given a linked list of numbers and a pivot k, partition the linked list so that all nodes less than k come before nodes greater than or equal to k.
+Given a linked list of numbers and a pivot k, partition the linked list so that all
+nodes less than k come before nodes greater than or equal to k.
 
-Example:
-
-Linked List = 5 -> 1 -> 8 -> 0 -> 3
-k = 3
-Output = 1 -> 0 -> 5 -> 8 -> 3
+For example, given the linked list 5 -> 1 -> 8 -> 0 -> 3 and k = 3, the solution could
+be 1 -> 0 -> 5 -> 8 -> 3.
 """
 
-# local import from the Datastructre module
-from DataStructures.LinkedList import Node, Linked_list
+from DataStructures.LinkedList import Node, LinkedList
 
-# FUNCTION TO PERFORM THE OPERATION
-def pivot(self, k):
-    # declaring the 2 pointers
-    ptr1 = self.head
-    ptr2 = self.head
 
-    # using the 2 pointers to swap the values
-    for _ in range(self.length):
+def pivot_linked_list(ll: LinkedList, k: int) -> None:
+    ptr1, ptr2 = ll.head, ll.head
+    length = len(ll)
+    k = k % length
+    for _ in range(length):
         if ptr2.val < k:
             ptr1.val, ptr2.val = ptr2.val, ptr1.val
             ptr1 = ptr1.next
-        # iterating through the linked list
         ptr2 = ptr2.next
 
 
-# adding the pivot function to the Linked List class
-setattr(Linked_list, "pivot", pivot)
+if __name__ == "__main__":
+    LL = LinkedList()
+    LL.add(5)
+    LL.add(1)
+    LL.add(8)
+    LL.add(0)
+    LL.add(3)
 
-# DRIVER CODE
-LL = Linked_list()
-LL.add(5)
-LL.add(1)
-LL.add(8)
-LL.add(0)
-LL.add(3)
+    print(LL)
 
-print(LL)
+    pivot_linked_list(LL, 3)
 
-LL.pivot(3)
+    print(LL)
 
-print(LL)
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(1)
+"""
