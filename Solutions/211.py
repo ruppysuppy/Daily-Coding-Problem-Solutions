3322d2b8ba1482ba260e6fa8c31e30499776d4f6
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-23 08:23:32 +0530

    Refactored solution 211

--- Solutions/211.py
+++ Solutions/211.py
@@ -1,85 +1,67 @@
 """
 Problem:
 
-Given a string and a pattern, find the starting indices of all occurrences of the pattern in the string. 
-
-Example:
-
-String = "abracadabra"
-Pattern = "abr"
-Output = [0, 7]
+Given a string and a pattern, find the starting indices of all occurrences of the
+pattern in the string. For example, given the string "abracadabra" and the pattern
+"abr", you should return [0, 7].
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def KMPSearch(string, pattern):
-    # using KMP algorithm for pattern matching
-    # getting the length of the pattern and the string
-    patten_length = len(pattern)
+from typing import List
+
+
+def kmp_search(string: str, pattern: str) -> List[int]:
+    pattern_length = len(pattern)
     string_length = len(string)
-    # array to hold the longest prefix suffix values for pattern
-    lps = [0] * patten_length
-    # array to store the results
+    lps = [0] * pattern_length
     result = []
+    compute_lps(pattern, pattern_length, lps)
 
-    j = 0  # index for patten
-    i = 0  # index for string
-
-    # preprocessing the pattern (computing the lps array)
-    compute_LPS(pattern, patten_length, lps)
-
-    # iterating through the string
+    j = 0
+    i = 0
     while i < string_length:
-        # if match occours i & j are incremented
         if pattern[j] == string[i]:
             i += 1
             j += 1
-
-        # if the entire pattern matches
-        if j == patten_length:
-            # adding the position to result & going back to the required location
-            # (based upon the pre-processed array)
+        # entire pattern match
+        if j == pattern_length:
             result.append(i - j)
             j = lps[j - 1]
-
-        # mismatch after j matches
+        # mismatch after j positions
         elif i < string_length and pattern[j] != string[i]:
-            # going back to the required location (based upon the pre-processed array) or incrementing i
             if j != 0:
                 j = lps[j - 1]
             else:
                 i += 1
-    # returning the result
     return result
 
 
-# helper function to pre-process the array
-def compute_LPS(pattern, patten_length, lps):
-    # length of the previous longest prefix suffix (initialized as 0)
+def compute_lps(pattern: str, pattern_length: int, lps: List[int]) -> None:
     length = 0
-    # lps[0] is always 0
     lps[0]
-    # index for patten
     i = 1
-
-    # generating the lps array
-    while i < patten_length:
+    while i < pattern_length:
         # match occours
         if pattern[i] == pattern[length]:
             length += 1
             lps[i] = length
             i += 1
-        # match doesn't occour
+            continue
+        if length != 0:
+            length = lps[length - 1]
         else:
-            # if the prefix match is postive its updated to the previous value in lps
-            if length != 0:
-                length = lps[length - 1]
-            # if prefix match is 0, lps for the value is set to 0 and i incremented
-            else:
-                lps[i] = 0
-                i += 1
+            lps[i] = 0
+            i += 1
 
 
-# DRIVER CODE
-print(KMPSearch("abracadabra", "abr"))
-print(KMPSearch("abracadabra", "xyz"))
-print(KMPSearch("aaaa", "aa"))
+if __name__ == "__main__":
+    print(kmp_search("abracadabra", "abr"))
+    print(kmp_search("abracadabra", "xyz"))
+    print(kmp_search("aaaa", "aa"))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(string_length + pattern_length)
+SPACE COMPLEXITY: O(pattern_length)
+"""
