714334f40f619393030a2fd455e048f00c4f0eac
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-24 08:45:23 +0530

    Refactored solution 214

--- Solutions/214.py
+++ Solutions/214.py
@@ -1,36 +1,36 @@
 """
 Problem:
 
-Given an integer n, return the length of the longest consecutive run of 1s in its binary representation.
+Given an integer n, return the length of the longest consecutive run of 1s in its
+binary representation.
 
-Example: 
-
-Input = 156
-Output = 3
+For example, given 156, you should return 3.
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def longest_chain_of_1(num):
-    # getting the binary representation
+
+def get_longest_chain_of_1s(num: int) -> int:
     num = bin(num)[2:]
-    # chain max stores the number of 1's in the longest chain
     chain_max = 0
-    # chain curr stores the number of 1's in the current chain
     chain_curr = 0
 
-    # iterating through the binary number
-    for i in num:
-        # updating chain max and chain curr
-        if i == "1":
+    for char in num:
+        if char == "1":
             chain_curr += 1
         else:
             chain_max = max(chain_max, chain_curr)
             chain_curr = 0
-
-    # returning the result
     return max(chain_max, chain_curr)
 
 
-# DRIVER CODE
-print(longest_chain_of_1(15))
-print(longest_chain_of_1(156))
+if __name__ == "__main__":
+    print(get_longest_chain_of_1s(15))
+    print(get_longest_chain_of_1s(156))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(log(n))
+SPACE COMPLEXITY: O(1)
+[there are log2(n) digits in the binary representation of any number n]
+"""
