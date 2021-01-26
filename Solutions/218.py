e60c43b8d8eb14d880fd0240f62e4defe09ea653
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-26 08:56:22 +0530

    Refactored solution 218

--- Solutions/218.py
+++ Solutions/218.py
@@ -1,44 +1,45 @@
 """
 Problem:
 
-Write an algorithm that computes the reversal of a directed graph. 
-For example, if a graph consists of A -> B -> C, it should become A <- B <- C.
+Write an algorithm that computes the reversal of a directed graph. For example, if a
+graph consists of A -> B -> C, it should become A <- B <- C.
 """
 
-# local import from Datastructure module
-from DataStructures.Graph import Graph_Directed_Unweighted
+from DataStructures.Graph import GraphDirectedUnweighted
 
 
-# FUNCTION TO PERFORM THE OPERATION
-def reverse_direction(self):
+def reverse_direction(graph: GraphDirectedUnweighted) -> None:
     visited = set()
-
-    for node in self.connections:
+    for node in graph.connections:
         # storing the nodes that require updation in to change as for loop doesn't
         # support simultaneous updation
         visited.add(node)
         to_change = []
-
-        for neighbour in self.connections[node]:
+        for neighbour in graph.connections[node]:
             if neighbour not in visited:
-                if node not in self.connections[neighbour]:
+                if node not in graph.connections[neighbour]:
                     to_change.append(neighbour)
-
         for neighbour in to_change:
-            self.connections[neighbour].add(node)
-            self.connections[node].remove(neighbour)
+            graph.connections[neighbour].add(node)
+            graph.connections[node].remove(neighbour)
 
 
-setattr(Graph_Directed_Unweighted, "reverse_direction", reverse_direction)
+if __name__ == "__main__":
+    graph = GraphDirectedUnweighted()
 
-# DRIVER CODE
-graph = Graph_Directed_Unweighted()
+    graph.add_edge("A", "B")
+    graph.add_edge("B", "C")
 
-graph.add_edge("A", "B")
-graph.add_edge("B", "C")
+    print(graph)
 
-print(graph)
+    reverse_direction(graph)
 
-graph.reverse_direction()
+    print(graph)
 
-print(graph)
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(v + e)
+SPACE COMPLEXITY: O(v)
+"""
