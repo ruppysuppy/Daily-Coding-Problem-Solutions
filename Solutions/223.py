b33de44c1ec85329ab9f3fe6a160dfc28716e5fa
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-27 09:41:33 +0530

    Refactored solutions 223 & 224

--- Solutions/223.py
+++ Solutions/223.py
@@ -1,53 +1,61 @@
 """
 Problem:
 
-Typically, an implementation of in-order traversal of a binary tree has O(h) space complexity, where h is the height of the tree. 
-Write a program to compute the in-order traversal of a binary tree using O(1) space.
+Typically, an implementation of in-order traversal of a binary tree has O(h) space
+complexity, where h is the height of the tree. Write a program to compute the in-order
+traversal of a binary tree using O(1) space.
 """
 
-from DataStructures.Tree import Binary_Tree, Node
+from typing import Generator
+
+from DataStructures.Tree import BinaryTree, Node
 
 
-def morris_traversal(tree):
+def morris_traversal(tree: BinaryTree) -> Generator[int, None, None]:
     current = tree.root
 
     while current is not None:
         if current.left is None:
             yield current.val
             current = current.right
+            continue
+        # Find the inorder predecessor of current
+        pre = current.left
+        while pre.right is not None and pre.right is not current:
+            pre = pre.right
+        if pre.right is None:
+            # Make current as right child of its inorder predecessor
+            pre.right = current
+            current = current.left
         else:
-            # Find the inorder predecessor of current
-            pre = current.left
-            while pre.right is not None and pre.right is not current:
-                pre = pre.right
-
-            if pre.right is None:
-                # Make current as right child of its inorder predecessor
-                pre.right = current
-                current = current.left
-
-            else:
-                # Revert the changes made in the 'if' part to restore the
-                # original tree. (Fix the right child of predecessor)
-                pre.right = None
-                yield current.val
-                current = current.right
+            # Revert the changes made in the 'if' part to restore the
+            # original tree. (Fix the right child of predecessor)
+            pre.right = None
+            yield current.val
+            current = current.right
 
 
-# DRIVER CODE
-tree = Binary_Tree()
+if __name__ == "__main__":
+    tree = BinaryTree()
 
-tree.root = Node(1)
-tree.root.left = Node(2)
-tree.root.right = Node(3)
+    tree.root = Node(1)
+
+    tree.root.left = Node(2)
+    tree.root.right = Node(3)
+
+    tree.root.left.left = Node(4)
+    tree.root.left.right = Node(5)
+
+    tree.root.right.right = Node(6)
+
+    print(tree)
+    for node in morris_traversal(tree):
+        print(node, end=" ")
 
 
-tree.root.left.left = Node(4)
-tree.root.left.right = Node(5)
+"""
+SPECS:
 
-tree.root.right.right = Node(6)
-
-print(tree)
-
-for node in morris_traversal(tree):
-    print(node, end=" ")
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(1)
+"""
