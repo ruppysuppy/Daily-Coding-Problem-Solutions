6433218e3a142b0321742e42452e8aac78309acb
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-27 09:27:23 +0530

    Refactored solutions 221 & 222

--- Solutions/221.py
+++ Solutions/221.py
@@ -1,35 +1,41 @@
 """
 Problem:
 
-Let's define a "sevenish" number to be one which is either a power of 7, or the sum of unique powers of 7. 
-The first few sevenish numbers are 1, 7, 8, 49, and so on. Create an algorithm to find the nth sevenish number.
+Let's define a "sevenish" number to be one which is either a power of 7, or the sum of
+unique powers of 7. The first few sevenish numbers are 1, 7, 8, 49, and so on. Create
+an algorithm to find the nth sevenish number.
 """
 
-# FUNCTION TO PERFORM THE OPERATION
-def get_sevenish_num(number):
+
+def get_nth_sevenish_num(number: int) -> int:
     curr = 1
     curr_iteration = 1
-
     while curr < number:
         curr_iteration += 1
         curr += curr_iteration
 
     curr -= curr_iteration
-
     result = 7 ** (curr_iteration - 1)
     curr_to_add = 1
 
     for _ in range(number - curr - 1):
         result += curr_to_add
         curr_to_add *= 7
-
     return result
 
 
-# DRIVER CODE
-print(get_sevenish_num(1))  # 1 = 7 ^ 0
-print(get_sevenish_num(2))  # 7 = 7 ^ 1
-print(get_sevenish_num(3))  # 8 = 7 ^ 0 + 7 ^ 1
-print(get_sevenish_num(4))  # 49 = 7 ^ 2
-print(get_sevenish_num(5))  # 50 = 7 ^ 0 + 7 ^ 2
-print(get_sevenish_num(6))  # 57 = 7 ^ 0 + 7 ^ 1 + 7 ^ 2
+if __name__ == "__main__":
+    print(get_nth_sevenish_num(1))  # 1 = 7 ^ 0
+    print(get_nth_sevenish_num(2))  # 7 = 7 ^ 1
+    print(get_nth_sevenish_num(3))  # 8 = 7 ^ 0 + 7 ^ 1
+    print(get_nth_sevenish_num(4))  # 49 = 7 ^ 2
+    print(get_nth_sevenish_num(5))  # 50 = 7 ^ 0 + 7 ^ 2
+    print(get_nth_sevenish_num(6))  # 57 = 7 ^ 0 + 7 ^ 1 + 7 ^ 2
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(log(n))
+SPACE COMPLEXITY: O(1)
+"""
--- Solutions/222.py
+++ Solutions/222.py
@@ -1,29 +1,36 @@
 """
 Problem:
 
-Given an absolute pathname that may have . or .. as part of it, return the shortest standardized path.
+Given an absolute pathname that may have . or .. as part of it, return the shortest
+standardized path.
 
-Example:
-
-Input = "/usr/bin/../bin/./scripts/../"
-Output = "/usr/bin/"
+For example, given /usr/bin/../bin/./scripts/../, return /usr/bin/.
 """
 
+from DataStructures.Stack import Stack
 
-def get_shortest_standardized_path(path):
+
+def get_shortest_standardized_path(path: str) -> str:
     path_list = path.split("/")
-    stack = []
+    stack = Stack()
 
-    for curr in path_list:
-        if curr == "..":
-            stack.pop()
-        elif curr == ".":
+    for curr_directory in path_list:
+        if curr_directory == ".":
             continue
+        elif curr_directory == "..":
+            stack.pop()
         else:
-            stack.append(curr)
-
+            stack.push(curr_directory)
     return "/".join(stack)
 
 
 if __name__ == "__main__":
     print(get_shortest_standardized_path("/usr/bin/../bin/./scripts/../"))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(n)
+"""
