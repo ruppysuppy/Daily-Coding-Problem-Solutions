f2a8037b1eaccf24142d604471ff740773679090
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-27 09:18:47 +0530

    Refactored solutions 219 & 220

--- Solutions/219.py
+++ Solutions/219.py
@@ -1,18 +1,23 @@
 """
 Problem:
 
-Connect 4 is a game where opponents take turns dropping red or black discs into a 7 x 6 vertically suspended grid. 
-The game ends either when one player creates a line of four consecutive discs of their color (horizontally, vertically, or diagonally), or when there are no more spots left in the grid.
+Connect 4 is a game where opponents take turns dropping red or black discs into a 7 x 6
+vertically suspended grid. The game ends either when one player creates a line of four
+consecutive discs of their color (horizontally, vertically, or diagonally), or when
+there are no more spots left in the grid.
+
 Design and implement Connect 4.
 """
 
+from typing import Optional
+
 
 class Connect4:
-    def __init__(self):
+    def __init__(self) -> None:
         self.board = [["-" for _ in range(7)] for _ in range(6)]
         self.to_play = "R"
 
-    def get_empty(self, position):
+    def get_empty(self, position: int) -> int:
         if self.board[0][position] != "-":
             return -1
         if self.board[5][position] == "-":
@@ -22,7 +27,7 @@ class Connect4:
             if self.board[i][position] == "-" and self.board[i + 1][position] != "-":
                 return i
 
-    def play_turn(self):
+    def play_turn(self) -> None:
         position = int(input("Enter the location to put the disk: "))
         position -= 1
         row = self.get_empty(position)
@@ -39,74 +44,72 @@ class Connect4:
             self.board[row][position] = "B"
             self.to_play = "R"
 
-    def victory_check(self):
+    def victory_check(self) -> Optional[str]:
+        # horizontal check
         for i in range(6):
             for j in range(4):
                 if self.board[i][j] in ("R", "B"):
                     disk = self.board[i][j]
-
                     for k in range(j, j + 4):
                         if self.board[i][k] != disk:
                             break
                     else:
                         return disk
-
+        # vertical check
         for i in range(3):
             for j in range(7):
                 if self.board[i][j] in ("R", "B"):
                     disk = self.board[i][j]
-
                     for k in range(i, i + 4):
                         if self.board[k][j] != disk:
                             break
                     else:
                         return disk
-
+        # top left to bottom right diagonal check
         for i in range(2):
             for j in range(3):
                 if self.board[i][j] in ("R", "B"):
                     disk = self.board[i][j]
-
                     for k in range(4):
                         if self.board[i + k][j + k] != disk:
                             break
                     else:
                         return disk
-
+        # top right to bottom left diagonal check
         for i in range(3, 6):
             for j in range(4, 7):
                 if self.board[i][j] in ("R", "B"):
                     disk = self.board[i][j]
-
                     for k in range(4):
                         if self.board[i - k][j - k] != disk:
                             break
                     else:
                         return disk
-
         return None
 
-    def full_check(self):
+    def full_check(self) -> bool:
         for i in range(6):
             for j in range(7):
                 if self.board[i][j] == "-":
                     return False
-
         return True
 
-    def print_board(self):
+    def print_board(self) -> None:
         for i in range(6):
             for j in range(7):
                 print(self.board[i][j], end=" ")
             print()
 
-    def play(self):
+    def play(self) -> None:
         while not self.full_check() and not self.victory_check():
-            print(self.to_play, "to play")
-            self.play_turn()
-            print("Board: ")
-            self.print_board()
-            print()
+            try:
+                print(self.to_play, "to play")
+                self.play_turn()
+                print("Board: ")
+                self.print_board()
+                print()
+            except IndexError:
+                print("Illegal move!")
 
         if self.full_check():
             print("Its a draw")
@@ -114,5 +117,5 @@ class Connect4:
             print("{} has won".format(self.victory_check()))
 
 
-# DRIVER CODE
-Connect4().play()
+if __name__ == "__main__":
+    Connect4().play()
--- Solutions/220.py
+++ Solutions/220.py
@@ -2,27 +2,40 @@
 Problem:
 
 In front of you is a row of N coins, with values v_1, v_2, ..., v_n.
-You are asked to play the following game. 
-You and an opponent take turns choosing either the first or last coin from the row, removing it from the row, and receiving the value of the coin.
-Write a program that returns the maximum amount of money you can win with certainty, if you move first, assuming your opponent plays optimally.
+
+You are asked to play the following game. You and an opponent take turns choosing
+either the first or last coin from the row, removing it from the row, and receiving the
+value of the coin.
+
+Write a program that returns the maximum amount of money you can win with certainty, if
+you move first, assuming your opponent plays optimally.
 """
 
+from typing import List
 
-# FUNCTION TO PERFORM THE OPERATION
-def optimal_strategy_of_game(coins_arr, amount=0, to_play=True):
+
+def optimal_strategy_of_game(
+    coins_arr: List[int], amount: int = 0, to_play: bool = True
+) -> int:
     if not coins_arr:
         return amount
-
     if to_play:
         return max(
             optimal_strategy_of_game(coins_arr[1:], amount + coins_arr[0], False),
             optimal_strategy_of_game(coins_arr[:-1], amount + coins_arr[-1], False),
         )
-
     if coins_arr[0] > coins_arr[-1]:
         return optimal_strategy_of_game(coins_arr[1:], amount, True)
     return optimal_strategy_of_game(coins_arr[:-1], amount, True)
 
 
-# DRIVER CODE
-print(optimal_strategy_of_game([1, 2, 3, 4, 5]))
+if __name__ == "__main__":
+    print(optimal_strategy_of_game([1, 2, 3, 4, 5]))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(2 ^ n)
+SPACE COMPLEXITY: O(n)
+"""
