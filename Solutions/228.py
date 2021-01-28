d4730ef440330231f347d9f4a93e2427acef0d09
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-28 09:54:36 +0530

    Refactored solutions 226 - 228

--- Solutions/228.py
+++ Solutions/228.py
@@ -1,45 +1,45 @@
 """
 Problem:
 
-Given a list of numbers, create an algorithm that arranges them in order to form the largest possible integer. 
-
-Example:
-
-Input = [10, 7, 76, 415]
-Output = 77641510
+Given a list of numbers, create an algorithm that arranges them in order to form the
+largest possible integer. For example, given [10, 7, 76, 415], you should return
+77641510.
 """
 
+from __future__ import annotations
+from typing import List
 
-class ModifiedInt:
-    # helper class for the sort operation (requires __lt__ dunder method)
 
-    def __init__(self, val):
-        if type(val) != int:
-            raise TypeError
-        self.val = str(val)
+class CustomInt:
+    def __init__(self, value: int) -> None:
+        self.value = str(value)
 
-    def __lt__(self, other):
-        # returns the number with shorter length or has a smaller more significant digit
-        if type(other) == ModifiedInt:
-            if self.val == other.val:
+    def __lt__(self, other: CustomInt) -> bool:
+        if self.value == other.value:
+            return False
+        for c1, c2 in zip(self.value, other.value):
+            if c1 > c2:
                 return False
-
-            for c1, c2 in zip(self.val, other.val):
-                if c1 > c2:
-                    return False
-                elif c1 < c2:
-                    return True
-
-            if len(self.val) > len(other.val):
+            elif c1 < c2:
                 return True
+        if len(self.value) > len(other.value):
+            return True
         return False
 
 
-def get_largest(arr):
-    arr = list(map(ModifiedInt, arr))
+def get_largest(arr: List[int]) -> int:
+    arr = list(map(CustomInt, arr))
     arr.sort(reverse=True)
-    return int("".join(map(lambda x: x.val, arr)))
+    return int("".join(map(lambda x: x.value, arr)))
 
 
-# DRIVER CODE
-print(get_largest([10, 7, 76, 415]))
+if __name__ == "__main__":
+    print(get_largest([10, 7, 76, 415]))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n x log(n))
+SPACE COMPLEXITY: O(n)
+"""
