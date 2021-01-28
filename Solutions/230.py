3f246b0b448b8e631859829c136bc52ebe079366
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-28 10:06:50 +0530

    Refactored solutions 229 - 230

--- Solutions/230.py
+++ Solutions/230.py
@@ -17,21 +17,31 @@ Output = 5 (we will need to try dropping the egg at every floor, beginning with 
 from sys import maxsize
 
 
-def calculate(eggs, floors):
-    dp_mat = [[0 for _ in range(floors + 1)] for _ in range(eggs + 1)]
+def calculate(eggs: int, floors: int) -> int:
+    dp_mat = [[maxsize for _ in range(floors + 1)] for _ in range(eggs + 1)]
+    # base cases
     for i in range(floors + 1):
         dp_mat[1][i] = i
-
+        dp_mat[0][i] = 0
+    for i in range(eggs + 1):
+        dp_mat[i][0] = 0
+    # populating the dp matrix
     for egg in range(2, eggs + 1):
         for floor in range(1, floors + 1):
-            dp_mat[egg][floor] = maxsize
             for i in range(1, floor + 1):
                 temp = 1 + max(dp_mat[egg - 1][i - 1], dp_mat[egg][floor - i])
                 dp_mat[egg][floor] = min(dp_mat[egg][floor], temp)
-
     return dp_mat[eggs][floors]
 
 
-# DRIVER CODE
-print(calculate(2, 20))
-print(calculate(3, 15))
+if __name__ == "__main__":
+    print(calculate(2, 20))
+    print(calculate(3, 15))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n x (floor ^ 2))
+SPACE COMPLEXITY: O(n x floor)
+"""
