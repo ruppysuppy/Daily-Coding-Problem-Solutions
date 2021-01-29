e6c71e6257cc63b8a1e7c4ee872df6a2e50c88ae
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-29 09:28:51 +0530

    Refactored solutions 237 - 238

--- Solutions/237.py
+++ Solutions/237.py
@@ -1,8 +1,9 @@
 """
 Problem:
 
-A tree is symmetric if its data and shape remain unchanged when it is reflected about the root node. 
-The following tree is an example:
+A tree is symmetric if its data and shape remain unchanged when it is reflected about
+the root node. The following tree is an example:
+
         4
       / | \
     3   5   3
@@ -11,56 +12,66 @@ The following tree is an example:
 Given a k-ary tree, determine whether it is symmetric.
 """
 
-# Node class to store a k-ary tree
+from typing import Dict, List
+
+
 class Node:
-    def __init__(self, val):
+    def __init__(self, val: int) -> None:
         self.val = val
         self.children = []
 
-    def __str__(self):
+    def __str__(self) -> str:
         return "{} -> {}".format(self.val, self.children)
 
 
-def update_levels_dict(root, levels, lnum):
-    # DFS to generate the nodes in the tree by level (stored in levels dictionary)
-    if lnum not in levels:
-        levels[lnum] = []
-    levels[lnum].append(root.val)
+def generate_tree_levels(
+    root: Node, levels: Dict[int, List[int]], level_number: int
+) -> Dict[int, List[int]]:
+    # DFS to generate the nodes in the tree by level
+    if level_number not in levels:
+        levels[level_number] = []
+    levels[level_number].append(root.val)
     for child in root.children:
-        update_levels_dict(child, levels, lnum + 1)
+        generate_tree_levels(child, levels, level_number + 1)
+    return levels
 
 
-def is_symmetric(tree):
-    # getting the nodes by the level number
-    levels = {}
-    update_levels_dict(tree, levels, 0)
+def is_symmetric(node: Node) -> bool:
+    levels = generate_tree_levels(node, {}, 0)
     # checking if the tree is symmetric
-    for level in levels:
-        arr = levels[level]
-        if arr != arr[::-1]:
+    for level_values in levels.values():
+        if level_values != level_values[::-1]:
             return False
     return True
 
 
-# DRIVER CODE
-a = Node(4)
-b = Node(5)
-c = Node(3)
-d = Node(3)
-e = Node(9)
-f = Node(9)
+if __name__ == "__main__":
+    a = Node(4)
+    b = Node(5)
+    c = Node(3)
+    d = Node(3)
+    e = Node(9)
+    f = Node(9)
 
-a.children = [c, b, d]
+    a.children = [c, b, d]
 
-c.children = [f]
-d.children = [e]
+    c.children = [f]
+    d.children = [e]
 
-print(is_symmetric(a))
+    print(is_symmetric(a))
 
-c.children = [f, Node(1)]
-d.children = [Node(1), e]
+    c.children = [f, Node(1)]
+    d.children = [Node(1), e]
 
-print(is_symmetric(a))
+    print(is_symmetric(a))
 
-c.val = 4
-print(is_symmetric(a))
+    c.val = 4
+    print(is_symmetric(a))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n + e)
+SPACE COMPLEXITY: O(n)
+"""
--- Solutions/238.py
+++ Solutions/238.py
@@ -2,28 +2,34 @@
 Problem:
 
 Blackjack is a two player card game whose rules are as follows:
-* The player and then the dealer are each given two cards.
-* The player can then "hit", or ask for arbitrarily many additional cards, so long as their total does not exceed 21.
-* The dealer must then hit if their total is 16 or lower, otherwise pass.
-* Finally, the two compare totals, and the one with the greatest sum not exceeding 21 is the winner.
-For this problem, cards values are counted as follows: each card between 2 and 10 counts as their face value, face cards count as 10, and aces count as 1.
-Given perfect knowledge of the sequence of cards in the deck, implement a blackjack solver that maximizes the player's score (that is, wins minus losses).
+
+- The player and then the dealer are each given two cards.
+- The player can then "hit", or ask for arbitrarily many additional cards, so long as
+  their total does not exceed 21.
+- The dealer must then hit if their total is 16 or lower, otherwise pass.
+- Finally, the two compare totals, and the one with the greatest sum not exceeding 21
+  is the winner.
+
+For this problem, cards values are counted as follows: each card between 2 and 10
+counts as their face value, face cards count as 10, and aces count as 1.
+
+Given perfect knowledge of the sequence of cards in the deck, implement a blackjack
+solver that maximizes the player's score (that is, wins minus losses).
 """
 
-from random import randint
+from random import shuffle
+from typing import List, Tuple
 
 
-def generate_sequence():
-    # generating a sequence of random cards
-    sequence = []
+def generate_random_card_sequence() -> List[int]:
     cards = [i for _ in range(4) for i in range(1, 11)]
-    while cards:
-        sequence.append(cards.pop(randint(0, len(cards) - 1)))
-    return sequence
+    shuffle(cards)
+    return cards
 
 
-def get_best_score(sequence, player_score=0, dealer_score=0):
-    # getting the best possible score (player) for the given sequence of cards
+def get_best_player_score(
+    sequence: List[int], player_score: int = 0, dealer_score: int = 0
+) -> Tuple[int, int]:
     if not sequence:
         return player_score, dealer_score
     elif player_score > 21 and dealer_score > 21:
@@ -33,24 +39,33 @@ def get_best_score(sequence, player_score=0, dealer_score=0):
     elif dealer_score > 21:
         return player_score, -1
     return max(
-        get_best_score(sequence[1:], player_score + sequence[0], dealer_score),
-        get_best_score(sequence[1:], player_score, dealer_score + sequence[0]),
+        get_best_player_score(sequence[1:], player_score + sequence[0], dealer_score),
+        get_best_player_score(sequence[1:], player_score, dealer_score + sequence[0]),
         (player_score, dealer_score),
         # the player's score has more weightage than the dealer's score
         key=lambda x: 1.01 * x[0] + x[1],
     )
 
 
-def simulate(n=1000):
+def simulate(n: int = 1_000) -> float:
     # simulating the game n times and returning the percent of victories for the player
     wins = 0
     for _ in range(n):
-        sequence = generate_sequence()
-        player_score, dealer_score = get_best_score(sequence)
+        sequence = generate_random_card_sequence()
+        player_score, dealer_score = get_best_player_score(sequence)
         if player_score > dealer_score and player_score <= 21:
             wins += 1
     return (wins / n) * 100
 
 
-# DRIVER CODE
-print(f"The Player won {simulate():.2f}% of the times")
+if __name__ == "__main__":
+    print(f"The Player won {simulate():.2f}% of the times")
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(1)
+SPACE COMPLEXITY: O(1)
+[the number of cards is constant (52)]
+"""
