7c7cdfd10369e7fbb486c6a09937e13bcddea029
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-29 08:50:58 +0530

    Refactored solutions 231 - 232

--- Solutions/232.py
+++ Solutions/232.py
@@ -2,10 +2,13 @@
 Problem:
 
 Implement a PrefixMapSum class with the following methods:
-* insert(key: str, value: int): Set a given key's value in the map. If the key already exists, overwrite the value. 
-* sum(prefix: str): Return the sum of all values of keys that begin with a given prefix. 
+
+insert(key: str, value: int): Set a given key's value in the map. If the key already
+exists, overwrite the value.
+sum(prefix: str): Return the sum of all values of keys that begin with a given prefix.
 
 For example, you should be able to run the following code:
+
 >>> mapsum.insert("columnar", 3)
 >>> assert mapsum.sum("col") == 3
 >>> mapsum.insert("column", 2)
@@ -16,16 +19,16 @@ from DataStructures.Trie import Trie
 
 
 class PrefixMapSum:
-    def __init__(self):
+    def __init__(self) -> None:
         self.trie = Trie()
         self.hash_map = {}
 
-    def insert(self, key, value):
+    def insert(self, key: str, value: int) -> None:
         if key not in self.hash_map:
             self.trie.add(key)
         self.hash_map[key] = value
 
-    def sum(self, prefix):
+    def sum(self, prefix: str) -> int:
         words = self.trie.get_suggestions(prefix)
         result = 0
         for word in words:
@@ -33,11 +36,11 @@ class PrefixMapSum:
         return result
 
 
-# DRIVER CODE
-mapsum = PrefixMapSum()
+if __name__ == "__main__":
+    mapsum = PrefixMapSum()
 
-mapsum.insert("columnar", 3)
-print(mapsum.sum("col"))
+    mapsum.insert("columnar", 3)
+    assert mapsum.sum("col") == 3
 
-mapsum.insert("column", 2)
-print(mapsum.sum("col"))
+    mapsum.insert("column", 2)
+    assert mapsum.sum("col") == 5
