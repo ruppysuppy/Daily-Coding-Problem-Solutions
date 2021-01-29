21783b53ec707c84b5507ff7ab9a7796b5cc61b2
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-29 08:50:58 +0530

    Refactored solutions 231 - 232

--- Solutions/231.py
+++ Solutions/231.py
@@ -1,79 +1,80 @@
 """
 Problem:
 
-Given a string with repeated characters, rearrange the string so that no two adjacent characters are the same. 
-If this is not possible, return None.
+Given a string with repeated characters, rearrange the string so that no two adjacent
+characters are the same. If this is not possible, return None.
 
-Example:
-
-Input = "aaabbc"
-Output = "ababac"
-
-Input = "aaab"
-Output = None
+For example, given "aaabbc", you could return "ababac". Given "aaab", return None.
 """
 
+from typing import Optional
 
-def get_unique_adjacent(string):
+from DataStructures.Queue import Queue
+
+
+def get_unique_adjacent(string: str) -> Optional[str]:
     length = len(string)
     freq = {}
-
     if length == 0:
         return string
 
-    # getting the character frequency
     for i in range(length):
-        if string[i] in freq:
-            freq[string[i]] += 1
-        else:
-            freq[string[i]] = 1
+        if string[i] not in freq:
+            freq[string[i]] = 0
+        freq[string[i]] += 1
 
     sorted_freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
-    queue = list(sorted_freq)
+    queue = Queue()
+    [queue.enqueue(item) for item in sorted_freq]
+    result = ""
 
-    # checking if a desired string can be formed
-    if length % 2 == 0:
-        if sorted_freq[0][1] > length // 2:
-            return None
-    else:
-        if sorted_freq[0][1] > (length // 2) + 1:
-            return None
+    if length % 2 == 0 and sorted_freq[0][1] > length // 2:
+        return None
+    elif length % 2 == 1 and sorted_freq[0][1] > (length // 2) + 1:
+        return None
 
-    res = ""
-
-    # creating the required string
-    while queue:
+    while not queue.is_empty():
         if len(queue) == 1:
-            if queue[0][1] == 2:
-                res = queue[0][0] + res + queue[0][0]
+            elem, freq = queue.peek()
+            if freq == 2:
+                result = elem + result + elem
                 break
-            elif queue[0][1] == 1:
-                if res[-1] != queue[0][0]:
-                    res += queue[0][0]
+            elif freq == 1:
+                if result[-1] != elem:
+                    result += elem
                 else:
-                    res = queue[0][0] + res
+                    result = elem + result
                 break
-            else:
-                return None
+            return None
 
-        res += queue[0][0] + queue[1][0]
+        elem1, freq1 = queue.peek()
+        elem2, freq2 = None, None
+        if len(queue) > 1:
+            elem2, freq2 = queue[1]
 
-        queue[0] = queue[0][0], queue[0][1] - 1
-        queue[1] = queue[1][0], queue[1][1] - 1
+        result += elem1 + elem2
 
+        queue[0] = elem1, freq1 - 1
+        if len(queue) > 1:
+            queue[1] = elem2, freq2 - 1
         if len(queue) > 1 and queue[1][1] == 0:
-            queue.pop(1)
+            queue.dequeue()
         if len(queue) > 0 and queue[0][1] == 0:
-            queue.pop(0)
+            queue.dequeue()
+    return result
 
-    return res
+
+if __name__ == "__main__":
+    print(get_unique_adjacent("aaabbc"))
+    print(get_unique_adjacent("aaabbcc"))
+    print(get_unique_adjacent("aaabbac"))
+    print(get_unique_adjacent("aaab"))
+    print(get_unique_adjacent("aaabbaa"))
 
 
-# DRIVER CODE
-print(get_unique_adjacent("aaabbc"))
-print(get_unique_adjacent("aaabbcc"))
-print(get_unique_adjacent("aaabbac"))
+"""
+SPECS:
 
-# cannot form a word of the desired form
-print(get_unique_adjacent("aaab"))
-print(get_unique_adjacent("aaabbaa"))
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(n)
+"""
