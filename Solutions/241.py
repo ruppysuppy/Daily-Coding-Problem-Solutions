ff72b8bbe9457555a0e314d37ab8b933863958aa
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-29 09:50:27 +0530

    Refactored solutions 241 - 242

--- Solutions/241.py
+++ Solutions/241.py
@@ -1,21 +1,24 @@
 """
 Problem:
 
-In academia, the h-index is a metric used to calculate the impact of a researcher's papers. 
-It is calculated as follows:
-A researcher has index h if at least h of her N papers have h citations each. 
-If there are multiple h satisfying this formula, the maximum is chosen.
+In academia, the h-index is a metric used to calculate the impact of a researcher's
+papers. It is calculated as follows:
+
+A researcher has index h if at least h of her N papers have h citations each. If there
+are multiple h satisfying this formula, the maximum is chosen.
+
+For example, suppose N = 5, and the respective citations of each paper are
+[4, 3, 0, 1, 5]. Then the h-index would be 3, since the researcher has 3 papers with at
+least 3 citations.
+
 Given a list of paper citations of a researcher, calculate their h-index.
-
-Example:
-
-N = 5
-citations = [4, 3, 0, 1, 5]
-Output = 3 (since the researcher has 3 papers with at least 3 citations)
 """
 
 
-def get_h_index(citations):
+from typing import List
+
+
+def get_h_index(citations: List[int]) -> int:
     citations.sort(reverse=True)
     for index, citation in enumerate(citations):
         if index >= citation:
@@ -24,7 +27,15 @@ def get_h_index(citations):
     return 0
 
 
-# DRIVER CODE
-print(get_h_index([4, 3, 0, 1, 5]))
-print(get_h_index([4, 1, 0, 1, 1]))
-print(get_h_index([4, 4, 4, 5, 4]))
+if __name__ == "__main__":
+    print(get_h_index([4, 3, 0, 1, 5]))
+    print(get_h_index([4, 1, 0, 1, 1]))
+    print(get_h_index([4, 4, 4, 5, 4]))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n x log(n))
+SPACE COMPLEXITY: O(1)
+"""
--- Solutions/242.py
+++ Solutions/242.py
@@ -1,33 +1,38 @@
 """
 Problem:
 
-You are given an array of length 24, where each element represents the number of new subscribers during the corresponding hour.
-Implement a data structure that efficiently supports the following:
-* update(hour: int, value: int): Increment the element at index hour by value.
-* query(start: int, end: int): Retrieve the number of subscribers that have signed up between start and end (inclusive).
-You can assume that all values get cleared at the end of the day, and that you will not be asked for start and end values that wrap around midnight.
+You are given an array of length 24, where each element represents the number of new
+subscribers during the corresponding hour. Implement a data structure that efficiently
+supports the following:
+
+- update(hour: int, value: int): Increment the element at index hour by value.
+- query(start: int, end: int): Retrieve the number of subscribers that have signed up
+  between start and end (inclusive).
+
+You can assume that all values get cleared at the end of the day, and that you will not
+be asked for start and end values that wrap around midnight.
 """
 
 
 class Hourly_Subscribers:
-    def __init__(self):
+    def __init__(self) -> None:
         self.sub_count = [0 for _ in range(24)]
 
-    def update(self, hour, value):
+    def update(self, hour: int, value: int) -> None:
         self.sub_count[hour - 1] += value
 
-    def query(self, start, end):
+    def query(self, start: int, end: int) -> int:
         return sum(self.sub_count[start : end + 1])
 
 
-# DRIVER CODE
-hs = Hourly_Subscribers()
+if __name__ == "__main__":
+    hs = Hourly_Subscribers()
 
-hs.update(2, 50)
-hs.update(5, 100)
+    hs.update(2, 50)
+    hs.update(5, 100)
 
-print(hs.query(1, 7))
+    print(hs.query(1, 7))
 
-hs.update(2, 10)
+    hs.update(2, 10)
 
-print(hs.query(1, 7))
+    print(hs.query(1, 7))
