9269bd3a204f481fb973e6a4e946fe2750b84d8a
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-29 10:02:19 +0530

    Refactored solutions 243 - 244

--- Solutions/243.py
+++ Solutions/243.py
@@ -1,41 +1,45 @@
 """
 Problem:
 
-Given an array of numbers N and an integer k, your task is to split N into k partitions such that the maximum sum of any partition is minimized. 
-Return this sum.
+Given an array of numbers N and an integer k, your task is to split N into k partitions
+such that the maximum sum of any partition is minimized. Return this sum.
 
-Example:
-
-N = [5, 1, 2, 7, 3, 4]
-k = 3
-Output = 8 (since the optimal partition is [5, 1, 2], [7], [3, 4])
+For example, given N = [5, 1, 2, 7, 3, 4] and k = 3, you should return 8, since the
+optimal partition is [5, 1, 2], [7], [3, 4].
 """
 
 from sys import maxsize
+from typing import List, Tuple
 
 
-def minimize_partition_sum_helper(arr, k):
-    # if the number of partition is 1, using the entire array as partition
+def minimize_partition_sum_helper(arr: List[int], k: int) -> Tuple[List[int], int]:
     if k == 1:
         return [arr], sum(arr)
 
-    min_val = maxsize
-    min_cand = None
-    # checking all possible partitions
+    min_value = maxsize
+    min_candidate = None
     for i in range(len(arr)):
         arr_1, sum_1 = [arr[:i]], sum(arr[:i])
         arr_2, sum_2 = minimize_partition_sum_helper(arr[i:], k - 1)
         candidate = arr_1 + arr_2, max(sum_1, sum_2)
-        if candidate[1] < min_val:
-            min_val = candidate[1]
-            min_cand = candidate
-    return min_cand
+        if candidate[1] < min_value:
+            min_value = candidate[1]
+            min_candidate = candidate
+    return min_candidate
 
 
-def minimize_partition_sum(arr, k):
+def minimize_partition_sum(arr: List[int], k: int) -> int:
     _, max_sum = minimize_partition_sum_helper(arr, k)
     return max_sum
 
 
-# DRIVER CODE
-print(minimize_partition_sum([5, 1, 2, 7, 3, 4], 3))
+if __name__ == "__main__":
+    print(minimize_partition_sum([5, 1, 2, 7, 3, 4], 3))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n ^ 2)
+SPACE COMPLEXITY: O(n ^ 2)
+"""
--- Solutions/244.py
+++ Solutions/244.py
@@ -1,28 +1,33 @@
 """
 Problem:
 
-The Sieve of Eratosthenes is an algorithm used to generate all prime numbers smaller than N.
-The method is to take increasingly larger prime numbers, and mark their multiples as composite.
-For example, to find all primes less than 100, we would first mark [4, 6, 8, ...] (multiples of two), then [6, 9, 12, ...] (multiples of three), and so on.
-Once we have done this for all primes less than N, the unmarked numbers that remain will be prime.
+The Sieve of Eratosthenes is an algorithm used to generate all prime numbers smaller
+than N. The method is to take increasingly larger prime numbers, and mark their
+multiples as composite.
+
+For example, to find all primes less than 100, we would first mark [4, 6, 8, ...]
+(multiples of two), then [6, 9, 12, ...] (multiples of three), and so on. Once we have
+done this for all primes less than N, the unmarked numbers that remain will be prime.
+
 Implement this algorithm.
 
-Bonus: Create a generator that produces primes indefinitely (that is, without taking N as an input).
+Bonus: Create a generator that produces primes indefinitely (that is, without taking N
+as an input).
 """
 
 
-def SoE(sieve=[]):
-    # if an sieve is passed, its doubled in size to generate a larger sieve
+from typing import Generator, List
+
+
+def sieve_of_eratosthenes(sieve: List[int] = []) -> List[int]:
     if sieve:
         length = len(sieve)
         sieve.extend([True for _ in range(length)])
-    # if no sieve is passed, a sieve is formed with 20 numbers
     else:
         length = 10
         sieve = [True for _ in range(length * 2)]
         sieve[0], sieve[1] = False, False
-
-    # computing the sieve
+    # sieve generation
     for i in range(2, 2 * length):
         if sieve[i]:
             for j in range(2 * i, 2 * length, i):
@@ -30,21 +35,26 @@ def SoE(sieve=[]):
     return sieve
 
 
-def primes_generator():
-    # generator function to keep yielding prime numbers
-    primes = SoE()
+def primes_generator() -> Generator[int, None, None]:
+    primes = sieve_of_eratosthenes()
     prev = 0
     while True:
-        # returning the prime numbers and doubling the size of primes array after the
-        # current array is exhausted
         for i in range(prev, len(primes)):
             if primes[i]:
                 yield i
         prev = len(primes)
-        primes = SoE(primes)
+        primes = sieve_of_eratosthenes(primes)
 
 
-# DRIVER CODE
-generator = primes_generator()
-for _ in range(30):
-    print(next(generator))
+if __name__ == "__main__":
+    generator = primes_generator()
+    for _ in range(35):
+        print(next(generator))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(n)
+"""
