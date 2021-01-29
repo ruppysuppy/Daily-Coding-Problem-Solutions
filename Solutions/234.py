ed625e35a08f1f5486594720f62f71f2ba67e0b2
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-29 09:00:42 +0530

    Refactored solutions 233 - 234

--- Solutions/234.py
+++ Solutions/234.py
@@ -1,58 +1,73 @@
 """
 Problem:
 
-Recall that the minimum spanning tree is the subset of edges of a tree that connect all its vertices with the smallest possible total edge weight. 
-Given an undirected graph with weighted edges, compute the maximum weight spanning tree.
+Recall that the minimum spanning tree is the subset of edges of a tree that connect all
+its vertices with the smallest possible total edge weight. Given an undirected graph
+with weighted edges, compute the maximum weight spanning tree.
 """
 
-from DataStructures.Graph import Graph_Undirected_Weighted
+from typing import Set
+
+from DataStructures.Graph import GraphUndirectedWeighted
 
 
-def get_max_span_helper(graph, start, remaining, score):
-    if not remaining:
-        return score
+def get_maximum_spanning_tree_helper(
+    graph: GraphUndirectedWeighted,
+    curr_node: int,
+    remaining_nodes: Set[int],
+    weight: int,
+) -> int:
+    if not remaining_nodes:
+        return weight
 
     scores = []
-
-    for dest in graph.connections[start]:
-        if dest in remaining:
-            rem_cp = set(remaining)
-            rem_cp.remove(dest)
-
-            new_score = get_max_span_helper(
-                graph, dest, rem_cp, score + graph.connections[start][dest]
+    for destination in graph.connections[curr_node]:
+        if destination in remaining_nodes:
+            rem_cp = set(remaining_nodes)
+            rem_cp.remove(destination)
+            new_score = get_maximum_spanning_tree_helper(
+                graph,
+                destination,
+                rem_cp,
+                weight + graph.connections[curr_node][destination],
             )
-
             scores.append(new_score)
-
     return max(scores)
 
 
-def get_max_span(graph):
+def get_maximum_spanning_tree(graph: GraphUndirectedWeighted) -> int:
     node_set = set(graph.connections.keys())
     start_node = node_set.pop()
 
-    score = get_max_span_helper(graph, start_node, node_set, 0)
-    return score
+    weight = get_maximum_spanning_tree_helper(graph, start_node, node_set, 0)
+    return weight
 
 
-# DRIVER CODE
-graph = Graph_Undirected_Weighted()
+if __name__ == "__main__":
+    graph = GraphUndirectedWeighted()
 
-graph.add_edge(1, 2, 5)
-graph.add_edge(1, 3, 2)
-graph.add_edge(3, 2, 1)
-graph.add_edge(3, 4, 3)
-graph.add_edge(2, 4, 4)
+    graph.add_edge(1, 2, 5)
+    graph.add_edge(1, 3, 2)
+    graph.add_edge(3, 2, 1)
+    graph.add_edge(3, 4, 3)
+    graph.add_edge(2, 4, 4)
 
-print(graph.connections)
-print(get_max_span(graph))
+    print(graph)
+    print(get_maximum_spanning_tree(graph))
 
-graph = Graph_Undirected_Weighted()
+    graph = GraphUndirectedWeighted()
 
-graph.add_edge(1, 2, 1)
-graph.add_edge(1, 3, 2)
-graph.add_edge(3, 2, 3)
+    graph.add_edge(1, 2, 1)
+    graph.add_edge(1, 3, 2)
+    graph.add_edge(3, 2, 3)
 
-print(graph.connections)
-print(get_max_span(graph))
+    print(graph)
+    print(get_maximum_spanning_tree(graph))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n x e)
+SPACE COMPLEXITY: O(n x e)
+"""
