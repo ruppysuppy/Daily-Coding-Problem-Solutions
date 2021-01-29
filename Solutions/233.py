354875adcbd984d20ca9ab908278cb6002a053f4
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-29 09:00:42 +0530

    Refactored solutions 233 - 234

--- Solutions/233.py
+++ Solutions/233.py
@@ -1,17 +1,26 @@
 """
 Problem:
 
-Implement the function fib(n), which returns the nth number in the Fibonacci sequence, using only O(1) space.
+Implement the function fib(n), which returns the nth number in the Fibonacci sequence,
+using only O(1) space.
 """
 
 
-def fib(n):
+def fib(n: int) -> int:
     curr, last = 1, 0
     for _ in range(n - 1):
         curr, last = last + curr, curr
     return curr
 
 
-# DRIVER CODE
-for i in range(1, 11):
-    print(f"Fib {i}:\t{fib(i)}")
+if __name__ == "__main__":
+    for i in range(1, 11):
+        print(f"Fib {i}:\t{fib(i)}")
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(1)
+"""
