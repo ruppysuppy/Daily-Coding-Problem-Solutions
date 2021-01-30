53f6848250daa70aafc96fdb38a38f5ba47be4e0
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-30 08:38:23 +0530

    Refactored solutions 245 - 246

--- Solutions/246.py
+++ Solutions/246.py
@@ -1,41 +1,46 @@
 """
 Problem:
 
-Given a list of words, determine whether the words can be chained to form a circle.
-A word X can be placed in front of another word Y in a circle if the last character of X is same as the first character of Y.
+Given a list of words, determine whether the words can be chained to form a circle. A
+word X can be placed in front of another word Y in a circle if the last character of X
+is same as the first character of Y.
 
-Example:
-
-Input = ['chair', 'height', 'racket', 'touch', 'tunic']
-Output = True (chair -> racket -> touch -> height -> tunic -> chair)
+For example, the words ['chair', 'height', 'racket', 'touch', 'tunic'] can form the
+following circle: chair -> racket -> touch -> height -> tunic -> chair.
 """
 
 
-def check_circle_formation_helper(word_list, start, end, curr, start_word, seen):
-    # checking if all words have been used
+from typing import Dict, List, Set
+
+
+def check_circle_formation_helper(
+    word_list: List[str],
+    start: Dict[str, Set[str]],
+    end: Dict[str, Set[str]],
+    curr: str,
+    start_word: str,
+    seen: Set[str],
+) -> bool:
     if len(seen) == len(word_list):
-        # checking if a cycle is formed
         if start_word[0] == curr[-1]:
             return True
         return False
     try:
-        # iterating over all possible words from the current word
         for word in start[curr[-1]]:
             if word not in seen:
                 seen_copy = seen.copy()
                 seen_copy.add(word)
-                # checking if a cycle can be formed
                 if check_circle_formation_helper(
                     word_list, start, end, word, start_word, seen_copy
                 ):
                     return True
-    except KeyError:  # incase the current word's last character isn't present in start
+    except KeyError:
+        # the current word's last character isn't present in start dictionary
         pass
     return False
 
 
-def check_circle_formation(word_list):
-    # generating the start and end maps
+def check_circle_formation(word_list: List[str]) -> bool:
     start = {}
     end = {}
     for word in word_list:
@@ -54,16 +59,24 @@ def check_circle_formation(word_list):
     return False
 
 
-# DRIVER CODE
-print(
-    check_circle_formation(["chair", "height", "racket", "touch", "tunic"])
-)  # chair, racket, touch, height, tunic, chair
-print(
-    check_circle_formation(["height", "racket", "touch", "tunic", "car"])
-)  # racket, touch, height, tunic, car, racket
-print(
-    check_circle_formation(["height", "racket", "touch", "tunic"])
-)  # racket, touch, height, tunic (but no looping)
-print(
-    check_circle_formation(["height", "racket", "touch", "tunic", "cat"])
-)  # no looping
+if __name__ == "__main__":
+    print(
+        check_circle_formation(["chair", "height", "racket", "touch", "tunic"])
+    )  # chair, racket, touch, height, tunic, chair
+    print(
+        check_circle_formation(["height", "racket", "touch", "tunic", "car"])
+    )  # racket, touch, height, tunic, car, racket
+    print(
+        check_circle_formation(["height", "racket", "touch", "tunic"])
+    )  # racket, touch, height, tunic (no looping even though there is a chain)
+    print(
+        check_circle_formation(["height", "racket", "touch", "tunic", "cat"])
+    )  # no looping
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n ^ 2)
+SPACE COMPLEXITY: O(n)
+"""
