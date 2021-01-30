fc63fc50e128d92914cac14fd8c7bc90d01fff95
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-30 08:38:23 +0530

    Refactored solutions 245 - 246

--- Solutions/245.py
+++ Solutions/245.py
@@ -1,24 +1,23 @@
 """
 Problem:
 
-You are given an array of integers, where each element represents the maximum number of steps that can be jumped going forward from that element.
-Write a function to return the minimum number of jumps you must take in order to get from the start to the end of the array.
+You are given an array of integers, where each element represents the maximum number of
+steps that can be jumped going forward from that element. Write a function to return
+the minimum number of jumps you must take in order to get from the start to the end of
+the array.
 
-Example:
-
-Input = [6, 2, 4, 0, 5, 1, 1, 4, 2, 9]
-Output = 2 (as the optimal solution involves jumping from 6 to 5, and then from 5 to 9)
+For example, given [6, 2, 4, 0, 5, 1, 1, 4, 2, 9], you should return 2, as the optimal
+solution involves jumping from 6 to 5, and then from 5 to 9.
 """
 
 from sys import maxsize
+from typing import List
 
 
-def get_min_jumps(arr):
+def get_min_jumps(arr: List[int]) -> int:
     length = len(arr)
-    # using dynamic programming to reduce O(n ^ n) problem to O(n ^ 2)
     dp = [0 for _ in range(length)]
     for i in range(length - 2, -1, -1):
-        # updating the minimum hops for each position
         if arr[i]:
             dp[i] = min(dp[i + 1 : i + arr[i] + 1]) + 1
         else:
@@ -26,5 +25,13 @@ def get_min_jumps(arr):
     return dp[0]
 
 
-# DRIVER CODE
-print(get_min_jumps([6, 2, 4, 0, 5, 1, 1, 4, 2, 9]))
+if __name__ == "__main__":
+    print(get_min_jumps([6, 2, 4, 0, 5, 1, 1, 4, 2, 9]))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n ^ 2)
+SPACE COMPLEXITY: O(n)
+"""
