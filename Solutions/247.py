bc3b9654669c80d3d3b89a83377ec1a6116ef158
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-30 08:44:59 +0530

    Refactored solutions 247 - 248

--- Solutions/247.py
+++ Solutions/247.py
@@ -1,51 +1,60 @@
 """
 Problem:
 
-Given a binary tree, determine whether or not it is height-balanced.
-A height-balanced binary tree can be defined as one in which the heights of the two subtrees of any node never differ by more than one.
+Given a binary tree, determine whether or not it is height-balanced. A height-balanced
+binary tree can be defined as one in which the heights of the two subtrees of any node
+never differ by more than one.
 """
 
-from DataStructures.Tree import Node, Binary_Tree
+from typing import Tuple
+
+from .DataStructures.Tree import Node, BinaryTree
 
 
-def height_helper(self):
-    # generating the left and right heights and the balance
-    if self.left == None:
+def height_helper(node: Node) -> Tuple[int, bool]:
+    if node.left is None:
         left_height, balance_left = 0, True
     else:
-        left_height, balance_left = self.left.height_helper()
-    if self.right == None:
+        left_height, balance_left = height_helper(node.left)
+    if node.right is None:
         right_height, balance_right = 0, True
     else:
-        right_height, balance_right = self.right.height_helper()
-    # returning the current balance factor and the balance check
+        right_height, balance_right = height_helper(node.right)
+
     balance = balance_left and balance_right
     current_balance = -1 <= (right_height - left_height) <= 1
     height = max(left_height, right_height) + 1
     return height, balance and current_balance
 
 
-def check_balance(self):
-    _, balance = self.root.height_helper()
+def check_balance(tree: BinaryTree) -> bool:
+    if tree.root is None:
+        return True
+    _, balance = height_helper(tree.root)
     return balance
 
 
-# adding required methods to the classes
-setattr(Node, "height_helper", height_helper)
-setattr(Binary_Tree, "check_balance", check_balance)
+if __name__ == "__main__":
+    tree = BinaryTree()
+
+    tree.root = Node(0)
+
+    tree.root.left = Node(1)
+    tree.root.right = Node(2)
+
+    tree.root.left.left = Node(3)
+    tree.root.left.right = Node(4)
+
+    print(check_balance(tree))
+
+    tree.root.left.right.left = Node(5)
+
+    print(check_balance(tree))
 
 
-# DRIVER CODE
-tree = Binary_Tree()
+"""
+SPECS:
 
-tree.root = Node(0)
-tree.root.left = Node(1)
-tree.root.right = Node(2)
-tree.root.left.left = Node(3)
-tree.root.left.right = Node(4)
-
-print(tree.check_balance())
-
-tree.root.left.right.left = Node(5)
-
-print(tree.check_balance())
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(log(n))
+"""
