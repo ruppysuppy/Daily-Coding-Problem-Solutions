ad54aa910071f36cbeb4a0996fedf397fbf0e0df
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-31 09:18:30 +0530

    Refactored solutions 255 - 256

--- Solutions/255.py
+++ Solutions/255.py
@@ -1,7 +1,9 @@
 """
 Problem:
 
-The transitive closure of a graph is a measure of which vertices are reachable from other vertices. It can be represented as a matrix M, where M[i][j] == 1 if there is a path between vertices i and j, and otherwise 0.
+The transitive closure of a graph is a measure of which vertices are reachable from
+other vertices. It can be represented as a matrix M, where M[i][j] == 1 if there is a
+path between vertices i and j, and otherwise 0.
 
 For example, suppose we are given the following graph in adjacency list form:
 
@@ -21,26 +23,44 @@ Given a graph, find its transitive closure.
 """
 
 
-def get_transitive_helper(origin, curr_node, graph, transitive_matrix, visited):
+from typing import List, Set
+
+
+def get_transitive_matrix_helper(
+    origin: int,
+    curr_node: int,
+    graph: List[List[int]],
+    transitive_matrix: List[List[int]],
+    visited: Set[int],
+) -> None:
     # helper function to generate the transitive matrix using dfs
     for node in graph[curr_node]:
         transitive_matrix[origin][node] = 1
         if node not in visited:
             visited.add(node)
-            get_transitive_helper(origin, node, graph, transitive_matrix, visited)
+            get_transitive_matrix_helper(
+                origin, node, graph, transitive_matrix, visited
+            )
 
 
-def get_transitive(graph):
+def get_transitive_matrix(graph: List[List[int]]) -> List[List[int]]:
     num_nodes = len(graph)
-    # creating and updating the transitive matrix
     transitive_matrix = [[0 for _ in range(num_nodes)] for _ in range(num_nodes)]
     for node in range(num_nodes):
-        get_transitive_helper(node, node, graph, transitive_matrix, set([node]))
+        get_transitive_matrix_helper(node, node, graph, transitive_matrix, set([node]))
     return transitive_matrix
 
 
-# DRIVER CODE
-graph = [[0, 1, 3], [1, 2], [2], [3]]
+if __name__ == "__main__":
+    graph = [[0, 1, 3], [1, 2], [2], [3]]
 
-for row in get_transitive(graph):
-    print(*row)
+    for row in get_transitive_matrix(graph):
+        print(*row)
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n ^ 2)
+SPACE COMPLEXITY: O(n ^ 2)
+"""
