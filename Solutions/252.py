e74c24fce74d3e7887936413b5563764d662fc60
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-31 08:50:53 +0530

    Refactored solutions 251 - 252

--- Solutions/252.py
+++ Solutions/252.py
@@ -1,26 +1,38 @@
 """
 Problem:
 
-The ancient Egyptians used to express fractions as a sum of several terms where each numerator is one. 
-Create an algorithm to turn an ordinary fraction a / b, where a < b, into an Egyptian fraction.
-For example, 4 / 13 can be represented as 1 / 4 + 1 / 18 + 1 / 468.
+The ancient Egyptians used to express fractions as a sum of several terms where each
+numerator is one. For example, 4 / 13 can be represented as
+1 / (4 + 1 / (18 + (1 / 468))).
+
+Create an algorithm to turn an ordinary fraction a / b, where a < b, into an Egyptian
+fraction.
 """
 
 from fractions import Fraction
 from math import ceil
+from typing import List
 
 
-def get_egyptian_frac(frac, prev_frac=list()):
-    # base case for recursion
-    if frac.numerator == 1:
-        prev_frac.append(frac)
-        return prev_frac
-    # generating the next fraction
-    egyptian_frac = Fraction(1, ceil(frac.denominator / frac.numerator))
-    prev_frac.append(egyptian_frac)
-    # calling the function recursively
-    return get_egyptian_frac(frac - egyptian_frac, prev_frac)
+def get_egyptian_frac(
+    fraction: Fraction, previous_fraction: List[Fraction] = list()
+) -> List[Fraction]:
+    if fraction.numerator == 1:
+        previous_fraction.append(fraction)
+        return previous_fraction
+
+    egyptian_fraction = Fraction(1, ceil(fraction.denominator / fraction.numerator))
+    previous_fraction.append(egyptian_fraction)
+    return get_egyptian_frac(fraction - egyptian_fraction, previous_fraction)
 
 
-# DRIVER CODE
-print(get_egyptian_frac(Fraction(4, 13)))
+if __name__ == "__main__":
+    print(get_egyptian_frac(Fraction(4, 13)))
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(log(n))
+SPACE COMPLEXITY: O(log(n))
+"""
