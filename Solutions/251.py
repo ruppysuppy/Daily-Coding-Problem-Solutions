91b828195901083c70c73638d4254bc4b26461cf
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-31 08:50:53 +0530

    Refactored solutions 251 - 252

--- Solutions/251.py
+++ Solutions/251.py
@@ -1,19 +1,20 @@
 """
 Problem:
 
-Given an array of a million integers between zero and a billion, out of order, how can you efficiently sort it?
-Assume that you cannot store an array of a billion elements in memory.
+Given an array of a million integers between zero and a billion, out of order, how can
+you efficiently sort it? Assume that you cannot store an array of a billion elements in
+memory.
 """
 
 from random import randint
+from typing import List
 
 
-def gen_arr():
-    # function to generate the required array
+def gen_arr() -> List[int]:
     return [randint(0, 1_000_000_000) for _ in range(1_000_000)]
 
 
-def countingSort(arr, exp, n):
+def counting_sort(arr: List[int], exp: int, n: int) -> int:
     # output array elements to store the sorted arr
     output = [0] * n
     count = [0] * 10
@@ -33,17 +34,22 @@ def countingSort(arr, exp, n):
         arr[i] = output[i]
 
 
-# Method to do Radix Sort
-def radixSort(arr):
+def radix_sort(arr: List[int]) -> None:
     length = len(arr)
-    # find the number digits in the largest number (generalized for any array)
     digits = len(str(max(arr)))
-    # perform counting sort for every digit
     exp = 1
     for _ in range(digits):
-        countingSort(arr, exp, length)
+        counting_sort(arr, exp, length)
         exp *= 10
 
 
-# DRIVER CODE
-radixSort(gen_arr())
+if __name__ == "__main__":
+    radix_sort(gen_arr())
+
+
+"""
+SPECS:
+
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(range of the numbers)
+"""
