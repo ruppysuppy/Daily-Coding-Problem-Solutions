f81f84975465e28e583496d5952e3387f113569d
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-31 09:18:30 +0530

    Refactored solutions 255 - 256

--- Solutions/256.py
+++ Solutions/256.py
@@ -1,38 +1,42 @@
 """
 Problem:
 
-Given a linked list, rearrange the node values such that they appear in alternating low -> high -> low -> high ... form. For example, given 1 -> 2 -> 3 -> 4 -> 5, you should return 1 -> 3 -> 2 -> 5 -> 4.
+Given a linked list, rearrange the node values such that they appear in alternating
+low -> high -> low -> high ... form. For example, given 1 -> 2 -> 3 -> 4 -> 5, you
+should return 1 -> 3 -> 2 -> 5 -> 4.
 """
 
-from DataStructures.LinkedList import Node, Linked_list
+from DataStructures.LinkedList import Node, LinkedList
 
 
-def rearrange(self):
-    # getting the nodes in sorted format
-    nodes = [int(node) for node in self]
+def rearrange(ll: LinkedList) -> None:
+    nodes_count = len(ll)
+    nodes = [int(node) for node in ll]
     nodes.sort()
-    # interleaving the nodes to form alternating pattern (low -> high -> low ...)
-    for i in range(2, len(nodes), 2):
+
+    for i in range(2, nodes_count, 2):
         nodes[i], nodes[i - 1] = nodes[i - 1], nodes[i]
-    # updating the linked list
-    curr = self.head
-    for i in range(len(nodes)):
+
+    curr = ll.head
+    for i in range(nodes_count):
         curr.val = nodes[i]
         curr = curr.next
 
 
-# adding the rearrange method to the class
-setattr(Linked_list, "rearrange", rearrange)
+if __name__ == "__main__":
+    LL = LinkedList()
+
+    for i in range(1, 6):
+        LL.add(i)
+
+    print(LL)
+    rearrange(LL)
+    print(LL)
 
 
-# DRIVER CODE
-LL = Linked_list()
+"""
+SPECS:
 
-for i in range(1, 6):
-    LL.add(i)
-
-print(LL)
-
-LL.rearrange()
-
-print(LL)
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(n)
+"""
