b8d5a2d1c54eb4f4eff76a10b8180b9cf254fd76
Tapajyoti Bose <ruppysuppy@gmail.com>
2021-01-31 09:27:03 +0530

    Refactored solutions 257 - 258

--- Solutions/258.py
+++ Solutions/258.py
@@ -1,7 +1,9 @@
 """
 Problem:
 
-In Ancient Greece, it was common to write text with the first line going left to right, the second line going right to left, and continuing to go back and forth. This style was called "boustrophedon".
+In Ancient Greece, it was common to write text with the first line going left to right,
+the second line going right to left, and continuing to go back and forth. This style
+was called "boustrophedon".
 
 Given a binary tree, write an algorithm to print the nodes in boustrophedon order.
 
@@ -16,26 +18,29 @@ For example, given the following tree:
 You should return [1, 3, 2, 4, 5, 6, 7].
 """
 
-from DataStructures.Tree import Node, Binary_Tree
+from typing import Dict, List
+from .DataStructures.Tree import Node, BinaryTree
 
 
-def get_boustrophedon_helper(self, level, accumulator):
+def get_boustrophedon_helper(
+    node: Node, level: int, accumulator: Dict[int, List[int]]
+) -> None:
     # using dfs to store a list of values by level
     if level not in accumulator:
         accumulator[level] = []
-    accumulator[level].append(self.val)
-    if self.left:
-        self.left.get_boustrophedon_helper(level + 1, accumulator)
-    if self.right:
-        self.right.get_boustrophedon_helper(level + 1, accumulator)
+    accumulator[level].append(node.val)
+    if node.left:
+        get_boustrophedon_helper(node.left, level + 1, accumulator)
+    if node.right:
+        get_boustrophedon_helper(node.right, level + 1, accumulator)
 
 
-def get_boustrophedon(self):
-    if not self.root:
+def get_boustrophedon(tree: BinaryTree) -> List[int]:
+    if not tree.root:
         return []
     # generating the nodes by level
     level_data = {}
-    self.root.get_boustrophedon_helper(1, level_data)
+    get_boustrophedon_helper(tree.root, 1, level_data)
     result = []
     # adding the even levels in reverse order in the result
     for level in sorted(list(level_data.keys())):
@@ -46,22 +51,25 @@ def get_boustrophedon(self):
     return result
 
 
-setattr(Node, "get_boustrophedon_helper", get_boustrophedon_helper)
-setattr(Binary_Tree, "get_boustrophedon", get_boustrophedon)
+if __name__ == "__main__":
+    tree = BinaryTree()
+    tree.root = Node(1)
+
+    tree.root.left = Node(2)
+    tree.root.right = Node(3)
+
+    tree.root.left.left = Node(4)
+    tree.root.left.right = Node(5)
+
+    tree.root.right.left = Node(6)
+    tree.root.right.right = Node(7)
+
+    print(get_boustrophedon(tree))
 
 
-# DRIVER CODE
-tree = Binary_Tree()
+"""
+SPECS:
 
-tree.root = Node(1)
-
-tree.root.left = Node(2)
-tree.root.right = Node(3)
-
-tree.root.left.left = Node(4)
-tree.root.left.right = Node(5)
-
-tree.root.right.left = Node(6)
-tree.root.right.right = Node(7)
-
-print(tree.get_boustrophedon())
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(n)
+"""
